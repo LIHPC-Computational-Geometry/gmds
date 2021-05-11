9d2b9c28f8e65613933a16101fbc3e6f267c6dfb
franck <franck.ledoux@live.fr>
2021-05-11 12:07:20 +0200

    Update env_linux.sh

--- .github/workflows/env_linux.sh
+++ .github/workflows/env_linux.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y gcc g++ cmake lcov autoconf libtool git python3 curl
 git clone --depth=1 https://github.com/spack/spack.git
 sed -i 's#"${ARCHITECTURE}/${COMPILERNAME}-${COMPILERVER}/${PACKAGE}-${VERSION}-${HASH}"#"${PACKAGE}"#g' spack/etc/spack/defaults/config.yaml
 . ./spack/share/spack/setup-env.sh
 spack external find cmake
+spack install lcov
+spack install autoconf
+spack install libtool
 spack install kokkos+openmp
