194465fc91cb90f7295fdd6ff18d7ab662ab356a
franck <franck.ledoux@live.fr>
2021-05-18 12:31:17 +0200

    Update env_macos.sh

--- .github/workflows/env_macos.sh
+++ .github/workflows/env_macos.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 git clone --depth=1 https://github.com/spack/spack.git
-sed -i 's/#"${ARCHITECTURE}/${COMPILERNAME}-${COMPILERVER}/${PACKAGE}-${VERSION}-${HASH}"//#"${PACKAGE}"/g' spack/etc/spack/defaults/config.yaml
+sed 's/#"${ARCHITECTURE}/${COMPILERNAME}-${COMPILERVER}/${PACKAGE}-${VERSION}-${HASH}"/#"${PACKAGE}"/g' spack/etc/spack/defaults/config.yaml
+
 . ./spack/share/spack/setup-env.sh
 spack external find cmake
 spack install lcov
