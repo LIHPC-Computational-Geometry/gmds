a84f8127c5fe98a205542a84d9a855b61e65ad29
franck <franck.ledoux@live.fr>
2021-05-18 12:37:28 +0200

    Update env_macos.sh

--- .github/workflows/env_macos.sh
+++ .github/workflows/env_macos.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 git clone --depth=1 https://github.com/spack/spack.git
-echo"${ARCHITECTURE}/${COMPILERNAME}-${COMPILERVER}/${PACKAGE}-${VERSION}-${HASH}"
-echo ${ARCHITECTURE}/${COMPILERNAME}-${COMPILERVER}/${PACKAGE}-${VERSION}-${HASH}
+. ./spack/share/spack/setup-env.sh
+
+echo "#${ARCHITECTURE}/${COMPILERNAME}-${COMPILERVER}/${PACKAGE}-${VERSION}-${HASH}"
+
 sed 's/"${ARCHITECTURE}/${COMPILERNAME}-${COMPILERVER}/${PACKAGE}-${VERSION}-${HASH}"/"${PACKAGE}"/g' spack/etc/spack/defaults/config.yaml
 
 . ./spack/share/spack/setup-env.sh
