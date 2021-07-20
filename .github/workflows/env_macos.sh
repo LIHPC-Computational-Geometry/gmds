81a13b6a72078637e3796812fe49c18fb49a3c8a
franck <franck.ledoux@live.fr>
2021-07-20 09:36:02 +0200

    Update env_macos.sh
    config files cp again

--- .github/workflows/env_macos.sh
+++ .github/workflows/env_macos.sh
@@ -5,7 +5,7 @@ git clone --depth=1 https://github.com/spack/spack.git
 
 
 #replace the default configuration file of spack by a simpler one without hash, compiler versions, tags and so on 
-cp /Users/runner/work/gmds/gmd/.github/workflows/misc/config.yaml /Users/runner/spack/etc/spack/defaults/
+cp /Users/runner/work/gmds/gmds/.github/workflows/misc/config.yaml /Users/runner/work/gmds/gmds/spack/etc/spack/defaults/
 
 . ./spack/share/spack/setup-env.sh
 spack external find cmake
