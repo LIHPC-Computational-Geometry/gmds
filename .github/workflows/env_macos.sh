f4ea000a5ba1d72bed4475baaf8327234e8928b2
franck ledoux <franck.ledoux@cea.fr>
2021-07-20 09:13:30 +0200

    update github workflow for spack usage on mac os

--- .github/workflows/env_macos.sh
+++ .github/workflows/env_macos.sh
@@ -3,9 +3,9 @@
 git clone --depth=1 https://github.com/spack/spack.git
 . ./spack/share/spack/setup-env.sh
 
-echo ${ARCHITECTURE}/${COMPILERNAME}-${COMPILERVER}/${PACKAGE}-${VERSION}-${HASH}
 
-sed 's/"${ARCHITECTURE}/${COMPILERNAME}-${COMPILERVER}/${PACKAGE}-${VERSION}-${HASH}"/"${PACKAGE}"/g' spack/etc/spack/defaults/config.yaml
+#replace the default configuration file of spack by a simpler one without hash, compiler versions, tags and so on 
+cp misc/config.yaml spack/etc/spack/defaults/
 
 . ./spack/share/spack/setup-env.sh
 spack external find cmake
