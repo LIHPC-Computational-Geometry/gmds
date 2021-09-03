83d4e0e125d643f2ea3ae023dc841c207d08c060
DanielD <daniel.douheret@lilo.org>
2021-09-03 17:10:43 +0200

    remove pres/sofiane/gitci/elgtooltest

--- Sofiane/src/CommonInfo.cpp
+++
@@ -1,4 +0,0 @@
-/******************************************************************************/
-/*create only to include the CommonInfo.h when compiling the other lib*/
-#include <gmds/sofiane/CommonInfo.h>
-/******************************************************************************/
--- test_samples/quarter_crown.geo
+++
@@ -1,2 +0,0 @@
-Merge "quarter_crown.vtk";
-Physical Volume(1) -= {1};
