dbb83c5bbda193412a707ab4746a5e9a45a2a4d3
franck ledoux <franck.ledoux@cea.fr>
2021-09-09 09:56:02 +0200

    GeodHoneyComb Component skeleton added

---
+++ geodHoneyComb/src/main.cpp
@@ -0,0 +1,13 @@
+/*----------------------------------------------------------------------------*/
+#include <gmds/ig/Mesh.h>
+#include <gmds/geodHoneyComb/GeodHexMesher.h>
+/*----------------------------------------------------------------------------*/
+#include <iostream>
+#include <gmds/io/VTKWriter.h>
+
+using namespace gmds;
+/*----------------------------------------------------------------------------*/
+int main(int argc, char* argv[])
+{
+    return 0;
+}
