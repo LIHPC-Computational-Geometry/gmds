743184b91f3c72309c6d2638d22bc463576b8e1f
Franck Ledoux <franck.ledoux@live.fr>
2021-10-21 18:01:12 +0200

    - correction in Claire's module for getting Windows dll exportation - Change and add some structurations

--- claire/inc/gmds/claire/Smooth2D.h
+++ claire/inc/gmds/claire/Smooth2D.h
@@ -1,56 +1,68 @@
+/*----------------------------------------------------------------------------*/
 #ifndef GMDS_SMOOTH2D_H
 #define GMDS_SMOOTH2D_H
-
+/*----------------------------------------------------------------------------*/
+#include "LIB_GMDS_CLAIRE_export.h"
 #include <gmds/ig/Mesh.h>
+/*----------------------------------------------------------------------------*/
 #include <string>
 #include <map>
+/*----------------------------------------------------------------------------*/
 namespace gmds {
-class Smooth2D
-{
- public:
-	/*--------------------------------------------------------------------*/
-	/** @enum  Status code for executing algorithms
-	 */
-	typedef enum {
-		FAIL,
-		SUCCESS
-	} STATUS;
+/*----------------------------------------------------------------------------*/
+    class LIB_GMDS_CLAIRE_API Smooth2D {
+    public:
+        /*--------------------------------------------------------------------*/
+        /** @enum  Status code for executing algorithms
+         */
+        typedef enum {
+            FAIL,
+            SUCCESS
+        } STATUS;
 
-	/*------------------------------------------------------------------------*/
-	/** @brief Constructor.
-	 *  @param AMesh the mesh where we work on
-	 *  @param AVarBnd node variable (value 1 means constrained node)
-	 *  @param ANbIterations nb max iterations
-	 */
-	Smooth2D(Mesh *AMesh,
-	         const Variable<int>* AVarBnd,
-	         const int ANbIterations = 100);
+        /*-------------------------------------------------------------------*/
+        /** @brief Constructor.
+         *  @param AMesh the mesh where we work on
+         *  @param AVarBnd node variable (value 1 means constrained node)
+         *  @param ANbIterations nb max iterations
+         */
+        Smooth2D(Mesh *AMesh,
+                 const Variable<int> *AVarBnd,
+                 const int ANbIterations = 100);
 
-	/*------------------------------------------------------------------------*/
-	/** @brief Set the max number of iterations
-	 *  @param[in] ANbIterations
-	 */
-	void setNbIterations(const int ANbIterations);
+        /*-------------------------------------------------------------------*/
+        /** @brief Set the max number of iterations
+         *  @param[in] ANbIterations
+         */
+        void setNbIterations(const int ANbIterations);
 
-	/*------------------------------------------------------------------------*/
-	/** @brief Execute the algorithm
-	 */
-	STATUS execute();
+        /*-------------------------------------------------------------------*/
+        /** @brief Execute the algorithm
+         */
+        STATUS execute();
+    private:
+        /*-------------------------------------------------------------------*/
+        /** @brief Goes through all the free nodes in the mesh and build
+         *         corresponding stencils
+         */
+        void buildStencils();
+    private:
+        /** mesh we work on */
+        Mesh *m_mesh;
+        /** Variable to store which nodes are constrained*/
+        const Variable<int> *m_node_constrained;
+        /** nb max iterations */
+        int m_nb_max_iterations;
 
- private:
-	/** mesh we work on */
-	Mesh *m_mesh;
-	/** Variable to store which nodes are constrained*/
-	const Variable<int>* m_node_constrained;
-	/** nb max iterations */
-	int m_nb_max_iterations;
-
-	typedef struct{
-		unsigned int val[3][3];
-	}stencil;
-	std::map<TCellID,stencil> m_node_to_stencil;
-};
-
+        /** free nodes in the mesh */
+        std::vector<TCellID> m_free_nodes;
+        typedef struct {
+            unsigned int val[3][3];
+        } stencil;
+        std::map<TCellID, stencil> m_stencil;
+    };
+/*----------------------------------------------------------------------------*/
 }     // namespace gmds
-
+/*----------------------------------------------------------------------------*/
 #endif     // GMDS_SMOOTH2D_H
+/*----------------------------------------------------------------------------*/
--- claire/src/Smooth2D.cpp
+++ claire/src/Smooth2D.cpp
@@ -23,120 +23,126 @@ void Smooth2D::setNbIterations(const int ANbIterations)
 /*------------------------------------------------------------------------*/
 Smooth2D::STATUS Smooth2D::execute()
 {
-	std::vector<TCellID> free_node_ids;
+    //we traverse all the nodes of the whole mesh to get the nodes that
+    //are not constrained, i.e are so free
 	for(auto n_id:m_mesh->nodes()){
 		if(m_node_constrained->value(n_id)!=1){
-			free_node_ids.push_back(n_id);
+			m_free_nodes.push_back(n_id);
 		}
 	}
+    //for all free nodes, we build and store stencils locally
+    buildStencils();
 
-	for(auto n_id:free_node_ids){
-		Node current_node = m_mesh->get<Node>(n_id);
-		std::vector<Face> current_faces = current_node.get<Face>();
-		// FIRST FACE
-		Face f0 = current_faces[0];
-		Node f0_n0;
-		Node f0_n1;
-		f0.getAdjacentNodes(current_node,f0_n0,f0_n1);
-		stencil current_stencil;
-		current_stencil.val[1][1] = current_node.id();
-		current_stencil.val[1][0] = f0_n0.id();
-		current_stencil.val[0][1] = f0_n1.id();
-		std::vector<TCellID> f_nodes=f0.getIDs<Node>();
-		for(auto f_n_id:f_nodes){
-			if(f_n_id!=current_node.id() &&
-				f_n_id!=f0_n0.id() &&
-				f_n_id!=f0_n1.id()) {
-				current_stencil.val[0][0] = f_n_id;
-			}
-		}
-		//SECOND FACE
-		std::vector<TCellID> faces_11_10 = m_mesh->getCommonFaces(current_node,f0_n0);
-		TCellID f1_id = NullID;
-		if(faces_11_10[0]==f0.id()){
-			f1_id=faces_11_10[1];
-		}
-		else{
-			f1_id=faces_11_10[0];
-		}
-		Face f1 = m_mesh->get<Face>(f1_id);
-		Node f1_n0;
-		Node f1_n1;
-		f1.getAdjacentNodes(current_node,f1_n0,f1_n1);
-		if(f1_n0.id()==current_stencil.val[1][0])
-			current_stencil.val[2][1]=f1_n1.id();
-		else
-			current_stencil.val[2][1]=f1_n0.id();
-
-		f_nodes=f1.getIDs<Node>();
-		for(auto f_n_id:f_nodes){
-			if(f_n_id!=current_node.id() &&
-			    f_n_id!=f1_n0.id() &&
-			    f_n_id!=f1_n1.id()) {
-				current_stencil.val[2][0] = f_n_id;
-			}
-		}
-		//THIRD FACE
-		std::vector<TCellID> faces_11_21 =
-		   m_mesh->getCommonFaces(current_node,
-										  m_mesh->get<Node>(current_stencil.val[2][1]));
-		TCellID f2_id = NullID;
-		if(faces_11_21[0]==f1.id()){
-			f2_id=faces_11_21[1];
-		}
-		else{
-			f2_id=faces_11_21[0];
-		}
-		Face f2 = m_mesh->get<Face>(f2_id);
-		Node f2_n0;
-		Node f2_n1;
-		f2.getAdjacentNodes(current_node,f2_n0,f2_n1);
-
-		if(f2_n0.id()==current_stencil.val[2][1])
-			current_stencil.val[1][2]=f2_n1.id();
-		else
-			current_stencil.val[1][2]=f2_n0.id();
-
-		f_nodes=f2.getIDs<Node>();
-		for(auto f_n_id:f_nodes){
-			if(f_n_id!=current_node.id() &&
-			    f_n_id!=f2_n0.id() &&
-			    f_n_id!=f2_n1.id()) {
-				current_stencil.val[2][2] = f_n_id;
-			}
-		}
-
-		//FOURTH FACE
-		std::vector<TCellID> faces_11_12 =
-		   m_mesh->getCommonFaces(current_node,
-		                          m_mesh->get<Node>(current_stencil.val[1][2]));
-		TCellID f3_id = NullID;
-		if(faces_11_12[0]==f2.id()){
-			f3_id=faces_11_12[1];
-		}
-		else{
-			f3_id=faces_11_12[0];
-		}
-		Face f3 = m_mesh->get<Face>(f3_id);
-
-		f_nodes=f3.getIDs<Node>();
-		for(auto f_n_id:f_nodes){
-			if(f_n_id!=current_node.id() &&
-				f_n_id!=current_stencil.val[1][2] &&
-				f_n_id!=current_stencil.val[0][1]) {
-				current_stencil.val[0][2] = f_n_id;
-			}
-		}
-
-		m_node_to_stencil.insert(std::make_pair(n_id,current_stencil));
-		std::cout<<"Node "<<n_id<<std::endl;
-		for(auto i=0;i<3;i++){
-			for(auto j=0;j<3;j++){
-				std::cout<<m_node_to_stencil[n_id].val[i][j]<<" ";
-			}
-			std::cout<<std::endl;
-		}
-	}
 	return Smooth2D::SUCCESS;
+}
+/*------------------------------------------------------------------------*/
+void Smooth2D::buildStencils() {
+    for(auto n_id:m_free_nodes){
+        Node current_node = m_mesh->get<Node>(n_id);
+        std::vector<Face> current_faces = current_node.get<Face>();
+        // FIRST FACE
+        Face f0 = current_faces[0];
+        Node f0_n0;
+        Node f0_n1;
+        f0.getAdjacentNodes(current_node,f0_n0,f0_n1);
+        stencil current_stencil;
+        current_stencil.val[1][1] = current_node.id();
+        current_stencil.val[1][0] = f0_n0.id();
+        current_stencil.val[0][1] = f0_n1.id();
+        std::vector<TCellID> f_nodes=f0.getIDs<Node>();
+        for(auto f_n_id:f_nodes){
+            if(f_n_id!=current_node.id() &&
+               f_n_id!=f0_n0.id() &&
+               f_n_id!=f0_n1.id()) {
+                current_stencil.val[0][0] = f_n_id;
+            }
+        }
+        //SECOND FACE
+        std::vector<TCellID> faces_11_10 = m_mesh->getCommonFaces(current_node,f0_n0);
+        TCellID f1_id = NullID;
+        if(faces_11_10[0]==f0.id()){
+            f1_id=faces_11_10[1];
+        }
+        else{
+            f1_id=faces_11_10[0];
+        }
+        Face f1 = m_mesh->get<Face>(f1_id);
+        Node f1_n0;
+        Node f1_n1;
+        f1.getAdjacentNodes(current_node,f1_n0,f1_n1);
+        if(f1_n0.id()==current_stencil.val[1][0])
+            current_stencil.val[2][1]=f1_n1.id();
+        else
+            current_stencil.val[2][1]=f1_n0.id();
+
+        f_nodes=f1.getIDs<Node>();
+        for(auto f_n_id:f_nodes){
+            if(f_n_id!=current_node.id() &&
+               f_n_id!=f1_n0.id() &&
+               f_n_id!=f1_n1.id()) {
+                current_stencil.val[2][0] = f_n_id;
+            }
+        }
+        //THIRD FACE
+        std::vector<TCellID> faces_11_21 =
+                m_mesh->getCommonFaces(current_node,
+                                       m_mesh->get<Node>(current_stencil.val[2][1]));
+        TCellID f2_id = NullID;
+        if(faces_11_21[0]==f1.id()){
+            f2_id=faces_11_21[1];
+        }
+        else{
+            f2_id=faces_11_21[0];
+        }
+        Face f2 = m_mesh->get<Face>(f2_id);
+        Node f2_n0;
+        Node f2_n1;
+        f2.getAdjacentNodes(current_node,f2_n0,f2_n1);
+
+        if(f2_n0.id()==current_stencil.val[2][1])
+            current_stencil.val[1][2]=f2_n1.id();
+        else
+            current_stencil.val[1][2]=f2_n0.id();
+
+        f_nodes=f2.getIDs<Node>();
+        for(auto f_n_id:f_nodes){
+            if(f_n_id!=current_node.id() &&
+               f_n_id!=f2_n0.id() &&
+               f_n_id!=f2_n1.id()) {
+                current_stencil.val[2][2] = f_n_id;
+            }
+        }
+
+        //FOURTH FACE
+        std::vector<TCellID> faces_11_12 =
+                m_mesh->getCommonFaces(current_node,
+                                       m_mesh->get<Node>(current_stencil.val[1][2]));
+        TCellID f3_id = NullID;
+        if(faces_11_12[0]==f2.id()){
+            f3_id=faces_11_12[1];
+        }
+        else{
+            f3_id=faces_11_12[0];
+        }
+        Face f3 = m_mesh->get<Face>(f3_id);
+
+        f_nodes=f3.getIDs<Node>();
+        for(auto f_n_id:f_nodes){
+            if(f_n_id!=current_node.id() &&
+               f_n_id!=current_stencil.val[1][2] &&
+               f_n_id!=current_stencil.val[0][1]) {
+                current_stencil.val[0][2] = f_n_id;
+            }
+        }
+
+        m_stencil.insert(std::make_pair(n_id, current_stencil));
+        std::cout<<"Node "<<n_id<<std::endl;
+        for(auto i=0;i<3;i++){
+            for(auto j=0;j<3;j++){
+                std::cout << m_stencil[n_id].val[i][j] << " ";
+            }
+            std::cout<<std::endl;
+        }
+    }
 }
 /*------------------------------------------------------------------------*/
