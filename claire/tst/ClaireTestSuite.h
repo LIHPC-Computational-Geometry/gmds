3a75b52b16027b8a7671fcd3bc7b4a00d24f0f03
Claire Roche <Claire.Roche@cea.fr>
2021-10-26 15:44:25 +0200

    - Test

--- claire/tst/ClaireTestSuite.h
+++ claire/tst/ClaireTestSuite.h
@@ -67,13 +67,11 @@ TEST(ClaireTestClass, testGrid2D_Perturbation_1)
 
 	int Nx = 6;								// Nombre de noeuds dans la direction x
 	int Ny = 6;								// Nombre de noeuds dans la direction y
-	double Lx = 1.0;						// Longueur du domaine dans la direction x
-	double Ly = 1.0;						// Longueur du domaine dans la direction y
-	double dx = Lx/(Nx-1);				// Pas d'espace suivant l'axe x
-	double dy = Ly/(Ny-1);				// Pas d'espace suivant l'axe y
+	double dx = 1.0/Nx;					// Pas d'espace suivant l'axe x
+	double dy = 1.0/Ny;					// Pas d'espace suivant l'axe y
 
 	GridBuilder gb(&m,2);
-	gb.execute(Nx, dx, Ny, dy);
+	gb.execute(Nx, 1.0, Ny, 1.0);
 
 	ASSERT_EQ(m.getNbNodes(),Nx*Ny);
 	ASSERT_EQ(m.getNbFaces(),(Nx-1)*(Ny-1));
@@ -89,32 +87,24 @@ TEST(ClaireTestClass, testGrid2D_Perturbation_1)
 	std::vector<TCellID> bnd_node_ids;
 	bnd_op.getBoundaryNodes(bnd_node_ids);
 	int Nbr_BoundaryNodes = 2*Nx + 2*(Ny-2);								// Nombre de noeuds sur les bords
-	ASSERT_EQ(Nbr_BoundaryNodes, bnd_node_ids.size());			// Vérification : est-ce qu'on a tous les noeuds des bords
+	ASSERT_EQ(Nbr_BoundaryNodes, bnd_node_ids.size());
 
 	Variable<int>* var_bnd = m.newVariable<int,GMDS_NODE>("constraint");
 	for(auto id:bnd_node_ids){
 		var_bnd->value(id)=1;								// Les noeuds des bords sont marqués par l'entier 1
 	}
 
-	// ------------ PERTURBATION ALEATOIRE DU MAILLAGE ------------
-	// Initialisation de la variable n qui est un noeud et n_coords qui récupère les coordonnées du noeud
 	Node n = m.get<Node>(1);
 	math::Point n_coords = n.point();
-	double c1 = 0.0;
-	double c2 = 0.0;
-	for(auto id:m.nodes()){
+	// Pb : sur quoi on itère ?
+	for(auto id:var_bnd->value()){
 		if(var_bnd->value(id)==0){
 			n = m.get<Node>(id);
-		   n_coords = n.point();
-			c1 = rand()/ double(RAND_MAX) ;
-			c2 = rand()/ double(RAND_MAX) ;
-			//std::cout << "Numéro node :" << n << std::endl ;
-			//std::cout << "Nombre aléatoire c1 =" << c1 << std::endl ;
-			n.setX( n_coords.X() + (2.0*c1-1.0) * dx);
-			n.setY( n_coords.Y() + (2.0*c2-1.0) * dy);
+			std::cout << "Numéro node :" << n << std::endl ;
+			n.setX( n_coords.X() + 0.5);
+			n.setY( n_coords.Y() + 0.5);
 		}
 	}
-	// ---------------------------------------------------------------
 
 	Smooth2D smoother(&m,var_bnd);
 	Smooth2D::STATUS result = smoother.execute();
