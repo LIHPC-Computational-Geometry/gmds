09676863f9cb82c379f95755d4487868edfd0862
Claire Roche <Claire.Roche@cea.fr>
2021-10-27 13:18:41 +0200

    Ajout de la fonction pour obtenir le milieu d'une branche (définie par trois points)

--- claire/src/Smooth2D.cpp
+++ claire/src/Smooth2D.cpp
@@ -13,13 +13,11 @@ Smooth2D::Smooth2D(Mesh *AMesh,
 	m_mesh = AMesh;
 	m_node_constrained = AVarBnd;
 	m_nb_max_iterations = ANbIterations;
-
 }
 /*------------------------------------------------------------------------*/
 void Smooth2D::setNbIterations(const int ANbIterations)
 {
 	m_nb_max_iterations=ANbIterations;
-
 }
 /*------------------------------------------------------------------------*/
 Smooth2D::STATUS Smooth2D::execute()
@@ -31,135 +29,30 @@ Smooth2D::STATUS Smooth2D::execute()
 			m_free_nodes.push_back(n_id);
 		}
 	}
+    //for all free nodes, we build and store stencils locally
+    buildStencils();
 
-	// Check if the mesh is structured or not
-	bool CheckMesh = CheckStructuredMesh();
 
-	if (CheckMesh) {
-		// for all free nodes, we build and store stencils locally
-		buildStencils();
+	 /*
+	 std::cout << "TEST 1 :" << std::endl ;
+	 math::Point A(0,0,0);
+	 math::Point B(1,0,0);
+	 math::Point C(3,0,0);
+	 math::Point Test_Mid ;
+	 Test_Mid = FindMidBranche(A, B, C) ;
 
-		// Initialization of a pointer to a variable to store the old coordinates of each node
-		Variable<math::Point> *old_coords = nullptr;
-		old_coords = m_mesh->newVariable<math::Point,GMDS_NODE>("old_coords") ;
-		// Now, old_coords point to a new table created as an attritube from m_mesh
-		// In this table, there is a math::Point for each node (GMDS_NODE)
-		// Exemple to access to elements
-		//math::Point test_point = old_coords->value(0) ;
-
-		for (int iteration = 1; iteration <= m_nb_max_iterations; iteration++) {
-			// Loop on the node ids of the mesh to store the coordinates of each node
-			for(auto n_id:m_mesh->nodes()) {
-				old_coords->set(n_id, m_mesh->get<Node>(n_id).point());
-			}
-
-			for (auto n_id : m_free_nodes) {
-				math::Point H1, H2, H3;
-				math::Point V1, V2, V3;
-				math::Point A, B, C;
-				Node noeud_traite;
-				Node noeud_voisin;
-				// std::cout << "Noeud :" << n_id << std::endl;
-				noeud_traite = m_mesh->get<Node>(n_id);
-
-				// Computation of the 3 mid-points from the first branch (verticale)
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[0][0]);
-				A = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[1][0]);
-				B = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[2][0]);
-				C = old_coords->value(noeud_voisin.id()) ;
-				V1 = FindMidBranche(A, B, C);
-
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[0][1]);
-				A = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[1][1]);
-				B = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[2][1]);
-				C = old_coords->value(noeud_voisin.id()) ;
-				V2 = FindMidBranche(A, B, C);
-
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[0][2]);
-				A = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[1][2]);
-				B = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[2][2]);
-				C = old_coords->value(noeud_voisin.id()) ;
-				V3 = FindMidBranche(A, B, C);
-
-				// Computation of the 3 mid-points from the second branch (horizontale)
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[0][0]);
-				A = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[0][1]);
-				B = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[0][2]);
-				C = old_coords->value(noeud_voisin.id()) ;
-				H1 = FindMidBranche(A, B, C);
-
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[1][0]);
-				A = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[1][1]);
-				B = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[1][2]);
-				C = old_coords->value(noeud_voisin.id()) ;
-				H2 = FindMidBranche(A, B, C);
-
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[2][0]);
-				A = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[2][1]);
-				B = old_coords->value(noeud_voisin.id()) ;
-				noeud_voisin = m_mesh->get<Node>(m_stencil[n_id].val[2][2]);
-				//C = noeud_voisin.point();
-				C = old_coords->value(noeud_voisin.id()) ;
-				H3 = FindMidBranche(A, B, C);
-
-				// Finding the intersection between the 4 segments
-				bool intersection_trouvee(false);
-				math::Point M(0, 0, 0);
-				math::Segment Seg_Vert_1(V1, V2);
-				math::Segment Seg_Vert_2(V2, V3);
-				math::Segment Seg_Hori_1(H1, H2);
-				math::Segment Seg_Hori_2(H2, H3);
-
-				intersection_trouvee = Seg_Vert_1.intersect2D(Seg_Hori_1, M);
-				if (!intersection_trouvee) {
-					intersection_trouvee = Seg_Vert_1.intersect2D(Seg_Hori_2, M);
-				}
-				if (!intersection_trouvee) {
-					intersection_trouvee = Seg_Vert_2.intersect2D(Seg_Hori_1, M);
-				}
-				if (!intersection_trouvee) {
-					intersection_trouvee = Seg_Vert_2.intersect2D(Seg_Hori_2, M);
-				}
-
-				/*
-				// PRINT FOR DEBUG
-				if (iteration==m_nb_max_iterations) {
-				   std::cout << "--------------------------------" << std::endl;
-				   std::cout << "Iterations :" << iteration << std::endl;
-				   std::cout << "Noeud :" << n_id << std::endl;
-				   std::cout << "Intersection trouvee ?" << intersection_trouvee << std::endl;
-				   std::cout << "Pos actuelle :" << noeud_traite.point() << std::endl;
-				   std::cout << "Nouvelle position :" << M << std::endl;
-				}
-				// END OF PRINTS FOR DEBUG
-				*/
-
-				if (intersection_trouvee) {
-					noeud_traite.setPoint(M);
-				}
-
-				// A .txt file is writen to plot with gluplot
-				if ( (n_id == 5) && (iteration == 1) ) {
-					write_debug_txt(n_id, old_coords, H1, H2, H3, V1, V2, V3, noeud_traite.point(), "test.txt");
-				}
-
-			}
-
-		}
-		m_mesh->deleteVariable(GMDS_NODE, old_coords);
-	}
-
+	 std::cout << "----------------------" << std::endl ;
+	 std::cout << "TEST 2 :" << std::endl ;
+	 A = (0,0,0);
+	 B = (0,1,0);
+	 B.X() = 0,
+	 B.Y() = 1;
+	 B.Z() = 0;
+	 C.X() = 2;
+	 C.Y() = 1;
+	 C.Z() = 0;
+	 Test_Mid = FindMidBranche(A, B, C) ;
+	  */
 
 	    return Smooth2D::SUCCESS;
 }
@@ -280,8 +173,8 @@ void Smooth2D::buildStencils() {
 
 
 /*
-// ------------ PERTURBATION ALEATOIRE DU MAILLAGE ------------
 void Smooth2D::PerturbationMaillage(const Variable<int>* var_bnd, const double dx, const double dy) {
+	// ------------ PERTURBATION ALEATOIRE DU MAILLAGE ------------
 	// Initialisation de la variable n qui est un noeud et n_coords qui récupère les coordonnées du noeud
 	Node n = m_mesh->get<Node>(1);
 	math::Point n_coords = n.point();
@@ -301,7 +194,8 @@ void Smooth2D::PerturbationMaillage(const Variable<int>* var_bnd, const double d
 	}
 	// ---------------------------------------------------------------
 }
-*/
+ */
+
 
 
 
@@ -313,139 +207,23 @@ void Smooth2D::PerturbationMaillage(const Variable<int>* var_bnd, const double d
 // En sortie : le point milieu
 math::Point Smooth2D::FindMidBranche(const math::Point A, const math::Point B, const math::Point C) {
 	math::Point Point_Milieu ;
-	math::Vector3d Vec_AB = B-A ;
-	math::Vector3d Vec_BC = C-B ;
-	double norme_1 = Vec_AB.norm() ;
-	double norme_2 = Vec_BC.norm() ;
+	double norme_1 = sqrt( pow((A.X()-B.X()),2) + pow(A.Y()-B.Y(),2) ) ;
+	double norme_2 = sqrt( pow((B.X()-C.X()),2) + pow(B.Y()-C.Y(),2) ) ;
 	double norme_branche = norme_1 + norme_2 ;
 	double norme_milieu = norme_branche / 2.0 ;
 
 	if (norme_milieu <= norme_1){
-		Vec_AB.normalize();
-		Point_Milieu = A + norme_milieu*Vec_AB ;
+		math::Vector3d Vec( B.X()-A.X(), B.Y()-A.Y(), 0 ) ;
+		Vec.normalize();
+		Point_Milieu = A + norme_milieu*Vec ;
 	}
 	else if (norme_milieu > norme_1){
-		math::Vector3d Vec_CB = - Vec_BC ;
-		Vec_CB.normalize();
-		Point_Milieu = C + norme_milieu*Vec_CB ;
+		math::Vector3d Vec( B.X()-C.X(), B.Y()-C.Y(), 0 ) ;
+		Vec.normalize();
+		Point_Milieu = C + norme_milieu*Vec ;
 	}
 
+	std::cout << "Point milieu :" << Point_Milieu << std::endl ;
 	return Point_Milieu;
-}
-/*------------------------------------------------------------------------*/
-
-
-
-
-
-
-/*------------------------------------------------------------------------*/
-// Fonction CheckStructuredMesh : Prend un maillage en entrée et vérifie si il est structuré ou non
-// En entrée :
-// En sortie :
-bool Smooth2D::CheckStructuredMesh() {
-	bool checkmesh(true);
-	// Check if each inner node have 4 adjacent faces
-	for(auto n_id:m_free_nodes) {
-		if (checkmesh) {
-			Node current_node = m_mesh->get<Node>(n_id);
-			std::vector<Face> current_faces = current_node.get<Face>();
-			int Nbr_Faces = current_faces.size();
-			if (Nbr_Faces != 4) {
-				checkmesh = false;
-			}
-		}
-	}
-
-	// Check if each face is a quad
-	for(auto n_id:m_mesh->faces()){
-		if (checkmesh) {
-			Face current_face = m_mesh->get<Face>(n_id);
-			std::vector<Node> current_nodes = current_face.get<Node>();
-			int Nbr_Noeuds = current_nodes.size();
-			if (Nbr_Noeuds != 4) {
-				checkmesh = false;
-			}
-		}
-	}
-
-	return checkmesh;
-}
-/*------------------------------------------------------------------------*/
-
-
-
-
-
-
-
-/*------------------------------------------------------------------------*/
-void Smooth2D::write_debug_txt(int n_id, const Variable<math::Point> *old_coords,
-                          math::Point H1, math::Point H2, math::Point H3,
-                          math::Point V1, math::Point V2, math::Point V3,
-                          math::Point Point_Intersection,
-                          std::string AFileName){
-
-	// First, we create the file where we are going to store the info
-	std::ofstream stream= std::ofstream(AFileName, std::ios::out);
-	//set the numerical precision (number of digits)
-	stream.precision(15);
-	//Header indicating which type of file it is
-	stream << "# Claire PHD Debug Version 1.0\n\n";
-
-	//Write the id of the node
-	stream << "# NODE " << n_id << "\n";
-
-	// For each node, I write the coordinates of the stencil
-	stream << "# COORDINATES OF THE STENCIL " << "\n";
-	for(auto i=0;i<3;i++){
-		for(auto j=0;j<3;j++){
-			Node n_local = m_mesh->get<Node>(m_stencil[n_id].val[i][j]);
-			math::Point point_local = old_coords->value(n_local.id()) ;
-			stream << point_local.X() << " " << point_local.Y() << " " << point_local.Z() << "\n";
-		}
-		stream << "\n" ;
-	}
-
-	for(auto j=0;j<3;j++){
-		for(auto i=0;i<3;i++){
-			Node n_local = m_mesh->get<Node>(m_stencil[n_id].val[i][j]);
-			math::Point point_local = old_coords->value(n_local.id()) ;
-			stream << point_local.X() << " " << point_local.Y() << " " << point_local.Z() << "\n";
-		}
-		stream << "\n" ;
-	}
-	stream << "\n" ;
-
-	// Write the 6 nodes for the Line-Sweeping algorithm
-	stream << "# FIRST BRANCH OF MID POINTS " << "\n";
-	stream << H1.X() << " " << H1.Y() << " " << H1.Z() << "\n";
-	stream << H2.X() << " " << H2.Y() << " " << H2.Z() << "\n";
-	stream << H3.X() << " " << H3.Y() << " " << H3.Z() << "\n";
-	stream << "\n";
-	stream << "\n";
-
-	stream << "# SECOND BRANCH OF MID POINTS " << "\n";
-	stream << V1.X() << " " << V1.Y() << " " << V1.Z() << "\n";
-	stream << V2.X() << " " << V2.Y() << " " << V2.Z() << "\n";
-	stream << V3.X() << " " << V3.Y() << " " << V3.Z() << "\n";
-	stream << "\n";
-	stream << "\n";
-
-	// Write the intersection point
-	stream << "# INTERSECTION POINT " << "\n";
-	stream << Point_Intersection.X() << " " << Point_Intersection.Y() << " " << Point_Intersection.Z() << "\n";
-	stream << "\n";
-	stream << "\n";
-
-	stream << "# FOR GNUPLOT :" << "\n";
-	stream << "plot 'test.txt' index 0 w lp linecolor rgb 'black' title 'stencil' " << "\n";
-	stream << "replot 'test.txt' index 1 w lp linecolor rgb 'red' title 'first mid branch' " << "\n";
-	stream << "replot 'test.txt' index 2 w lp linecolor rgb 'green' title 'second mid branch' " << "\n";
-	stream << "replot 'test.txt' index 3 w lp linecolor rgb 'pink' title 'intersection point' " << "\n";
-	stream << "\n";
-
-
-	stream.close();
 }
 /*------------------------------------------------------------------------*/
