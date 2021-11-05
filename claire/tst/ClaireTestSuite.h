856c2cfbff30b014a6730aad5f8aa655df07b30e
Claire Roche <Claire.Roche@cea.fr>
2021-11-05 13:20:33 +0100

    Ajout du cas test : Maillage d'une forme quart de cylindre creux en partant d'un maillage d'un carré, en redéfinissant les bords et en utilisant l'algorithme de lissage

--- claire/tst/ClaireTestSuite.h
+++ claire/tst/ClaireTestSuite.h
@@ -128,3 +128,107 @@ TEST(ClaireTestClass, testGrid2D_Perturbation_1)
 
 	ASSERT_EQ(Smooth2D::SUCCESS, result);
 }
+
+
+
+
+
+
+
+TEST(ClaireTestClass, testGrid2D_Quart_Cylindre)
+{
+	Mesh m(MeshModel(DIM3 | R | F | N | F2N | N2F));
+
+	int Nx = 10;								// Nombre de noeuds dans la direction x
+	int Ny = 10;								// Nombre de noeuds dans la direction y
+	double Lx = 1.0;						// Longueur du domaine dans la direction x
+	double Ly = 1.0;						// Longueur du domaine dans la direction y
+	double dx = Lx/(Nx-1);				// Pas d'espace suivant l'axe x
+	double dy = Ly/(Ny-1);				// Pas d'espace suivant l'axe y
+
+	double R_int = 1.0;					// Rayon du cylindre
+	double R_ext = 5.0;					// Rayon de la frontière extérieure
+
+	GridBuilder gb(&m,2);
+	gb.execute(Nx, dx, Ny, dy);
+
+	ASSERT_EQ(m.getNbNodes(),Nx*Ny);
+	ASSERT_EQ(m.getNbFaces(),(Nx-1)*(Ny-1));
+
+	//==================================================================
+	// MESH PREPARATION
+	//==================================================================
+	MeshDoctor doctor(&m);
+	doctor.updateUpwardConnectivity();
+
+	//Get the boundary node ids
+	BoundaryOperator2D bnd_op(&m);
+	std::vector<TCellID> bnd_node_ids;
+	bnd_op.getBoundaryNodes(bnd_node_ids);
+	int Nbr_BoundaryNodes = 2*Nx + 2*(Ny-2);								// Nombre de noeuds sur les bords
+	ASSERT_EQ(Nbr_BoundaryNodes, bnd_node_ids.size());			// Vérification : est-ce qu'on a tous les noeuds des bords
+
+	Variable<int>* var_bnd = m.newVariable<int,GMDS_NODE>("constraint");
+	for(auto id:bnd_node_ids){
+		var_bnd->value(id)=1;								// Les noeuds des bords sont marqués par l'entier 1
+	}
+
+
+	// ------------ REDEFINITION DES BORDS DU MAILLAGE ------------
+	Node n = m.get<Node>(1);
+	math::Point n_coords = n.point();
+	double theta(0.0);
+	// On parcours les noeuds du bord pour les replacer et former un domaine
+	// quart de cylindre creux.
+	// ATTENTION : ne fonctionne que grace à l'observation de la manière dont
+	// sont numérotés les noeuds lors de la génération du maillage avec
+	// GridBuilder
+	for(auto id:bnd_node_ids){
+			n = m.get<Node>(id);
+			n_coords = n.point();
+			//std::cout << "Numéro node :" << n << std::endl ;
+			//std::cout << "Nombre aléatoire c1 =" << c1 << std::endl ;
+			n.setX( n_coords.X() );
+			n.setY( n_coords.Y() );
+		   // "Bord gauche" du domaine carré
+		   if (id <= Ny-1) {
+			   n.setX( - R_int - (double(id)/(Ny-1))*(R_ext-R_int) );
+				n.setY( 0.0 );
+		   }
+		   // "Bord droit" du domaine carré
+		   else if (id >= (Ny-1)*Nx) {
+			   n.setX( 0.0 );
+			   n.setY( R_int + ((double(id)-(Nx-1.0)*Ny)/(Ny-1.0))*(R_ext-R_int) );
+		   }
+		   // "Bord bas et haut" du domaine carré
+		   else {
+			   // "Bord bas" du domaine carré
+			   if ( id%Ny == 0 ) {
+				   int k = id / Ny;
+				   theta = M_PI - (double(k) / (Nx - 1.0)) * M_PI / 2.0;
+				   n.setX(R_int*cos(theta) );
+				   n.setY(R_int*sin(theta) );
+			   }
+			   // "Bord haut" du domaine carré
+			   else {
+				   int k = ( (id+1) / Ny) - 1;
+				   theta = M_PI - (double(k) / (Nx - 1.0)) * M_PI / 2.0;
+				   n.setX(R_ext*cos(theta) );
+				   n.setY(R_ext*sin(theta) );
+			   }
+		   }
+	}
+	// ---------------------------------------------------------------
+
+	Smooth2D smoother(&m,var_bnd, 100);
+	Smooth2D::STATUS result = smoother.execute();
+
+
+	IGMeshIOService ioService_geom(&m);
+	VTKWriter writer_geom(&ioService_geom);
+	writer_geom.setCellOptions(N|F);
+	writer_geom.setDataOptions(N|F);
+	writer_geom.write("smooth2D_test_quart_cylindre_result.vtk");
+
+	ASSERT_EQ(Smooth2D::SUCCESS, result);
+}
