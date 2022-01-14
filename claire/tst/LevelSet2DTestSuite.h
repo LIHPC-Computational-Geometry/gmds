d6177af8e3dcc5b800bb4f3f23dfb702f5f154fb
Claire Ordi Pro <claireroche47@gmail.com>
2022-01-14 14:26:14 +0100

    Nettoyage de la classe LevelSet2D et début d'implémentation des méthodes.

--- claire/tst/LevelSet2DTestSuite.h
+++ claire/tst/LevelSet2DTestSuite.h
@@ -16,53 +16,8 @@
 /*----------------------------------------------------------------------------*/
 using namespace gmds;
 /*----------------------------------------------------------------------------*/
-/*
+
 TEST(LevelSet2DTestClass, LevelSet2D_Test1)
-{
-	Mesh m(MeshModel(DIM3 | R | F | N | F2N | N2F));
-
-	GridBuilder gb(&m,2);
-	gb.execute(3,1.0, 4, 1.0);
-
-	ASSERT_EQ(m.getNbNodes(),12);
-	ASSERT_EQ(m.getNbFaces(),6);
-
-	//==================================================================
-	// MESH PREPARATION
-	//==================================================================
-	MeshDoctor doctor(&m);
-	doctor.updateUpwardConnectivity();
-
-	//Get the boundary node ids
-	BoundaryOperator2D bnd_op(&m);
-	std::vector<TCellID> bnd_node_ids;
-	bnd_op.getBoundaryNodes(bnd_node_ids);
-	ASSERT_EQ(10, bnd_node_ids.size());
-
-	Variable<int>* var_bnd = m.newVariable<int,GMDS_NODE>("constraint");
-	for(auto id:bnd_node_ids){
-		var_bnd->value(id)=1;
-	}
-
-	Node n5 = m.get<Node>(5);
-	math::Point p5 = n5.point();
-	math::Point p5_new(p5.X()+0.2,p5.Y()+0.3,p5.Z());
-	n5.setPoint(p5_new);
-
-	LevelSet2D ls(&m);
-	LevelSet2D::STATUS result = ls.execute();
-
-	IGMeshIOService ioService_geom(&m);
-	VTKWriter writer_geom(&ioService_geom);
-	writer_geom.setCellOptions(N|F);
-	writer_geom.setDataOptions(N|F);
-	writer_geom.write("LevelSet2D_Test1_Result.vtk");
-
-	ASSERT_EQ(LevelSet2D::SUCCESS, result);
-}
-*/
-
-TEST(LevelSet2DTestClass, LevelSet2D_Test2)
 {
 	// WE READ
 	gmds::Mesh m(gmds::MeshModel(gmds::DIM3|gmds::F|gmds::N|gmds::E| gmds::N2E|
@@ -85,44 +40,27 @@ TEST(LevelSet2DTestClass, LevelSet2D_Test2)
 	std::vector<TCellID> bnd_node_ids;
 	bnd_op.getBoundaryNodes(bnd_node_ids);
 
-	/*
-	// Seeking the ids on the front we want to advance
-	// OPTION 1 : on stocke les id des noeuds du front dans un vecteur dynamique
-	// Mais je ne sais pas pourquoi, ça ne fonctionne pas
-	std::vector<TCellID> front_nodes_Ids;
+	int markFrontNodes = m.newMark<gmds::Node>();
 	for(auto id:bnd_node_ids){
 		Node n = m.get<Node>(id);
 		double coord_y = n.Y() ;
 		if (coord_y == 0) {
 			// For the square test case, the front to advance is the boundary where y=0
-			front_nodes_Ids.push_back(n.id());
-			std::cout << "Noeud sur le front à avancé :" << id << std::endl;
-		}
-	}
-	*/
-
-
-	// OPTION 2 : j'aime moins parce qu'au lieu de stocker uniquement les id qu'on souhaite dans un vecteur, on a un entier
-	// défini à chaque noeud
-	Variable<int>* front_nodes_Ids = m.newVariable<int,GMDS_NODE>("constraint");
-	for(auto id:bnd_node_ids){
-		Node n = m.get<Node>(id);
-		double coord_y = n.Y() ;
-		if (coord_y == 0) {
-			// For the square test case, the front to advance is the boundary where y=0
-			front_nodes_Ids->value(id) = 1;
-			std::cout << "Noeud sur le front à avancé :" << id << std::endl;
+			m.mark<Node>(id,markFrontNodes);
+			std::cout << "Noeud sur le front à avancer :" << id << std::endl;
 		}
 	}
 
-
-	LevelSet2D ls(&m, front_nodes_Ids);
+	LevelSet2D ls(&m, markFrontNodes);
 	ls.execute();
+
+	m.unmarkAll<Node>(markFrontNodes);
+	m.freeMark<Node>(markFrontNodes);
 
 	gmds::VTKWriter vtkWriter(&ioService);
 	vtkWriter.setCellOptions(gmds::N|gmds::F);
 	vtkWriter.setDataOptions(gmds::N|gmds::F);
-	vtkWriter.write("LevelSet2D_Test2_Result.vtk");
+	vtkWriter.write("LevelSet2D_Test1_Result.vtk");
 
 	ASSERT_TRUE(true);
 }
