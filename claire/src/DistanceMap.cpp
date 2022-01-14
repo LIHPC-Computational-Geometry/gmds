2cb8859b9c6ab9c9d4c936b33c1d47b65ebc5dbd
Claire Ordi Pro <claireroche47@gmail.com>
2022-01-14 13:09:45 +0100

    Ajout de tests pour la classe DistanceMap.

--- claire/src/DistanceMap.cpp
+++ claire/src/DistanceMap.cpp
@@ -25,7 +25,30 @@ void DistanceMap::add(double v0, TCellID n_id){
 /*-------------------------------------------------------------------*/
 void DistanceMap::remove(double v0, TCellID n_id){
 
-	std::remove(m_map[v0].begin(),m_map[v0].end(),n_id);
+	// Cette manière n'a pas l'air de fonctionner
+	//std::remove(m_map[v0].begin(),m_map[v0].end(),n_id);
+
+	// Option non optimisée, il faudrait un while pour arrêter le parcours
+	// lorsque l'indice est trouvé
+	// ---
+	int indice(-1);
+	int increm(0);
+	/*
+	for (auto i:m_map[v0]){
+		if (m_map[v0][i]==n_id){
+			indice = i;
+		}
+	} */
+
+	while (increm < m_map[v0].size() && indice == -1) {
+		if (m_map[v0][increm]==n_id){
+			indice = increm;
+		}
+		increm += 1;
+	}
+	// ---
+
+	m_map[v0].erase(m_map[v0].begin()+indice);
 
 	// Si le premier vecteur est nul, il faut alors retirer le couple
 	// (clé, vecteur d'id) de la map.
@@ -76,8 +99,10 @@ bool DistanceMap::check(){
 	for(auto val:m_map){
 		for(auto id:val.second) {
 			int compteur = 0;
+			//std::cout << "Noeud traité :" << id << std::endl;
 			for(auto val_2:m_map){
-				for(auto id_2:val.second) {
+				for(auto id_2:val_2.second) {
+					//std::cout << id_2 << std::endl;
 					if (id==id_2){
 						compteur +=1;
 					}
@@ -106,9 +131,6 @@ void DistanceMap::getNbrIds(double v0, int &nbr){
 	}
 };
 /*-------------------------------------------------------------------*/
-
-
-
 
 
 
--- claire/tst/DistanceMapTestSuite.h
+++ claire/tst/DistanceMapTestSuite.h
@@ -26,10 +26,43 @@ TEST(DistanceMapTestClass, DistanceMap_Test1)
 	distmap.add(1,4);
 	distmap.add(1.2,5);
 	std::cout<<distmap<<std::endl;
+
 	ASSERT_EQ(distmap(0).size(), 2);
 
+	// Test des méthodes .add et .getNbrIds
 	int nbr_ids;
 	distmap.getNbrIds(0, nbr_ids);
+	ASSERT_EQ(nbr_ids, 2);
+
+	// Test de la méthode remove
+	distmap.remove(0, 1);
+	std::cout<<distmap<<std::endl;
+	distmap.getNbrIds(0, nbr_ids);
+	ASSERT_EQ(nbr_ids, 1);
+
+	// Test de la méthode check
+	distmap.add(2,2);
+	std::cout<<distmap<<std::endl;
+	bool test = distmap.check() ;
+	ASSERT_EQ(test, false);
+
+	distmap.remove(2, 2);
+	std::cout<<distmap<<std::endl;
+	test = distmap.check() ;
+	ASSERT_EQ(test, true);
+
+	// Test de la méthode getAndRemoveFirst
+	double MinDist ;
+	TCellID MinId ;
+	distmap.getAndRemoveFirst(MinDist, MinId);
+	std::cout<<distmap<<std::endl;
+	ASSERT_EQ(MinDist, 0);
+	ASSERT_EQ(MinId, 3);
+
+	// Test de la méthode update
+	distmap.update(2, 1.2, 2);
+	std::cout<<distmap<<std::endl;
+	distmap.getNbrIds(1.2, nbr_ids);
 	ASSERT_EQ(nbr_ids, 2);
 }
 
