fdc98ac650b606a02bba3b2902497529641cf17f
Claire Ordi Pro <claireroche47@gmail.com>
2022-01-17 15:59:01 +0100

    Ajout d'un cas test pour la génération de level set avec un front séparé en deux parties.

--- claire/tst/LevelSet2DTestSuite.h
+++ claire/tst/LevelSet2DTestSuite.h
@@ -160,14 +160,18 @@ TEST(LevelSet2DTestClass, LevelSet2D_Test2)
 	LevelSet2D ls(&m, markFrontNodes);
 	ls.execute();
 
+	// Affichage de débug
+	/*
 	Node n0 = m.get<Node>(5);
 	std::vector<Edge> adjacent_edges = n0.get<Edge>() ;
-	std::cout<< "Pour le noeud 5, il y a" << adjacent_edges.size() << "arêtes adjacentes." << std::endl;
+	std::cout<< "Pour le noeud 5, il y a " << adjacent_edges.size() << " arête(s) adjacentes." << std::endl;
 
 	Variable<double> *var = m.getVariable<double,GMDS_NODE>("distance");
 	for(auto n_id:m.nodes()){
 		std::cout<<"End with "<<n_id<<" to val: "<<var->value(n_id)<<std::endl;
 	}
+	 */
+
 	m.unmarkAll<Node>(markFrontNodes);
 	m.freeMark<Node>(markFrontNodes);
 
@@ -175,6 +179,77 @@ TEST(LevelSet2DTestClass, LevelSet2D_Test2)
 	vtkWriter.setCellOptions(gmds::N|gmds::F);
 	vtkWriter.setDataOptions(gmds::N|gmds::F);
 	vtkWriter.write("LevelSet2D_Test2_Result.vtk");
+
+	ASSERT_TRUE(true);
+}
+
+
+
+
+
+
+
+TEST(LevelSet2DTestClass, LevelSet2D_Test3)
+{
+	// WE READ
+	gmds::Mesh m(gmds::MeshModel(gmds::DIM3|gmds::F|gmds::N|gmds::E| gmds::N2E|
+	                             gmds::N2F|gmds::F2N|gmds::E2N|gmds::F2E|gmds::E2F));
+
+	std::string dir(TEST_SAMPLES_DIR);
+	std::string vtk_file = dir+"/Carre_Quart_Cylindre.vtk";
+
+	gmds::IGMeshIOService ioService(&m);
+	gmds::VTKReader vtkReader(&ioService);
+	vtkReader.setCellOptions(gmds::N|gmds::F);
+	vtkReader.read(vtk_file);
+
+	gmds::MeshDoctor doc(&m);
+	doc.buildEdgesAndX2E();
+	doc.updateUpwardConnectivity();
+
+	// Noeud détruit car n'appartient pas au maillage ni à la géométrie.
+	// Il apparaît à cause de la façon dont a été généré le cas test avec GMSH.
+	m.deleteNode(5);
+
+	//Get the boundary node ids
+	BoundaryOperator2D bnd_op(&m);
+	std::vector<TCellID> bnd_node_ids;
+	bnd_op.getBoundaryNodes(bnd_node_ids);
+
+	int markFrontNodes = m.newMark<gmds::Node>();
+	for(auto id:bnd_node_ids){
+		Node n = m.get<Node>(id);
+		double coord_y = n.Y() ;
+		double coord_x = n.X() ;
+		if ( coord_x == 0 || coord_y == 0 ) {
+			// For this test case, the front to advance is the boundary where x²+y²=1
+			m.mark<Node>(id,markFrontNodes);
+			std::cout << "Noeud marqué :" << id << std::endl;
+		}
+	}
+
+	LevelSet2D ls(&m, markFrontNodes);
+	ls.execute();
+
+	// Affichage de débug
+	/*
+	Node n0 = m.get<Node>(5);
+	std::vector<Edge> adjacent_edges = n0.get<Edge>() ;
+	std::cout<< "Pour le noeud 5, il y a " << adjacent_edges.size() << " arête(s) adjacentes." << std::endl;
+
+	Variable<double> *var = m.getVariable<double,GMDS_NODE>("distance");
+	for(auto n_id:m.nodes()){
+	   std::cout<<"End with "<<n_id<<" to val: "<<var->value(n_id)<<std::endl;
+	}
+	 */
+
+	m.unmarkAll<Node>(markFrontNodes);
+	m.freeMark<Node>(markFrontNodes);
+
+	gmds::VTKWriter vtkWriter(&ioService);
+	vtkWriter.setCellOptions(gmds::N|gmds::F);
+	vtkWriter.setDataOptions(gmds::N|gmds::F);
+	vtkWriter.write("LevelSet2D_Test3_Result.vtk");
 
 	ASSERT_TRUE(true);
 }
