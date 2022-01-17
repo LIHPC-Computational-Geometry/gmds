a9f7916b0b7963620280b78e9d600b26f146b0da
Claire Ordi Pro <claireroche47@gmail.com>
2022-01-17 15:50:28 +0100

    Correction du cas test pour l'algo de level set 2D.

--- claire/tst/LevelSet2DTestSuite.h
+++ claire/tst/LevelSet2DTestSuite.h
@@ -67,6 +67,57 @@ TEST(LevelSet2DTestClass, LevelSet2D_Test1)
 
 
 
+
+TEST(LevelSet2DTestClass, LevelSet2D_Test1_Refined_1)
+{
+	// WE READ
+	gmds::Mesh m(gmds::MeshModel(gmds::DIM3|gmds::F|gmds::N|gmds::E| gmds::N2E|
+	                             gmds::N2F|gmds::F2N|gmds::E2N|gmds::F2E|gmds::E2F));
+
+	std::string dir(TEST_SAMPLES_DIR);
+	std::string vtk_file = dir+"/Carre_Refined_1.vtk";
+
+	gmds::IGMeshIOService ioService(&m);
+	gmds::VTKReader vtkReader(&ioService);
+	vtkReader.setCellOptions(gmds::N|gmds::F);
+	vtkReader.read(vtk_file);
+
+	gmds::MeshDoctor doc(&m);
+	doc.buildEdgesAndX2E();
+	doc.updateUpwardConnectivity();
+
+	//Get the boundary node ids
+	BoundaryOperator2D bnd_op(&m);
+	std::vector<TCellID> bnd_node_ids;
+	bnd_op.getBoundaryNodes(bnd_node_ids);
+
+	int markFrontNodes = m.newMark<gmds::Node>();
+	for(auto id:bnd_node_ids){
+		Node n = m.get<Node>(id);
+		double coord_y = n.Y() ;
+		if (coord_y == 0) {
+			// For the square test case, the front to advance is the boundary where y=0
+			m.mark<Node>(id,markFrontNodes);
+		}
+	}
+
+	LevelSet2D ls(&m, markFrontNodes);
+	ls.execute();
+
+	m.unmarkAll<Node>(markFrontNodes);
+	m.freeMark<Node>(markFrontNodes);
+
+	gmds::VTKWriter vtkWriter(&ioService);
+	vtkWriter.setCellOptions(gmds::N|gmds::F);
+	vtkWriter.setDataOptions(gmds::N|gmds::F);
+	vtkWriter.write("LevelSet2D_Test1_Refined_1_Result.vtk");
+
+	ASSERT_TRUE(true);
+}
+
+
+
+
 TEST(LevelSet2DTestClass, LevelSet2D_Test2)
 {
 	// WE READ
@@ -84,6 +135,10 @@ TEST(LevelSet2DTestClass, LevelSet2D_Test2)
 	gmds::MeshDoctor doc(&m);
 	doc.buildEdgesAndX2E();
 	doc.updateUpwardConnectivity();
+
+	// Noeud détruit car n'appartient pas au maillage ni à la géométrie.
+	// Il apparaît à cause de la façon dont a été généré le cas test avec GMSH.
+	m.deleteNode(5);
 
 	//Get the boundary node ids
 	BoundaryOperator2D bnd_op(&m);
@@ -105,6 +160,10 @@ TEST(LevelSet2DTestClass, LevelSet2D_Test2)
 	LevelSet2D ls(&m, markFrontNodes);
 	ls.execute();
 
+	Node n0 = m.get<Node>(5);
+	std::vector<Edge> adjacent_edges = n0.get<Edge>() ;
+	std::cout<< "Pour le noeud 5, il y a" << adjacent_edges.size() << "arêtes adjacentes." << std::endl;
+
 	Variable<double> *var = m.getVariable<double,GMDS_NODE>("distance");
 	for(auto n_id:m.nodes()){
 		std::cout<<"End with "<<n_id<<" to val: "<<var->value(n_id)<<std::endl;
@@ -115,7 +174,7 @@ TEST(LevelSet2DTestClass, LevelSet2D_Test2)
 	gmds::VTKWriter vtkWriter(&ioService);
 	vtkWriter.setCellOptions(gmds::N|gmds::F);
 	vtkWriter.setDataOptions(gmds::N|gmds::F);
-	vtkWriter.write("LevelSet2D_Test3_Result.vtk");
+	vtkWriter.write("LevelSet2D_Test2_Result.vtk");
 
 	ASSERT_TRUE(true);
 }
