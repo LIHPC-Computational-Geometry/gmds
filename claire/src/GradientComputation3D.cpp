c7cff94a43567cb236384647fc28e0efc70d8f66
Claire Ordi Pro <claireroche47@gmail.com>
2022-01-21 16:31:05 +0100

    Construction des méthodes de la classe GradientComputation3D.

--- claire/src/GradientComputation3D.cpp
+++ claire/src/GradientComputation3D.cpp
@@ -12,7 +12,7 @@ using namespace gmds;
 GradientComputation3D::GradientComputation3D(Mesh *AMesh, Variable<double>* Adistance) {
 	m_mesh = AMesh;
 	m_distance = Adistance;
-	m_gradient3D = m_mesh->newVariable<math::Vector3d ,GMDS_FACE>("gradient_3D");
+	m_gradient3D = m_mesh->newVariable<math::Vector3d ,GMDS_REGION>("gradient_3D");
 }
 
 
@@ -20,11 +20,56 @@ GradientComputation3D::GradientComputation3D(Mesh *AMesh, Variable<double>* Adis
 GradientComputation3D::STATUS GradientComputation3D::execute()
 {
 
-	for(auto face_id:m_mesh->faces()) {
-		//math::Vector3d Gradient = computeGradientOnSimpleFace(face_id);
-		//m_gradient3D->set(face_id, Gradient) ;
+	for(auto region_id:m_mesh->regions()) {
+		math::Vector3d Gradient = computeGradientOnSimpleRegion(region_id);
+		m_gradient3D->set(region_id, Gradient) ;
 	}
 
 	return GradientComputation3D::SUCCESS;
+}
+/*------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------*/
+math::Vector3d GradientComputation3D::computeGradientOnSimpleRegion(TCellID region_id){
+	math::Vector3d Gradient ;
+	Region reg = m_mesh->get<Region>(region_id);
+	std::vector<TCellID> region_nodes_ids = reg.getIDs<Node>();
+
+	double dist_n0 = m_distance->value(region_nodes_ids[0]) ;
+	double dist_n1 = m_distance->value(region_nodes_ids[1]) ;
+	double dist_n2 = m_distance->value(region_nodes_ids[2]) ;
+	double dist_n3 = m_distance->value(region_nodes_ids[3]) ;
+	double Vt = reg.volume();
+
+	Node n0 = m_mesh->get<Node>(region_nodes_ids[0]);
+	Node n1 = m_mesh->get<Node>(region_nodes_ids[1]);
+	Node n2 = m_mesh->get<Node>(region_nodes_ids[2]);
+	Node n3 = m_mesh->get<Node>(region_nodes_ids[3]);
+
+	math::Point p0 = n0.point();
+	math::Point p1 = n1.point();
+	math::Point p2 = n2.point();
+	math::Point p3 = n3.point();
+
+	math::Vector3d Vec_loc_1;
+	math::Vector3d Vec_loc_2;
+
+	Vec_loc_1 = p0-p2 ;
+	Vec_loc_1 = p3-p2 ;
+	math::Vector3d Vec_1 = Vec_loc_1.cross(Vec_loc_2);
+
+	Vec_loc_1 = p0-p3 ;
+	Vec_loc_1 = p1-p3 ;
+	math::Vector3d Vec_2 = Vec_loc_1.cross(Vec_loc_2);
+
+	Vec_loc_1 = p2-p0 ;
+	Vec_loc_1 = p1-p0 ;
+	math::Vector3d Vec_3 = Vec_loc_1.cross(Vec_loc_2);
+
+	Gradient = Vec_1*(dist_n1-dist_n0)/(2.0*Vt) + Vec_2*(dist_n2-dist_n0)/(2.0*Vt) + Vec_3*(dist_n3-dist_n0)/(2.0*Vt) ;
+
+	return Gradient;
 }
 /*------------------------------------------------------------------------*/
