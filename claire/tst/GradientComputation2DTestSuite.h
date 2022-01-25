ac95cf0daad28b6314ee9ca21834a19ba4903f33
Claire Ordi Pro <claireroche47@gmail.com>
2022-01-25 11:36:26 +0100

    Ajout de cas tests pour la classe GradientComputation2D.

--- claire/tst/GradientComputation2DTestSuite.h
+++ claire/tst/GradientComputation2DTestSuite.h
@@ -137,3 +137,68 @@ TEST(GradientComputation2DTestClass, GradientComputation2D_Test2)
 
 	ASSERT_TRUE(true);
 }
+
+
+
+
+
+TEST(GradientComputation2DTestClass, GradientComputation2D_Test3)
+{
+	// WE READ
+	gmds::Mesh m(gmds::MeshModel(gmds::DIM3|gmds::F|gmds::N|gmds::E| gmds::N2E|
+	                             gmds::N2F|gmds::F2N|gmds::E2N|gmds::F2E|gmds::E2F));
+
+	std::string dir(TEST_SAMPLES_DIR);
+	std::string vtk_file = dir+"/Carre_Quart_Cylindre_maxsize_0.1.vtk";
+
+	gmds::IGMeshIOService ioService(&m);
+	gmds::VTKReader vtkReader(&ioService);
+	vtkReader.setCellOptions(gmds::N|gmds::F);
+	vtkReader.read(vtk_file);
+
+	gmds::MeshDoctor doc(&m);
+	doc.buildEdgesAndX2E();
+	doc.updateUpwardConnectivity();
+
+	// Noeud détruit car n'appartient pas au maillage ni à la géométrie.
+	// Il apparaît à cause de la façon dont a été généré le cas test avec GMSH.
+	m.deleteNode(5);
+
+	//Get the boundary node ids
+	BoundaryOperator2D bnd_op(&m);
+	std::vector<TCellID> bnd_node_ids;
+	bnd_op.getBoundaryNodes(bnd_node_ids);
+
+	int markFrontNodesInt = m.newMark<gmds::Node>();
+	int markFrontNodesOut = m.newMark<gmds::Node>();
+	for(auto id:bnd_node_ids){
+		Node n = m.get<Node>(id);
+		double coord_y = n.Y() ;
+		double coord_x = n.X() ;
+		if ( sqrt( pow(coord_x,2) + pow(coord_y,2)) == 1) {
+			// For this test case, the front to advance is the boundary where x²+y²=1
+			m.mark<Node>(id,markFrontNodesInt);
+		}
+		else if (coord_x == -2 || coord_y == 2) {
+			m.mark<Node>(id,markFrontNodesOut);
+		}
+	}
+
+	LevelSetFromIntToOut lsCombined(&m, markFrontNodesInt, markFrontNodesOut);
+	lsCombined.execute();
+
+	m.unmarkAll<Node>(markFrontNodesInt);
+	m.freeMark<Node>(markFrontNodesInt);
+	m.unmarkAll<Node>(markFrontNodesOut);
+	m.freeMark<Node>(markFrontNodesOut);
+
+	GradientComputation2D grad2D(&m, m.getVariable<double,GMDS_NODE>("distance_combined"));
+	grad2D.execute();
+
+	gmds::VTKWriter vtkWriter(&ioService);
+	vtkWriter.setCellOptions(gmds::N|gmds::F);
+	vtkWriter.setDataOptions(gmds::N|gmds::F);
+	vtkWriter.write("GradientComputation2D_Test3_Result.vtk");
+
+	ASSERT_TRUE(true);
+}
