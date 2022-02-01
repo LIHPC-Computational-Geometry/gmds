9f6e4ef76a6c92ba651955f657707539f1a709a2
Claire Ordi Pro <claireroche47@gmail.com>
2022-02-01 12:22:14 +0100

    Ajout de la classe pour suivre un point suivant un champ de gradient définit aux sommets du maillage.

--- claire/src/PointFollowingVectorFieldOnNodes.cpp
+++ claire/src/PointFollowingVectorFieldOnNodes.cpp
@@ -1,32 +1,34 @@
 //
-// Created by rochec on 26/01/2022.
+// Created by rochec on 01/02/2022.
 //
+
 /*------------------------------------------------------------------------*/
-#include <gmds/claire/PointFollowingVectorField2D.h>
+#include <gmds/claire/PointFollowingVectorFieldOnNodes.h>
 #include <limits>
 /*------------------------------------------------------------------------*/
 using namespace gmds;
 /*------------------------------------------------------------------------*/
 
-PointFollowingVectorField2D::PointFollowingVectorField2D(Mesh *AMesh, math::Point A_Pstart, double A_distance, Variable<math::Vector3d>* A_gradient2D) {
+PointFollowingVectorFieldOnNodes::PointFollowingVectorFieldOnNodes(Mesh *AMesh, math::Point A_Pstart, double A_distance, Variable<math::Vector3d>* A_gradient) {
 	m_mesh = AMesh;
 	m_Pstart = A_Pstart;
 	m_distance = A_distance;
-	m_gradient2D = A_gradient2D;
+	m_gradient = A_gradient;
 	m_discrete_path.push_back(m_Pstart);
 }
 
 
 /*------------------------------------------------------------------------*/
-PointFollowingVectorField2D::STATUS PointFollowingVectorField2D::execute()
+PointFollowingVectorFieldOnNodes::STATUS PointFollowingVectorFieldOnNodes::execute()
 {
 	m_Pend = m_Pstart;
 	double minLenght = minEdgeLenght();
 	std::cout << "min : " << minLenght << std::endl;
 
 	while (m_distance != 0){
-		TCellID pointFace_id = inWhichTriangle(m_Pend) ;
-		math::Vector3d Grad_local = m_gradient2D->value(pointFace_id) ;
+		//TCellID pointFace_id = inWhichTriangle(m_Pend) ;
+		TCellID closest_node_id = closestNode(m_Pend);
+		math::Vector3d Grad_local = m_gradient->value(closest_node_id) ;
 		if (m_distance >= minLenght){
 			m_Pend = m_Pend + minLenght*Grad_local;
 			//m_distance = m_distance - Grad_local.norm();
@@ -42,67 +44,29 @@ PointFollowingVectorField2D::STATUS PointFollowingVectorField2D::execute()
 
 	std::cout << "Point final : " << m_Pend << std::endl;
 
+	// Ecriture du chemin suivi
 	writeDiscretePathInVTK();
 
-	return PointFollowingVectorField2D::SUCCESS;
+	return PointFollowingVectorFieldOnNodes::SUCCESS;
 }
 /*------------------------------------------------------------------------*/
 
 
 
-
-
 /*------------------------------------------------------------------------*/
-bool PointFollowingVectorField2D::isInTriangle(TCellID face_id, math::Point M){
-	bool isInFace(false);
-	Face face = m_mesh->get<Face>(face_id);
-	std::vector<TCellID> face_nodes_ids = face.getIDs<Node>();
-	TCellID vi_id = face_nodes_ids[0];
-	TCellID vj_id = face_nodes_ids[2];
-	TCellID vk_id = face_nodes_ids[1];
-
-	Node vi = m_mesh->get<Node>(vi_id);
-	Node vj = m_mesh->get<Node>(vj_id);
-	Node vk = m_mesh->get<Node>(vk_id);
-
-	math::Point vi_coord = vi.point();
-	math::Point vj_coord = vj.point();
-	math::Point vk_coord = vk.point();
-
-	math::Vector3d vij = vj_coord-vi_coord ;
-	math::Vector3d vjk = vk_coord-vj_coord ;
-	math::Vector3d vki = vi_coord-vk_coord ;
-	math::Vector3d viM = M-vi_coord ;
-	math::Vector3d vjM = M-vj_coord ;
-	math::Vector3d vkM = M-vk_coord ;
-
-	double d1 = ( vij.cross(viM) ).dot( viM.cross(-vki) ) ;
-	double d2 = ( -vij.cross(vjM) ).dot( vjM.cross(vjk) ) ;
-	double d3 = ( vki.cross(vkM) ).dot( vkM.cross(-vjk) ) ;
-
-	if (d1 >= 0 && d2 >= 0 && d3 >= 0) {
-		isInFace = true;
-	}
-	return isInFace;
-}
-/*------------------------------------------------------------------------*/
-
-
-
-
-/*------------------------------------------------------------------------*/
-TCellID PointFollowingVectorField2D::inWhichTriangle(math::Point M){
-	TCellID face_id;
-	bool isInFace(false);
-	for (auto f_id:m_mesh->faces()){
-		if(!isInFace){
-			isInFace = isInTriangle(f_id, M);
-			if(isInFace){
-				face_id = f_id;
-			}
+TCellID PointFollowingVectorFieldOnNodes::closestNode(math::Point M){
+	TCellID closest_node_id;
+	double min_norme(std::numeric_limits<double>::max());
+	for (auto n_id:m_mesh->nodes()){
+		Node n = m_mesh->get<Node>(n_id);
+		math::Vector3d Vec = M-n.point() ;
+		double norme = Vec.norm() ;
+		if(norme < min_norme){
+			min_norme = norme;
+			closest_node_id = n_id;
 		}
 	}
-	return face_id;
+	return closest_node_id;
 }
 /*------------------------------------------------------------------------*/
 
@@ -110,7 +74,7 @@ TCellID PointFollowingVectorField2D::inWhichTriangle(math::Point M){
 
 
 /*------------------------------------------------------------------------*/
-double PointFollowingVectorField2D::minEdgeLenght(){
+double PointFollowingVectorFieldOnNodes::minEdgeLenght(){
 	// Initialisation avec une arête prise au hasard. Pb : si l'arête 0 a été
 	// retirée
 	Edge edge_0 = m_mesh->get<Edge>(0);
@@ -131,7 +95,7 @@ double PointFollowingVectorField2D::minEdgeLenght(){
 
 
 /*------------------------------------------------------------------------*/
-void PointFollowingVectorField2D::writeDiscretePathInVTK(){
+void PointFollowingVectorFieldOnNodes::writeDiscretePathInVTK(){
 
 	// First, we create the file where we are going to store the info
 	std::ofstream stream= std::ofstream("GMDS_discrete_path.vtk", std::ios::out);
