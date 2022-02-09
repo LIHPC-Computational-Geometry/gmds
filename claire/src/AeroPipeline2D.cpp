3be9ad728be7a6fb1df9dc86bf66dc9bcd00fcfe
Claire Ordi Pro <claireroche47@gmail.com>
2022-02-09 14:09:44 +0100

    Ajout des marques sur les noeuds en paroi et extérieur en attribut de la classe AbstractAeroPipeline.

--- claire/src/AeroPipeline2D.cpp
+++ claire/src/AeroPipeline2D.cpp
@@ -10,6 +10,8 @@ using namespace gmds;
 
 AeroPipeline2D::AeroPipeline2D(ParamsAero Aparams) {
 	m_params = Aparams;
+	m_markFrontNodesParoi = m_mesh->newMark<gmds::Node>();
+	m_markFrontNodesExt = m_mesh->newMark<gmds::Node>();
 }
 
 void AeroPipeline2D::execute(){
