70a3859a251d1b45b53425e60685eda575f242d3
Claire Ordi Pro <claireroche47@gmail.com>
2022-02-09 13:32:37 +0100

    Ajout de la classe fille AeroPipeline3D.

---
+++ claire/inc/gmds/claire/AeroPipeline3D.h
@@ -0,0 +1,33 @@
+//
+// Created by rochec on 09/02/2022.
+//
+
+#ifndef GMDS_AEROPIPELINE3D_H
+#define GMDS_AEROPIPELINE3D_H
+
+/*----------------------------------------------------------------------------*/
+#include "LIB_GMDS_CLAIRE_export.h"
+#include <gmds/claire/AbstractAeroPipeline.h>
+/*----------------------------------------------------------------------------*/
+namespace  gmds {
+/*----------------------------------------------------------------------------*/
+/** \class  AeroPipeline2D
+ *  \brief  Pipeline de génération de maillages 3D pour l'aéro.
+ */
+class LIB_GMDS_CLAIRE_API AeroPipeline3D: public AbstractAeroPipeline {
+ public:
+
+	/*------------------------------------------------------------------------*/
+	/** \brief Default constructor
+	 */
+	AeroPipeline3D();
+
+ private:
+
+
+};
+/*----------------------------------------------------------------------------*/
+}
+/*----------------------------------------------------------------------------*/
+
+#endif     // GMDS_AEROPIPELINE3D_H
