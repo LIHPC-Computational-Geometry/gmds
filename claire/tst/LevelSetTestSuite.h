838665bddee6186ef73f12b45f11c1d1b1906f6a
Claire Ordi Pro <claireroche47@gmail.com>
2022-02-15 14:46:07 +0100

    Ajout de maillages pour le cas test C1_2D dans le dossier test_samples/Aero. Ajout d'un cas test pour étude de convergence en maillage dans LevelSetTestSuite.

--- claire/tst/LevelSetTestSuite.h
+++ claire/tst/LevelSetTestSuite.h
@@ -649,3 +649,131 @@ TEST(LevelSetTestClass, LevelSetCombined_3D_Test1)
 
 
 
+/*----------------------------------------------------------------------------*/
+/*        Etude convergence en maillage LevelSetEloi/Extended                 */
+/*----------------------------------------------------------------------------*/
+
+
+TEST(LevelSetTestClass, LevelSet_Cvg_2D_Test1)
+{
+	std::string dir(TEST_SAMPLES_DIR);
+
+	std::vector<std::string> liste_fichiers_vtk;
+	liste_fichiers_vtk.push_back(dir+"/Aero/C1_2D_0.5.vtk");
+	liste_fichiers_vtk.push_back(dir+"/Aero/C1_2D_0.25.vtk");
+	liste_fichiers_vtk.push_back(dir+"/Aero/C1_2D_0.1.vtk");
+	liste_fichiers_vtk.push_back(dir+"/Aero/C1_2D_0.075.vtk");
+	liste_fichiers_vtk.push_back(dir+"/Aero/C1_2D_0.05.vtk");
+	liste_fichiers_vtk.push_back(dir+"/Aero/C1_2D_0.025.vtk");	// Trop fin
+	//liste_fichiers_vtk.push_back(dir+"/Aero/C1_2D_0.01.vtk"); // Trop fin
+
+	// First, we create the file where we are going to store the info
+	std::ofstream stream= std::ofstream("LevelSet_Etude_Convergence.table", std::ios::out);
+	//set the numerical precision (number of digits)
+	stream.precision(15);
+	//Header indicating which type of file it is
+	stream << "NbrNoeuds EloiL2 ExtendedL2 EloiMax ExtendedMax\n";
+
+	for(int i=0;i<liste_fichiers_vtk.size();i++) {
+		std::cout << "-----------------------------" << std::endl;
+		std::cout << "-----------------------------" << std::endl;
+		std::cout << "|         Maillage : " << i+1 << "      |" << std::endl;
+		std::cout << "-----------------------------" << std::endl;
+
+		// WE READ
+		gmds::Mesh m(gmds::MeshModel(gmds::DIM3|gmds::F|gmds::N|gmds::E| gmds::N2E|
+		                             gmds::N2F|gmds::F2N|gmds::E2N|gmds::F2E|gmds::E2F));
+
+		std::string vtk_file = liste_fichiers_vtk[i];
+
+		gmds::IGMeshIOService ioService(&m);
+		gmds::VTKReader vtkReader(&ioService);
+		vtkReader.setCellOptions(gmds::N | gmds::F);
+		vtkReader.read(vtk_file);
+
+		gmds::MeshDoctor doc(&m);
+		doc.buildEdgesAndX2E();
+		doc.updateUpwardConnectivity();
+
+		// Get the boundary node ids
+		BoundaryOperator2D bnd_op(&m);
+		std::vector<TCellID> bnd_node_ids;
+		bnd_op.getBoundaryNodes(bnd_node_ids);
+
+		double err = pow(10, -6);
+		Variable<double> *var_dist_Eloi = m.newVariable<double, GMDS_NODE>("GMDS_Distance_Eloi");
+		Variable<double> *var_dist_Extended = m.newVariable<double, GMDS_NODE>("GMDS_Distance_Extended");
+		Variable<double> *var_dist_Exacte = m.newVariable<double, GMDS_NODE>("GMDS_Distance_Exacte");
+
+		int markFrontNodes = m.newMark<gmds::Node>();
+		for (auto id : bnd_node_ids) {
+			Node n = m.get<Node>(id);
+			double coord_y = n.Y();
+			double coord_x = n.X();
+			if (abs(sqrt(pow(coord_x, 2) + pow(coord_y, 2)) - 0.5) < err) {
+				m.mark<Node>(id, markFrontNodes);
+			}
+		}
+		std::cout << "-> Fin initialisation bords" << std::endl;
+
+		LevelSetEloi ls_Eloi(&m, markFrontNodes, var_dist_Eloi);
+		LevelSetEloi::STATUS result_Eloi = ls_Eloi.execute();
+		std::cout << "-> Fin calcul LS Eloi" << std::endl;
+
+		LevelSetExtended ls_Extended(&m, markFrontNodes, var_dist_Extended);
+		LevelSetEloi::STATUS result_Extended = ls_Extended.execute();
+		std::cout << "-> Fin calcul LS Extended" << std::endl;
+
+		double err_nL1_Eloi(0);
+		double err_nL1_Extended(0);
+
+		double err_nL2_Eloi(0);
+		double err_nL2_Extended(0);
+
+		double err_max_Eloi(0);
+		double err_max_Extended(0);
+
+		double sum_dist_exacte(0);
+		double rayon_int(0.5);
+		int nbr_noeuds(0);
+		for (auto n_id : m.nodes()) {
+			Node n = m.get<Node>(n_id);
+			math::Point P = n.point();
+			double dist_exacte = sqrt(pow(P.X(), 2) + pow(P.Y(), 2)) - rayon_int;
+			var_dist_Exacte->set(n_id, dist_exacte);
+			sum_dist_exacte += pow(dist_exacte, 2);
+			// Erreur en norme L2
+			err_nL2_Eloi += pow(dist_exacte - var_dist_Eloi->value(n_id), 2);
+			err_nL2_Extended += pow(dist_exacte - var_dist_Extended->value(n_id), 2);
+			// Erreur max
+			double err_loc_Eloi = abs(dist_exacte - var_dist_Eloi->value(n_id)) / abs(dist_exacte);
+			double err_loc_Extended = abs(dist_exacte - var_dist_Extended->value(n_id)) / abs(dist_exacte);
+			if (err_loc_Eloi > err_max_Eloi && (var_dist_Eloi->value(n_id) > err)) {
+				err_max_Eloi = err_loc_Eloi;
+			}
+			if (err_loc_Extended > err_max_Extended && (var_dist_Extended->value(n_id) > err)) {
+				err_max_Extended = err_loc_Extended;
+			}
+			nbr_noeuds++;
+		}
+		err_nL2_Eloi = sqrt(err_nL2_Eloi / sum_dist_exacte);
+		err_nL2_Extended = sqrt(err_nL2_Extended / sum_dist_exacte);
+
+		// Ecriture des erreurs dans le fichier
+		stream << nbr_noeuds << " " <<  err_nL2_Eloi << " " << err_nL2_Extended << " " << err_max_Eloi << " " << err_max_Extended << "\n";
+
+		m.unmarkAll<Node>(markFrontNodes);
+		m.freeMark<Node>(markFrontNodes);
+
+		gmds::VTKWriter vtkWriter(&ioService);
+		vtkWriter.setCellOptions(gmds::N | gmds::F);
+		vtkWriter.setDataOptions(gmds::N | gmds::F);
+		vtkWriter.write("LevelSet_Cvg_2D_Test1_Result.vtk");
+
+		ASSERT_EQ(LevelSetEloi::SUCCESS, result_Eloi);
+		ASSERT_EQ(LevelSetExtended::SUCCESS, result_Extended);
+	}
+
+	stream.close();
+
+}
