52ad1e81aa07f68d5cdfb7663591f964df2180cf
Claire Ordi Pro <claireroche47@gmail.com>
2022-02-24 15:40:58 +0100

    Construction de la méthode pour avoir les noeuds adjacents à un noeud dans la classe AbstractAeroPipeline.

--- claire/src/AeroPipeline3D.cpp
+++ claire/src/AeroPipeline3D.cpp
@@ -8,6 +8,7 @@
 #include <gmds/claire/LeastSquaresGradientComputation.h>
 #include <gmds/ig/Mesh.h>
 #include <gmds/ig/MeshDoctor.h>
+#include <gmds/igalgo/BoundaryOperator.h>
 #include <gmds/io/IGMeshIOService.h>
 #include <gmds/io/VTKWriter.h>
 #include <gmds/io/VTKReader.h>
@@ -94,6 +95,69 @@ void AeroPipeline3D::InitialisationFronts(){
 	// Ext -> Noeuds sur la frontière extérieure
 	m_markFrontNodesParoi = m_mesh->newMark<gmds::Node>();
 	m_markFrontNodesExt = m_mesh->newMark<gmds::Node>();
+
+	//Get the boundary node ids
+	BoundaryOperator bnd_op(m_mesh);
+	std::vector<TCellID> bnd_node_ids;
+	bnd_op.getBoundaryNodes(bnd_node_ids);
+
+	int markBoundaryNodes = m_mesh->newMark<Node>();
+
+	// Initialise une marque sur les noeuds du bord
+	for (auto n_id:bnd_node_ids){
+		Node n = m_mesh->get<Node>(n_id);
+		m_mesh->mark(n, markBoundaryNodes);
+	}
+
+	// Variable qui contient la couleur du bord
+	Variable<int>* var_color_bords ;
+	var_color_bords = m_mesh->newVariable<int, GMDS_NODE>("COLOR_BORDS");
+
+	int color = 0; //Default value is 0
+	int markTreated = m_mesh->newMark<Node>();
+
+	for (auto n_id:m_mesh->nodes())
+	{
+		// Si un noeud est marqué sur le bord et qu'il n'est pas encore traité
+		if (m_mesh->isMarked<Node>(n_id, markBoundaryNodes) &&
+		    !m_mesh->isMarked<Node>(n_id, markTreated)){
+			Node n = m_mesh->get<Node>(n_id);
+
+			// Nouveau bord, nouvelle couleur
+			color++;
+			m_mesh->mark(n, markTreated);
+			(*var_color_bords)[n_id] = color;
+
+			// Propagation à tous les noeuds connexes qui sont sur le bord
+			std::vector<Node> next;
+			next.push_back(n);
+
+			while (!next.empty()) {
+				// On récupère un noeud de la liste de noeuds next à traiter
+				Node current_node = next.back();
+				next.pop_back();
+
+				// On récupère les noeuds adjacents au noeud traité
+				std::vector<Node> adjacent_nodes = AdjacentNodes(current_node) ;
+
+				for (auto n_adj: adjacent_nodes) {
+					TCellID n_adj_id = n_adj.id();
+					if(m_mesh->isMarked<Node>(n_adj_id, markBoundaryNodes) &&
+					    !m_mesh->isMarked<Node>(n_adj_id, markTreated)){
+						// Si le noeud est sur le bord et qu'il n'a pas été traité
+						// On met à jour sa couleur et on le marque comme traité
+						m_mesh->mark(n_adj, markTreated);
+						(*var_color_bords)[n_adj_id] = color;
+
+						// Ajout du noeud dans la liste des noeuds à proprager
+						next.push_back(n_adj);
+					}
+				}
+
+			}
+
+		}
+	}
 
 }
 /*------------------------------------------------------------------------*/
