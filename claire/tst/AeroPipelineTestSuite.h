0737fca2d789176056d1d79ec1ae75bcba5de15f
Claire Ordi Pro <claireroche47@gmail.com>
2022-02-24 17:02:31 +0100

    Mise en place début du pipeline AeroPipeline3D. Initialisation des marques sur le front externe et la paroi, calcul des level set et du gradient.

--- claire/tst/AeroPipelineTestSuite.h
+++ claire/tst/AeroPipelineTestSuite.h
@@ -52,7 +52,7 @@ TEST(AeroPipelineTestClass, AeroPipeline3D_Test1)
 	// le temps de mettre en place un fichier .ini
 	params_aero.dim=ParamsAero::DIM_3D;
 	std::string dir(TEST_SAMPLES_DIR);
-	params_aero.input_file=dir+"/B0.vtk";
+	params_aero.input_file=dir+"/Aero/3D/biconique.vtk";
 	params_aero.output_file="AeroPipeline3D_Test1_Result.vtk";
 	params_aero.output_dir="gmds/claire/tst/";
 
