f2edbccb861b0442fb1452ddaa2064d3f7dc9132
Claire Ordi Pro <claireroche47@gmail.com>
2022-03-22 10:40:49 +0100

    Ajout de la méthode MeshCleaner dans la classe Utils.

--- claire/tst/LevelSetTestSuite.h
+++ claire/tst/LevelSetTestSuite.h
@@ -434,7 +434,7 @@ TEST(LevelSetTestClass, LevelSet_Test_Unit_C1_3D)
 	doctor.buildEdgesAndX2E();
 	doctor.updateUpwardConnectivity();
 
-	double eps = pow(10, -6);
+	double eps = pow(10, -5);
 	double eps_2 = pow(10, -3);
 
 	int markFrontNodesInt = m.newMark<gmds::Node>();
@@ -468,14 +468,313 @@ TEST(LevelSetTestClass, LevelSet_Test_Unit_C1_3D)
 
 	/*
 	for(auto id:m.nodes()){
-	   std::cout << "ASSERT_NEAR(var_dist->value( " << id << "),  " << var_dist->value(id) << ", eps);" << std::endl;
+	 if( id%10 == 0 ){
+	   	std::cout << "ASSERT_NEAR(var_dist->value( " << id << "),  " << var_dist->value(id) << ", eps);" << std::endl;
+		}
 	}
 	 */
+
+	{
+		ASSERT_NEAR(var_dist->value( 0),  0, eps);
+		ASSERT_NEAR(var_dist->value( 10),  1.61241, eps);
+		ASSERT_NEAR(var_dist->value( 20),  1.63179, eps);
+		ASSERT_NEAR(var_dist->value( 30),  0, eps);
+		ASSERT_NEAR(var_dist->value( 40),  0, eps);
+		ASSERT_NEAR(var_dist->value( 50),  0, eps);
+		ASSERT_NEAR(var_dist->value( 60),  0, eps);
+		ASSERT_NEAR(var_dist->value( 70),  0, eps);
+		ASSERT_NEAR(var_dist->value( 80),  0, eps);
+		ASSERT_NEAR(var_dist->value( 90),  1.5962, eps);
+		ASSERT_NEAR(var_dist->value( 100),  1.65789, eps);
+		ASSERT_NEAR(var_dist->value( 110),  1.59394, eps);
+		ASSERT_NEAR(var_dist->value( 120),  1.60694, eps);
+		ASSERT_NEAR(var_dist->value( 130),  1.59233, eps);
+		ASSERT_NEAR(var_dist->value( 140),  1.5918, eps);
+		ASSERT_NEAR(var_dist->value( 150),  1.71121, eps);
+		ASSERT_NEAR(var_dist->value( 160),  1.63226, eps);
+		ASSERT_NEAR(var_dist->value( 170),  1.69452, eps);
+		ASSERT_NEAR(var_dist->value( 180),  1.62043, eps);
+		ASSERT_NEAR(var_dist->value( 190),  1.5768, eps);
+		ASSERT_NEAR(var_dist->value( 200),  1.63219, eps);
+		ASSERT_NEAR(var_dist->value( 210),  1.71106, eps);
+		ASSERT_NEAR(var_dist->value( 220),  1.67479, eps);
+		ASSERT_NEAR(var_dist->value( 230),  1.58522, eps);
+		ASSERT_NEAR(var_dist->value( 240),  1.5993, eps);
+		ASSERT_NEAR(var_dist->value( 250),  1.63547, eps);
+		ASSERT_NEAR(var_dist->value( 260),  1.64956, eps);
+		ASSERT_NEAR(var_dist->value( 270),  1.62404, eps);
+		ASSERT_NEAR(var_dist->value( 280),  1.63969, eps);
+		ASSERT_NEAR(var_dist->value( 290),  1.66174, eps);
+		ASSERT_NEAR(var_dist->value( 300),  1.64382, eps);
+		ASSERT_NEAR(var_dist->value( 310),  1.60292, eps);
+		ASSERT_NEAR(var_dist->value( 320),  1.68418, eps);
+		ASSERT_NEAR(var_dist->value( 330),  1.63033, eps);
+		ASSERT_NEAR(var_dist->value( 340),  1.63582, eps);
+		ASSERT_NEAR(var_dist->value( 350),  1.6229, eps);
+		ASSERT_NEAR(var_dist->value( 360),  1.55687, eps);
+		ASSERT_NEAR(var_dist->value( 370),  1.61603, eps);
+		ASSERT_NEAR(var_dist->value( 380),  1.58987, eps);
+		ASSERT_NEAR(var_dist->value( 390),  1.62534, eps);
+		ASSERT_NEAR(var_dist->value( 400),  1.5847, eps);
+		ASSERT_NEAR(var_dist->value( 410),  1.60256, eps);
+		ASSERT_NEAR(var_dist->value( 420),  1.63073, eps);
+		ASSERT_NEAR(var_dist->value( 430),  1.59585, eps);
+		ASSERT_NEAR(var_dist->value( 440),  1.58763, eps);
+		ASSERT_NEAR(var_dist->value( 450),  1.59926, eps);
+		ASSERT_NEAR(var_dist->value( 460),  1.66821, eps);
+		ASSERT_NEAR(var_dist->value( 470),  1.65529, eps);
+		ASSERT_NEAR(var_dist->value( 480),  1.62263, eps);
+		ASSERT_NEAR(var_dist->value( 490),  1.65274, eps);
+		ASSERT_NEAR(var_dist->value( 500),  1.56724, eps);
+		ASSERT_NEAR(var_dist->value( 510),  1.60224, eps);
+		ASSERT_NEAR(var_dist->value( 520),  1.65441, eps);
+		ASSERT_NEAR(var_dist->value( 530),  1.67438, eps);
+		ASSERT_NEAR(var_dist->value( 540),  1.61979, eps);
+		ASSERT_NEAR(var_dist->value( 550),  1.62692, eps);
+		ASSERT_NEAR(var_dist->value( 560),  1.6159, eps);
+		ASSERT_NEAR(var_dist->value( 570),  1.62201, eps);
+		ASSERT_NEAR(var_dist->value( 580),  1.60073, eps);
+		ASSERT_NEAR(var_dist->value( 590),  1.72696, eps);
+		ASSERT_NEAR(var_dist->value( 600),  1.56032, eps);
+		ASSERT_NEAR(var_dist->value( 610),  1.62631, eps);
+		ASSERT_NEAR(var_dist->value( 620),  1.70401, eps);
+		ASSERT_NEAR(var_dist->value( 630),  1.63168, eps);
+		ASSERT_NEAR(var_dist->value( 640),  1.68273, eps);
+		ASSERT_NEAR(var_dist->value( 650),  1.67028, eps);
+		ASSERT_NEAR(var_dist->value( 660),  1.56639, eps);
+		ASSERT_NEAR(var_dist->value( 670),  1.6016, eps);
+		ASSERT_NEAR(var_dist->value( 680),  1.56633, eps);
+		ASSERT_NEAR(var_dist->value( 690),  1.73682, eps);
+		ASSERT_NEAR(var_dist->value( 700),  1.74781, eps);
+		ASSERT_NEAR(var_dist->value( 710),  1.67324, eps);
+		ASSERT_NEAR(var_dist->value( 720),  1.62059, eps);
+		ASSERT_NEAR(var_dist->value( 730),  1.6386, eps);
+		ASSERT_NEAR(var_dist->value( 740),  1.56522, eps);
+		ASSERT_NEAR(var_dist->value( 750),  0.801593, eps);
+		ASSERT_NEAR(var_dist->value( 760),  0.795046, eps);
+		ASSERT_NEAR(var_dist->value( 770),  0.835696, eps);
+		ASSERT_NEAR(var_dist->value( 780),  0.992935, eps);
+		ASSERT_NEAR(var_dist->value( 790),  1.06457, eps);
+		ASSERT_NEAR(var_dist->value( 800),  1.08728, eps);
+		ASSERT_NEAR(var_dist->value( 810),  1.0638, eps);
+		ASSERT_NEAR(var_dist->value( 820),  0.504526, eps);
+		ASSERT_NEAR(var_dist->value( 830),  0.487168, eps);
+		ASSERT_NEAR(var_dist->value( 840),  1.12722, eps);
+		ASSERT_NEAR(var_dist->value( 850),  0.586665, eps);
+		ASSERT_NEAR(var_dist->value( 860),  0.446255, eps);
+		ASSERT_NEAR(var_dist->value( 870),  0.459978, eps);
+		ASSERT_NEAR(var_dist->value( 880),  0.483766, eps);
+		ASSERT_NEAR(var_dist->value( 890),  1.12859, eps);
+		ASSERT_NEAR(var_dist->value( 900),  1.13422, eps);
+		ASSERT_NEAR(var_dist->value( 910),  1.18768, eps);
+		ASSERT_NEAR(var_dist->value( 920),  1.19167, eps);
+		ASSERT_NEAR(var_dist->value( 930),  0.399557, eps);
+		ASSERT_NEAR(var_dist->value( 940),  1.1755, eps);
+		ASSERT_NEAR(var_dist->value( 950),  1.2125, eps);
+		ASSERT_NEAR(var_dist->value( 960),  1.18804, eps);
+		ASSERT_NEAR(var_dist->value( 970),  0.39235, eps);
+		ASSERT_NEAR(var_dist->value( 980),  1.16178, eps);
+		ASSERT_NEAR(var_dist->value( 990),  0.668155, eps);
+		ASSERT_NEAR(var_dist->value( 1000),  0.354938, eps);
+		ASSERT_NEAR(var_dist->value( 1010),  1.19716, eps);
+		ASSERT_NEAR(var_dist->value( 1020),  1.2251, eps);
+		ASSERT_NEAR(var_dist->value( 1030),  0.838927, eps);
+		ASSERT_NEAR(var_dist->value( 1040),  0.67041, eps);
+		ASSERT_NEAR(var_dist->value( 1050),  1.17535, eps);
+		ASSERT_NEAR(var_dist->value( 1060),  1.21023, eps);
+		ASSERT_NEAR(var_dist->value( 1070),  1.22418, eps);
+		ASSERT_NEAR(var_dist->value( 1080),  0.736147, eps);
+		ASSERT_NEAR(var_dist->value( 1090),  1.2817, eps);
+		ASSERT_NEAR(var_dist->value( 1100),  0.849743, eps);
+		ASSERT_NEAR(var_dist->value( 1110),  1.30965, eps);
+		ASSERT_NEAR(var_dist->value( 1120),  1.24458, eps);
+		ASSERT_NEAR(var_dist->value( 1130),  0.821591, eps);
+		ASSERT_NEAR(var_dist->value( 1140),  0.326307, eps);
+		ASSERT_NEAR(var_dist->value( 1150),  0.873712, eps);
+		ASSERT_NEAR(var_dist->value( 1160),  0.893291, eps);
+		ASSERT_NEAR(var_dist->value( 1170),  0.321761, eps);
+		ASSERT_NEAR(var_dist->value( 1180),  0.742257, eps);
+		ASSERT_NEAR(var_dist->value( 1190),  0.706967, eps);
+		ASSERT_NEAR(var_dist->value( 1200),  1.21535, eps);
+		ASSERT_NEAR(var_dist->value( 1210),  1.27691, eps);
+		ASSERT_NEAR(var_dist->value( 1220),  1.30376, eps);
+		ASSERT_NEAR(var_dist->value( 1230),  1.29467, eps);
+		ASSERT_NEAR(var_dist->value( 1240),  1.36071, eps);
+		ASSERT_NEAR(var_dist->value( 1250),  1.34309, eps);
+		ASSERT_NEAR(var_dist->value( 1260),  1.27825, eps);
+		ASSERT_NEAR(var_dist->value( 1270),  1.2892, eps);
+		ASSERT_NEAR(var_dist->value( 1280),  1.40077, eps);
+		ASSERT_NEAR(var_dist->value( 1290),  0.879879, eps);
+		ASSERT_NEAR(var_dist->value( 1300),  1.00754, eps);
+		ASSERT_NEAR(var_dist->value( 1310),  1.32511, eps);
+		ASSERT_NEAR(var_dist->value( 1320),  1.38266, eps);
+		ASSERT_NEAR(var_dist->value( 1330),  0.28421, eps);
+		ASSERT_NEAR(var_dist->value( 1340),  0.923614, eps);
+		ASSERT_NEAR(var_dist->value( 1350),  1.28072, eps);
+		ASSERT_NEAR(var_dist->value( 1360),  0.778665, eps);
+		ASSERT_NEAR(var_dist->value( 1370),  0.685197, eps);
+		ASSERT_NEAR(var_dist->value( 1380),  0.967138, eps);
+		ASSERT_NEAR(var_dist->value( 1390),  1.42285, eps);
+		ASSERT_NEAR(var_dist->value( 1400),  0.256751, eps);
+		ASSERT_NEAR(var_dist->value( 1410),  1.05374, eps);
+	}
 
 	LevelSetExtended lsExtended(&m, markFrontNodesInt, var_dist);
 	LevelSetExtended::STATUS resultExtended = lsExtended.execute();
 	ASSERT_EQ(LevelSetExtended::SUCCESS, resultExtended);
 
+	/*
+	for(auto id:m.nodes()){
+		if( id%10 == 0 ){
+			std::cout << "ASSERT_NEAR(var_dist->value( " << id << "),  " << var_dist->value(id) << ", eps);" << std::endl;
+		}
+	}
+	 */
+
+	{
+		ASSERT_NEAR(var_dist->value( 0),  0, eps);
+		ASSERT_NEAR(var_dist->value( 10),  1.53548, eps);
+		ASSERT_NEAR(var_dist->value( 20),  1.53032, eps);
+		ASSERT_NEAR(var_dist->value( 30),  0, eps);
+		ASSERT_NEAR(var_dist->value( 40),  0, eps);
+		ASSERT_NEAR(var_dist->value( 50),  0, eps);
+		ASSERT_NEAR(var_dist->value( 60),  0, eps);
+		ASSERT_NEAR(var_dist->value( 70),  0, eps);
+		ASSERT_NEAR(var_dist->value( 80),  0, eps);
+		ASSERT_NEAR(var_dist->value( 90),  1.54097, eps);
+		ASSERT_NEAR(var_dist->value( 100),  1.536, eps);
+		ASSERT_NEAR(var_dist->value( 110),  1.51927, eps);
+		ASSERT_NEAR(var_dist->value( 120),  1.52062, eps);
+		ASSERT_NEAR(var_dist->value( 130),  1.51842, eps);
+		ASSERT_NEAR(var_dist->value( 140),  1.50843, eps);
+		ASSERT_NEAR(var_dist->value( 150),  1.56375, eps);
+		ASSERT_NEAR(var_dist->value( 160),  1.55341, eps);
+		ASSERT_NEAR(var_dist->value( 170),  1.54642, eps);
+		ASSERT_NEAR(var_dist->value( 180),  1.54171, eps);
+		ASSERT_NEAR(var_dist->value( 190),  1.53341, eps);
+		ASSERT_NEAR(var_dist->value( 200),  1.52609, eps);
+		ASSERT_NEAR(var_dist->value( 210),  1.56359, eps);
+		ASSERT_NEAR(var_dist->value( 220),  1.53177, eps);
+		ASSERT_NEAR(var_dist->value( 230),  1.51291, eps);
+		ASSERT_NEAR(var_dist->value( 240),  1.56595, eps);
+		ASSERT_NEAR(var_dist->value( 250),  1.54097, eps);
+		ASSERT_NEAR(var_dist->value( 260),  1.54466, eps);
+		ASSERT_NEAR(var_dist->value( 270),  1.53272, eps);
+		ASSERT_NEAR(var_dist->value( 280),  1.54491, eps);
+		ASSERT_NEAR(var_dist->value( 290),  1.54771, eps);
+		ASSERT_NEAR(var_dist->value( 300),  1.50661, eps);
+		ASSERT_NEAR(var_dist->value( 310),  1.53549, eps);
+		ASSERT_NEAR(var_dist->value( 320),  1.52007, eps);
+		ASSERT_NEAR(var_dist->value( 330),  1.54585, eps);
+		ASSERT_NEAR(var_dist->value( 340),  1.51734, eps);
+		ASSERT_NEAR(var_dist->value( 350),  1.5389, eps);
+		ASSERT_NEAR(var_dist->value( 360),  1.53579, eps);
+		ASSERT_NEAR(var_dist->value( 370),  1.54084, eps);
+		ASSERT_NEAR(var_dist->value( 380),  1.54097, eps);
+		ASSERT_NEAR(var_dist->value( 390),  1.52327, eps);
+		ASSERT_NEAR(var_dist->value( 400),  1.52623, eps);
+		ASSERT_NEAR(var_dist->value( 410),  1.53683, eps);
+		ASSERT_NEAR(var_dist->value( 420),  1.5443, eps);
+		ASSERT_NEAR(var_dist->value( 430),  1.53576, eps);
+		ASSERT_NEAR(var_dist->value( 440),  1.54332, eps);
+		ASSERT_NEAR(var_dist->value( 450),  1.52772, eps);
+		ASSERT_NEAR(var_dist->value( 460),  1.5361, eps);
+		ASSERT_NEAR(var_dist->value( 470),  1.56596, eps);
+		ASSERT_NEAR(var_dist->value( 480),  1.51088, eps);
+		ASSERT_NEAR(var_dist->value( 490),  1.54447, eps);
+		ASSERT_NEAR(var_dist->value( 500),  1.54668, eps);
+		ASSERT_NEAR(var_dist->value( 510),  1.52736, eps);
+		ASSERT_NEAR(var_dist->value( 520),  1.53566, eps);
+		ASSERT_NEAR(var_dist->value( 530),  1.54158, eps);
+		ASSERT_NEAR(var_dist->value( 540),  1.52833, eps);
+		ASSERT_NEAR(var_dist->value( 550),  1.52716, eps);
+		ASSERT_NEAR(var_dist->value( 560),  1.52266, eps);
+		ASSERT_NEAR(var_dist->value( 570),  1.51141, eps);
+		ASSERT_NEAR(var_dist->value( 580),  1.53216, eps);
+		ASSERT_NEAR(var_dist->value( 590),  1.54135, eps);
+		ASSERT_NEAR(var_dist->value( 600),  1.51866, eps);
+		ASSERT_NEAR(var_dist->value( 610),  1.51926, eps);
+		ASSERT_NEAR(var_dist->value( 620),  1.52582, eps);
+		ASSERT_NEAR(var_dist->value( 630),  1.56913, eps);
+		ASSERT_NEAR(var_dist->value( 640),  1.54712, eps);
+		ASSERT_NEAR(var_dist->value( 650),  1.55872, eps);
+		ASSERT_NEAR(var_dist->value( 660),  1.54097, eps);
+		ASSERT_NEAR(var_dist->value( 670),  1.5403, eps);
+		ASSERT_NEAR(var_dist->value( 680),  1.52689, eps);
+		ASSERT_NEAR(var_dist->value( 690),  1.56117, eps);
+		ASSERT_NEAR(var_dist->value( 700),  1.56339, eps);
+		ASSERT_NEAR(var_dist->value( 710),  1.52901, eps);
+		ASSERT_NEAR(var_dist->value( 720),  1.53793, eps);
+		ASSERT_NEAR(var_dist->value( 730),  1.5576, eps);
+		ASSERT_NEAR(var_dist->value( 740),  1.51283, eps);
+		ASSERT_NEAR(var_dist->value( 750),  0.765228, eps);
+		ASSERT_NEAR(var_dist->value( 760),  0.77472, eps);
+		ASSERT_NEAR(var_dist->value( 770),  0.752711, eps);
+		ASSERT_NEAR(var_dist->value( 780),  0.925648, eps);
+		ASSERT_NEAR(var_dist->value( 790),  0.97754, eps);
+		ASSERT_NEAR(var_dist->value( 800),  0.96888, eps);
+		ASSERT_NEAR(var_dist->value( 810),  0.979349, eps);
+		ASSERT_NEAR(var_dist->value( 820),  0.478643, eps);
+		ASSERT_NEAR(var_dist->value( 830),  0.487168, eps);
+		ASSERT_NEAR(var_dist->value( 840),  1.06428, eps);
+		ASSERT_NEAR(var_dist->value( 850),  0.488837, eps);
+		ASSERT_NEAR(var_dist->value( 860),  0.446255, eps);
+		ASSERT_NEAR(var_dist->value( 870),  0.459978, eps);
+		ASSERT_NEAR(var_dist->value( 880),  0.483766, eps);
+		ASSERT_NEAR(var_dist->value( 890),  1.10488, eps);
+		ASSERT_NEAR(var_dist->value( 900),  1.10751, eps);
+		ASSERT_NEAR(var_dist->value( 910),  1.12721, eps);
+		ASSERT_NEAR(var_dist->value( 920),  1.14436, eps);
+		ASSERT_NEAR(var_dist->value( 930),  0.399557, eps);
+		ASSERT_NEAR(var_dist->value( 940),  1.12639, eps);
+		ASSERT_NEAR(var_dist->value( 950),  1.12337, eps);
+		ASSERT_NEAR(var_dist->value( 960),  1.14909, eps);
+		ASSERT_NEAR(var_dist->value( 970),  0.39235, eps);
+		ASSERT_NEAR(var_dist->value( 980),  1.12843, eps);
+		ASSERT_NEAR(var_dist->value( 990),  0.648654, eps);
+		ASSERT_NEAR(var_dist->value( 1000),  0.354938, eps);
+		ASSERT_NEAR(var_dist->value( 1010),  1.11989, eps);
+		ASSERT_NEAR(var_dist->value( 1020),  1.19771, eps);
+		ASSERT_NEAR(var_dist->value( 1030),  0.773162, eps);
+		ASSERT_NEAR(var_dist->value( 1040),  0.64068, eps);
+		ASSERT_NEAR(var_dist->value( 1050),  1.1583, eps);
+		ASSERT_NEAR(var_dist->value( 1060),  1.18579, eps);
+		ASSERT_NEAR(var_dist->value( 1070),  1.20493, eps);
+		ASSERT_NEAR(var_dist->value( 1080),  0.701456, eps);
+		ASSERT_NEAR(var_dist->value( 1090),  1.16043, eps);
+		ASSERT_NEAR(var_dist->value( 1100),  0.791094, eps);
+		ASSERT_NEAR(var_dist->value( 1110),  1.21254, eps);
+		ASSERT_NEAR(var_dist->value( 1120),  1.2, eps);
+		ASSERT_NEAR(var_dist->value( 1130),  0.816124, eps);
+		ASSERT_NEAR(var_dist->value( 1140),  0.326307, eps);
+		ASSERT_NEAR(var_dist->value( 1150),  0.811776, eps);
+		ASSERT_NEAR(var_dist->value( 1160),  0.826004, eps);
+		ASSERT_NEAR(var_dist->value( 1170),  0.321761, eps);
+		ASSERT_NEAR(var_dist->value( 1180),  0.708221, eps);
+		ASSERT_NEAR(var_dist->value( 1190),  0.586593, eps);
+		ASSERT_NEAR(var_dist->value( 1200),  1.18588, eps);
+		ASSERT_NEAR(var_dist->value( 1210),  1.19538, eps);
+		ASSERT_NEAR(var_dist->value( 1220),  1.23188, eps);
+		ASSERT_NEAR(var_dist->value( 1230),  1.21293, eps);
+		ASSERT_NEAR(var_dist->value( 1240),  1.25122, eps);
+		ASSERT_NEAR(var_dist->value( 1250),  1.24298, eps);
+		ASSERT_NEAR(var_dist->value( 1260),  1.23037, eps);
+		ASSERT_NEAR(var_dist->value( 1270),  1.21764, eps);
+		ASSERT_NEAR(var_dist->value( 1280),  1.25552, eps);
+		ASSERT_NEAR(var_dist->value( 1290),  0.804332, eps);
+		ASSERT_NEAR(var_dist->value( 1300),  0.943324, eps);
+		ASSERT_NEAR(var_dist->value( 1310),  1.25665, eps);
+		ASSERT_NEAR(var_dist->value( 1320),  1.25354, eps);
+		ASSERT_NEAR(var_dist->value( 1330),  0.28421, eps);
+		ASSERT_NEAR(var_dist->value( 1340),  0.815433, eps);
+		ASSERT_NEAR(var_dist->value( 1350),  1.24015, eps);
+		ASSERT_NEAR(var_dist->value( 1360),  0.67796, eps);
+		ASSERT_NEAR(var_dist->value( 1370),  0.546801, eps);
+		ASSERT_NEAR(var_dist->value( 1380),  0.902189, eps);
+		ASSERT_NEAR(var_dist->value( 1390),  1.24407, eps);
+		ASSERT_NEAR(var_dist->value( 1400),  0.256751, eps);
+		ASSERT_NEAR(var_dist->value( 1410),  0.945214, eps);
+	}
 
 	LevelSetCombined lsCombined(&m, markFrontNodesInt, markFrontNodesOut,
 	                            var_dist,
@@ -484,6 +783,14 @@ TEST(LevelSetTestClass, LevelSet_Test_Unit_C1_3D)
 
 	LevelSetCombined::STATUS resultComb = lsCombined.execute();
 	ASSERT_EQ(LevelSetCombined::SUCCESS, resultComb);
+
+	/*
+	for(auto id:m.nodes()){
+	 if( id%10 == 0 ){
+	      std::cout << "ASSERT_NEAR(var_dist->value( " << id << "),  " << var_dist->value(id) << ", eps);" << std::endl;
+	   }
+	}
+	 */
 
 	m.unmarkAll<Node>(markFrontNodesInt);
 	m.freeMark<Node>(markFrontNodesInt);
