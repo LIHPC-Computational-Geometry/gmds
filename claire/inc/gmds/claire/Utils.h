719f6b0252f995e76fa49f0567921a4924204e9f
Claire Ordi Pro <claireroche47@gmail.com>
2022-03-22 09:16:25 +0100

    Ajout de la classe Utils pour mettre des petites méthodes.

---
+++ claire/inc/gmds/claire/Utils.h
@@ -0,0 +1,53 @@
+//
+// Created by rochec on 21/03/2022.
+//
+
+#ifndef GMDS_UTILS_H
+#define GMDS_UTILS_H
+/*----------------------------------------------------------------------------*/
+// gmds file headers
+/*----------------------------------------------------------------------------*/
+#include "LIB_GMDS_CLAIRE_export.h"
+#include "gmds/ig/Mesh.h"
+/*----------------------------------------------------------------------------*/
+namespace gmds {
+/*----------------------------------------------------------------------------*/
+namespace math {
+/*----------------------------------------------------------------------------*/
+/** \class Utils
+ *  \brief
+ **/
+class LIB_GMDS_CLAIRE_API Utils {
+ public:
+	/*------------------------------------------------------------------------*/
+	/** \brief  Compute the distance between two nodes given the ids
+         *
+         * \param[in] AMesh the mesh
+         * \param[in] n0_id first node id
+         * \param[in] n1_id second node id
+         *
+         * \return  the distance between the nodes n0 and n1
+	 */
+	static double distFromNodeIds(Mesh *AMesh, TCellID n0_id, TCellID n1_id);
+
+	/*------------------------------------------------------------------------*/
+	/** \brief  Return the common edge between 2 points if it exists, NullID
+	 		* sinon
+         *
+         * \param[in] n0_id first node id
+         * \param[in] n1_id second node id
+         *
+         * \return  the edge between the nodes n0 and n1
+	 */
+	static TCellID CommonEdge(Mesh *AMesh, TCellID n0_id, TCellID n1_id);
+
+	/*------------------------------------------------------------------------*/
+
+};
+/*----------------------------------------------------------------------------*/
+}  // namespace math
+/*----------------------------------------------------------------------------*/
+}  // namespace gmds
+/*----------------------------------------------------------------------------*/
+
+#endif     // GMDS_UTILS_H
