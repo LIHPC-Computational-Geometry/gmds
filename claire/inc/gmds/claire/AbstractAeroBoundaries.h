b5b2c55b48f263fb3a7a72e7a82d950df48bc8ea
Claire Ordi Pro <claireroche47@gmail.com>
2022-03-23 16:20:17 +0100

    Ajout de la classe abstraite AbstractAeroBoundaries pour traiter les bords/fronts du maillage triangulaire aéro, les caractériser, etc. Ajout de la classe fille AeroBoundaries_2D pour traiter les bords 2D.

---
+++ claire/inc/gmds/claire/AbstractAeroBoundaries.h
@@ -0,0 +1,65 @@
+//
+// Created by rochec on 23/03/2022.
+//
+
+#ifndef GMDS_ABSTRACTAEROBOUNDARIES_H
+#define GMDS_ABSTRACTAEROBOUNDARIES_H
+
+/*----------------------------------------------------------------------------*/
+#include "LIB_GMDS_CLAIRE_export.h"
+#include <gmds/ig/Mesh.h>
+#include <string>
+#include <map>
+#include <fstream>
+/*----------------------------------------------------------------------------*/
+namespace gmds{
+/*---------------------------------------------------------------------------*/
+// Frame File Headers
+/*----------------------------------------------------------------------------*/
+/** \class  AbstractAeroBoundaries
+ *  \brief  Classe abstraite pour les informations relatives aux frontières
+ *  dans le cadre de l'aéro.
+ */
+class LIB_GMDS_CLAIRE_API AbstractAeroBoundaries{
+
+ public:
+	/*--------------------------------------------------------------------*/
+	/** @enum  Status code for executing algorithms
+	 */
+	typedef enum {
+		FAIL,
+		SUCCESS
+	} STATUS;
+	/*--------------------------------------------------------------------*/
+	/** @brief Constructor.
+         *  @param
+	 */
+	AbstractAeroBoundaries(Mesh *AMesh);
+	/*-------------------------------------------------------------------*/
+	/** \brief Function to be called for mesh generation
+	 */
+	virtual void execute()=0;
+	/*-------------------------------------------------------------------*/
+
+ protected:
+	/** pointer to mesh we work on */
+	Mesh* m_mesh;
+	/** Nombre de bords distincts de bords */
+	int m_nbrBords;
+	/** Nombre de bords distincts sur la paroi */
+	int m_nbrBordsParoi;
+	/** Couleur qui correspond au bord extérieur */
+	int m_bigest_color;
+	/** Colorie chaque bord d'une couleur != */
+	Variable<int>* m_var_color_bords ;
+	/** Marque sur les noeuds de la paroi */
+	int m_markFrontNodesParoi;
+	/** Marque sur les noeuds de la frontière ext */
+	int m_markFrontNodesExt;
+
+};
+/*----------------------------------------------------------------------------*/
+}
+/*----------------------------------------------------------------------------*/
+
+#endif     // GMDS_ABSTRACTAEROBOUNDARIES_H
---
+++ claire/inc/gmds/claire/AeroBoundaries_2D.h
@@ -0,0 +1,36 @@
+//
+// Created by rochec on 23/03/2022.
+//
+
+#ifndef GMDS_AEROBOUNDARIES_2D_H
+#define GMDS_AEROBOUNDARIES_2D_H
+
+/*----------------------------------------------------------------------------*/
+#include "LIB_GMDS_CLAIRE_export.h"
+#include <gmds/claire/AbstractAeroBoundaries.h>
+/*----------------------------------------------------------------------------*/
+namespace  gmds {
+/*----------------------------------------------------------------------------*/
+/** \class  AeroBoundaries_2D
+ *  \brief  Caractéristiques des frontières du maillage 2D.
+ */
+class LIB_GMDS_CLAIRE_API AeroBoundaries_2D: public AbstractAeroBoundaries {
+ public:
+
+	/*------------------------------------------------------------------------*/
+	/** \brief Default constructor
+	 */
+	AeroBoundaries_2D(Mesh *AMesh);
+
+	/*------------------------------------------------------------------------*/
+	/** \brief Function to be called for mesh generation
+	 */
+	virtual void execute();
+	/*------------------------------------------------------------------------*/
+
+};
+/*----------------------------------------------------------------------------*/
+}
+/*----------------------------------------------------------------------------*/
+
+#endif     // GMDS_AEROBOUNDARIES_2D_H
