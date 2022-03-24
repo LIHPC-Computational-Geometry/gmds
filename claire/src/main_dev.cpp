56d446a27902981446479ec0e15ac1260f8191bb
Claire Ordi Pro <claireroche47@gmail.com>
2022-03-24 13:32:04 +0100

    Ajout d'un main pour les dev (conseil François).

---
+++ claire/src/main_dev.cpp
@@ -0,0 +1,16 @@
+//
+// Created by rochec on 24/03/2022.
+//
+
+#include <gmds/io/IGMeshIOService.h>
+#include <gmds/io/VTKReader.h>
+#include <iostream>
+/*----------------------------------------------------------------------------*/
+using namespace gmds;
+
+/*----------------------------------------------------------------------------*/
+int main(int argc, char* argv[])
+{
+	std::cout << "=== TEST ====" << std::endl;
+
+}
