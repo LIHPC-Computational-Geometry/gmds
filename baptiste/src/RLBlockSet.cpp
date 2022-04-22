16052e8d0c46fa535bf36056502bd3675daf3585
bptstbn <bony.baptiste@orange.fr>
2022-04-22 16:41:30 +0200

    Initial commit

---
+++ baptiste/src/RLBlockSet.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Baptiste Bony on 22/04/22.
+//
+
+#include </home/bonyb/Documents/GitHub/gmds/baptiste/inc/gmds/baptiste/RLBlockSet.h>
+using namespace gmds;
+
+
+void setFrame(gmds::Node xMin, gmds::Node yMin, gmds::Node xMax, gmds::Node yMax)
+{
+
+}
+
+int getNumberOfBlocks()
+{
+
+}
+
+void deleteBlock(const int faceID)
+{
+
+}
