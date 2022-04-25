59d89c61651ad3546a96fb801210d727a7410369
bptstbn <bony.baptiste@orange.fr>
2022-04-25 10:55:32 +0200

    Start setFrame

--- baptiste/src/RLBlockSet.cpp
+++ baptiste/src/RLBlockSet.cpp
@@ -3,12 +3,31 @@
 //
 
 #include </home/bonyb/Documents/GitHub/gmds/baptiste/inc/gmds/baptiste/RLBlockSet.h>
+#include <gmds/utils/Variable.h>
+
 using namespace gmds;
 
-
-void setFrame(gmds::Node xMin, gmds::Node yMin, gmds::Node xMax, gmds::Node yMax)
+RLBlockSet::RLBlockSet(gmds::Mesh mesh)
 {
+    m_mesh = mesh;
+}
 
+void createFourNodes(double xA, double yA, double xB, double yB);
+{
+    std::vector<double> AX = {xA, xB, xA, xB};
+    std::vector<double> AY = {yA, yA, yB, yB};
+    std::vector<double> AZ = {0, 0, 0, 0};
+    m_mesh.createNodes(AX, AY, AZ);
+}
+
+void setFrame(double xMin, double yMin, double xMax, double yMax)
+{
+    double xPitch = abs(xMax - xMin)/3;
+    double yPitch = abs(yMax - yMin)/3;
+    double x1 = xMin + xPitch;
+    double x2 = xMin + 2*xPitch;
+    double y1 = yMin + yPitch;
+    double y2 = yMin + 2*yPitch;
 }
 
 int getNumberOfBlocks()
