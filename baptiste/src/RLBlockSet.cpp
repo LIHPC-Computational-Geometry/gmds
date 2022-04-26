37fae80ad52e1b7e7dfc9d4f6e96d5c846bf7e64
bptstbn <bony.baptiste@orange.fr>
2022-04-26 09:56:09 +0200

    Add LinearSpacedArray

--- baptiste/src/RLBlockSet.cpp
+++ baptiste/src/RLBlockSet.cpp
@@ -12,32 +12,30 @@ using namespace gmds;
 RLBlockSet::RLBlockSet(Mesh *AMesh)
 	:m_mesh(*AMesh){;}
 
+std::vector<double> LinearSpacedArray(double a, double b, std::size_t N)
+{
+	double h = (b - a) / static_cast<double>(N-1);
+	std::vector<double> v(N);
+	std::vector<double>::iterator x;
+	double val;
+	for (x = v.begin(), val = a; x != v.end(); ++x, val += h)
+	{
+		*x = val;
+	}
+	return v;
+}
 
 void RLBlockSet::createFourNodes(double xA, double yA, double xB, double yB)
 {
-    std::vector<double> AX = {xA, xB, xA, xB};
-    std::vector<double> AY = {yA, yA, yB, yB};
-    std::vector<double> AZ = {0, 0, 0, 0};
-	 Node n = m_mesh.newNode(xA, yA);
+	std::vector<double> AX = {xA, xB, xA, xB};
+   std::vector<double> AY = {yA, yA, yB, yB};
+   std::vector<double> AZ = {0, 0, 0, 0};
+	Node n = m_mesh.newNode(xA, yA);
 }
 
-void RLBlockSet::setFrame(double xMin, double yMin, double xMax, double yMax)
+void RLBlockSet::setFrame(double xMin, double yMin, double xMax, double yMax, int nX, int nY)
 {
-    double xPitch = abs(xMax - xMin)/3;
-    double yPitch = abs(yMax - yMin)/3;
-    double x1 = xMin + xPitch;
-    double x2 = xMin + 2*xPitch;
-    double y1 = yMin + yPitch;
-    double y2 = yMin + 2*yPitch;
-    createFourNodes(xMin, yMin, x1, y1);
-    createFourNodes(x1, yMin, x2, y1);
-    createFourNodes(x2, yMin, xMax, y1);
-    createFourNodes(xMin, y1, x1, y2);
-    createFourNodes(x1, y1, x2, y2);
-    createFourNodes(x2, y1, xMax, y2);
-    createFourNodes(xMin, y2, x1, yMax);
-    createFourNodes(x1, y2, x2, yMax);
-    createFourNodes(x2, y2, xMax, yMax);
+
 }
 
 int RLBlockSet::getNumberOfBlocks()
