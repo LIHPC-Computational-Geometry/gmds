ab3648bfd88218aa15d1c03246fe83f4e74199e7
bptstbn <bony.baptiste@orange.fr>
2022-04-27 15:13:27 +0200

    Implement deleteBlock & start visualization

--- baptiste/src/main.cpp
+++ baptiste/src/main.cpp
@@ -2,14 +2,14 @@
 #include "gmds/baptiste/RLBlockSet.h"
 #include "gmds/ig/Mesh.h"
 #include "gmds/io/IGMeshIOService.h"
-
+#include "gmds/io/VTKWriter.h"
 
 //using namespace std;
 using namespace gmds;
 
 int main()
 {
-	std::cout<<"Hello World" << "\n";
+	std::cout << "Hello World" << "\n";
 
 	RLBlockSet blockSet(MeshModel(DIM3|F|N|F2N|N2F));
 
@@ -21,7 +21,27 @@ int main()
 	blockSet.setFrame(xMin, yMin, xMax, yMax);
 
 	int nbFaces = blockSet.getNumberOfBlocks();
-	std::cout<<nbFaces;
+	std::cout << "Number of blocks : " << nbFaces << "\n";
+
+	for(auto faceID: blockSet.m_mesh.faces())
+	{
+		std::cout << faceID << "\n";
+		if (faceID > 4)
+		{
+			blockSet.deleteBlock(faceID);
+			std::cout << "Face " << faceID << " deleted" << "\n";
+		}
+	}
+
+	nbFaces = blockSet.getNumberOfBlocks();
+	std::cout << "Number of blocks : " << nbFaces << "\n";
+
+	// Save Mesh
+	IGMeshIOService ioService(&blockSet.m_mesh);
+	VTKWriter vtkWriter(&ioService);
+	vtkWriter.setCellOptions(gmds::N|gmds::F);
+	vtkWriter.setDataOptions(gmds::N|gmds::F);
+	vtkWriter.write("MyBlockSet.vtk");
 
 	return 0;
 }
