1f1266c2e04516a2a932ec351aa6efeb9af44bf4
nicolas le goff <nicolas.le-goff@cea.fr>
2022-06-16 00:20:13 +0200

    added a disabled test that shows that volfrac computation works on a non-convex quad

--- igalgo/tst/VolFracComputationTestSuite.h
+++ igalgo/tst/VolFracComputationTestSuite.h
@@ -25,6 +25,12 @@ TEST(VolFracComputation, test2D)
 	gmds::MeshDoctor doc(&mImprint);
 	doc.orient2DFaces();
 
+	gmds::IGMeshIOService ioService_writet(&mImprint);
+	gmds::VTKWriter vtkWritert(&ioService_writet);
+	vtkWritert.setCellOptions(gmds::N|gmds::F);
+	vtkWritert.setDataOptions(gmds::N|gmds::F);
+	vtkWritert.write("VolFracComputation_test2D_tri.vtk");
+
 	gmds::Mesh m(MeshModel(DIM2|F|N|F2N));
 	gmds::GridBuilder gb(&m,2);
 	gb.execute(3,.75, 4, .75);
@@ -39,3 +45,40 @@ TEST(VolFracComputation, test2D)
 	vtkWriter.setDataOptions(gmds::N|gmds::F);
 	vtkWriter.write("VolFracComputation_test2D.vtk");
 }
+/*----------------------------------------------------------------------------*/
+TEST(VolFracComputation, DISABLED_test2D_nonconvex)
+{
+	gmds::Mesh mImprint(MeshModel(DIM2|F|N|F2N));
+	gmds::Node t0 = mImprint.newNode(-2,1,0);
+	gmds::Node t1 = mImprint.newNode(1,-2,0);
+	gmds::Node t2 = mImprint.newNode(1,1,0);
+	mImprint.newTriangle(t0,t1,t2);
+
+	// reorient the faces of the mesh when necessary
+	gmds::MeshDoctor doc(&mImprint);
+	doc.orient2DFaces();
+
+	gmds::IGMeshIOService ioService_writet(&mImprint);
+	gmds::VTKWriter vtkWritert(&ioService_writet);
+	vtkWritert.setCellOptions(gmds::N|gmds::F);
+	vtkWritert.setDataOptions(gmds::N|gmds::F);
+	vtkWritert.write("VolFracComputation_test2D_nonconvex_tri.vtk");
+
+	gmds::Mesh m(MeshModel(DIM2|F|N|F2N));
+	gmds::Node q0 = m.newNode(0,0,0);
+	gmds::Node q1 = m.newNode(0,-4,0);
+	gmds::Node q2 = m.newNode(5,5,0);
+	gmds::Node q3 = m.newNode(-4,0,0);
+	m.newQuad(q0,q1,q2,q3);
+
+	gmds::Variable<double>* vf = m.newVariable<double, gmds::GMDS_FACE>( "vf");
+
+	gmds::volfraccomputation_2d(&m, &mImprint, vf);
+
+	gmds::IGMeshIOService ioService_write(&m);
+	gmds::VTKWriter vtkWriter(&ioService_write);
+	vtkWriter.setCellOptions(gmds::N|gmds::F);
+	vtkWriter.setDataOptions(gmds::N|gmds::F);
+	vtkWriter.write("VolFracComputation_test2D_nonconvex_quad.vtk");
+}
+/*----------------------------------------------------------------------------*/
