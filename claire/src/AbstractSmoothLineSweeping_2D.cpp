c21cdc10994d39b5fdef02de599571b98410f8a2
Claire Ordi Pro <claireroche47@gmail.com>
2022-07-21 14:40:28 +0200

    Add Boundary Slipping for SmoothLineSweeping class.

--- claire/src/AbstractSmoothLineSweeping_2D.cpp
+++ claire/src/AbstractSmoothLineSweeping_2D.cpp
@@ -8,11 +8,14 @@
 using namespace gmds;
 /*------------------------------------------------------------------------*/
 
-AbstractSmoothLineSweeping_2D::AbstractSmoothLineSweeping_2D(Blocking2D::Block* B, int nb_max_it) {
-	m_B = B;
-	m_nb_max_iterations = nb_max_it;
-	m_Nx = m_B->getNbDiscretizationI();
-	m_Ny = m_B->getNbDiscretizationJ();
+AbstractSmoothLineSweeping_2D::AbstractSmoothLineSweeping_2D(Blocking2D::Block* B, int nb_max_it):
+  	m_B(B),
+  	m_nb_max_iterations(nb_max_it),
+  	m_Nx(m_B->getNbDiscretizationI()-1),
+	m_Ny(m_B->getNbDiscretizationJ()-1),
+  	m_old_coord_x(m_Nx,m_Ny),
+	m_old_coord_y(m_Nx,m_Ny)
+{
 	m_tol = pow(10,-6);
 	m_theta = 0.0;
 }
@@ -22,19 +25,17 @@ AbstractSmoothLineSweeping_2D::AbstractSmoothLineSweeping_2D(Blocking2D::Block* 
 /*------------------------------------------------------------------------*/
 AbstractSmoothLineSweeping_2D::STATUS AbstractSmoothLineSweeping_2D::execute()
 {
-	Array2D<TCoord>* old_coord_x;
-	Array2D<TCoord>* old_coord_y;
-
 	double err = pow(10,6);
 	int iteration(0);
 
 	while ( err > m_tol && iteration < m_nb_max_iterations )
 	{
 		One_Step_Smoothing();
+		BoundarySlipping();
 
-		err = L2_norm_relative_error(old_coord_x, old_coord_y);
+		err = L2_norm_relative_error(&m_old_coord_x, &m_old_coord_y);
 		iteration++;
-		Update_old_coords(old_coord_x, old_coord_y);
+		Update_old_coords(&m_old_coord_x, &m_old_coord_y);
 	}
 
 	return AbstractSmoothLineSweeping_2D::SUCCESS;
@@ -47,18 +48,43 @@ AbstractSmoothLineSweeping_2D::STATUS AbstractSmoothLineSweeping_2D::execute()
 /*------------------------------------------------------------------------*/
 void AbstractSmoothLineSweeping_2D::One_Step_Smoothing(){
 
-	for (int i=0; i < m_Nx; i++)
+	for (int i=1; i < m_Nx; i++)
 	{
-		for (int j=0; j < m_Ny; j++)
+		for (int j=1; j < m_Ny; j++)
 		{
-			Node n = (*m_B)(i, j);
 			math::Point new_ideal_pos = ComputeNewPosition(i,j) ;
 
 			// Damping
+			Node n = (*m_B)(i, j);
 			n.setPoint( m_theta*n.point() + (1.0-m_theta)*new_ideal_pos );
 		}
 	}
 
+}
+/*------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------*/
+void AbstractSmoothLineSweeping_2D::BoundarySlipping()
+{
+	for (int i=1;i<m_Nx;i++)
+	{
+		math::Point Mid = FindMidBranche((*m_B)(i-1,0).point(), (*m_B)(i,0).point(), (*m_B)(i+1,0).point());
+		(*m_B)(i, 0).setPoint(m_theta*(*m_B)(i, 0).point() + (1.0-m_theta)*Mid );
+
+		Mid = FindMidBranche((*m_B)(i-1,m_Ny).point(), (*m_B)(i,m_Ny).point(), (*m_B)(i+1,m_Ny).point());
+		(*m_B)(i, m_Ny).setPoint(m_theta*(*m_B)(i, m_Ny).point() + (1.0-m_theta)*Mid );
+	}
+
+
+	for (int j=1;j<m_Ny;j++)
+	{
+		math::Point Mid = FindMidBranche((*m_B)(0,j-1).point(), (*m_B)(0,j).point(), (*m_B)(0,j+1).point());
+		(*m_B)(0, j).setPoint(m_theta*(*m_B)(0, j).point() + (1.0-m_theta)*Mid);
+
+		Mid = FindMidBranche((*m_B)(m_Nx,j-1).point(), (*m_B)(m_Nx,j).point(), (*m_B)(m_Nx,j+1).point());
+		(*m_B)(m_Nx, j).setPoint(m_theta*(*m_B)(m_Nx, j).point() + (1.0-m_theta)*Mid);
+	}
 }
 /*------------------------------------------------------------------------*/
 
@@ -94,13 +120,9 @@ double AbstractSmoothLineSweeping_2D::L2_norm_relative_error(Array2D<TCoord>* ol
 	double num(0.0);
 	double denom(0.0);
 
-	int Nx = m_B->getNbDiscretizationI();
-	int Ny = m_B->getNbDiscretizationJ();
-	for (int i=0; i <= Nx; i++) {
-		for (int j = 0; j <= Ny; j++) {
+	for (int i=0; i < m_Nx; i++) {
+		for (int j = 0; j < m_Ny; j++) {
 			Node n = (*m_B)(i, j);
-			(*old_coord_x)(i, j) = n.X();
-			(*old_coord_y)(i, j) = n.Y();
 
 			double err_loc = sqrt( pow((*old_coord_x)(i, j) - n.X() , 2) + pow((*old_coord_y)(i, j) - n.Y(),2) );
 
