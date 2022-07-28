fa36cb5ac8dfaa27aa6ecbb9ed94353713b16e83
Claire Ordi Pro <claireroche47@gmail.com>
2022-07-28 15:42:54 +0200

    Adding new parameters for the aero algorithm in Params.

--- claire/tst/AeroPipelineTestSuite.h
+++ claire/tst/AeroPipelineTestSuite.h
@@ -31,29 +31,29 @@ TEST(AeroPipelineTestClass, AeroPipeline2D_Test1)
 
 	// IN/OUT Parameters
 	std::string dir(TEST_SAMPLES_DIR);
-	params_aero.input_file=dir+"/Aero/2D/NACA_0012_2D.vtk";
+	params_aero.input_file=dir+"/Aero/2D/Diamond_Airfoil_2D_Papier.vtk";
 	params_aero.output_file="AeroPipeline2D_Quad.vtk";
 	params_aero.output_dir="gmds/claire/tst/";
 
 	// Physical Parameters for the algorithm
-	params_aero.delta_cl = 0.04;				// Epaisseur de la première couche, pour la couche limite
+	params_aero.delta_cl = 5;				// Epaisseur de la première couche, pour la couche limite
 	params_aero.angle_attack = 0;			// Angle of attack (in degrees)
 
 	// Wall Discretization Parameter
 	params_aero.nbrMinBloc = 10;					// Minimal number of block on the wall
 	params_aero.nbrCellsInCL = 30;				// Number of cells in the boundary layer
-	params_aero.edge_size_wall = 0.001 ;									// Maximal size of the cells on the wall
-	params_aero.edge_size_default = 0.02 ;									// Default size of the cells in the domain
-	params_aero.edge_size_first_ortho_wall = 8*pow(10,-5);	//
+	params_aero.edge_size_wall = 0.5 ;										// Maximal size of the cells on the wall
+	params_aero.edge_size_default = 1 ;										// Default size of the cells in the domain
+	params_aero.edge_size_first_ortho_wall = 1*pow(10,-2);	//
 
 	// Vector Field Computation Parameters
 	params_aero.vectors_field = 3;				// Choose the way the vectors field is computed for the extrusion
-	params_aero.x_VectorField_Z1 = 1.5;			// Choose the x value of the first zone  [-inf, x_VectorField_Z1]
-	params_aero.x_VectorField_Z2 = 3.5;			// Choose the x value of the second zone [x_VectorField_Z2, +inf]
+	params_aero.x_VectorField_Z1 = 100;			// Choose the x value of the first zone  [-inf, x_VectorField_Z1]
+	params_aero.x_VectorField_Z2 = 300;			// Choose the x value of the second zone [x_VectorField_Z2, +inf]
 
 	// Extrusion Parameters
 	params_aero.nbr_couches = 5;			// Number of layer in extrusion
-	params_aero.x_lim = 1.2;			// Limites physiques à partir desquelles
+	params_aero.x_lim = 100;				// Limites physiques à partir desquelles
 	params_aero.y_lim = -10000;			// l'insertion et la fusion de blocs
 	params_aero.z_lim = -10000;			// sont autorisées
 
