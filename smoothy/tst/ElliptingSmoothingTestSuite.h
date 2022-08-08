5310a1a79313ed3b1497bd52a2ae4a855ade672f
franck.ledoux <franck.ledoux@live.fr>
2022-08-08 23:24:25 +0200

    159 test addition for the elliptic smoothing (#168)
    * Addition of 2 test cases.
    
    * - two test for aero cases are added but do not provide the expected results (tangled meshes at the end)
    - should investigate why
    
    * Addition of 2 test cases.
    
    * - two test for aero cases are added but do not provide the expected results (tangled meshes at the end)
    - should investigate why
    
    * Addition of test cases for the elliptic smoothing. It works on aero test cases by fixing the number max of iterations (1000 now).
    
    Two setters are available to choose the this number and the theta value now.
    
    * - test name duplication
    
    Co-authored-by: franck.ledoux <franck.ledoux@cea.fr>

--- smoothy/tst/ElliptingSmoothingTestSuite.h
+++ smoothy/tst/ElliptingSmoothingTestSuite.h
@@ -173,7 +173,6 @@ TEST(EllipticSmoothingTestSuite, grid2D_smoother)
 	std::default_random_engine eng(rd());
 	std::uniform_real_distribution<float> distr(FLOAT_MIN, FLOAT_MAX);
 
-
 	for(auto n_id:m.nodes()){
 		if(!m.isMarked<Node>(n_id,mark_bnd_nodes)) {
 			Node n = m.get<Node>(n_id);
@@ -198,5 +197,208 @@ TEST(EllipticSmoothingTestSuite, grid2D_smoother)
 		Face fj = m.get<Face>(j);
 		ASSERT_NEAR(1.0,fj.computeScaledJacobian2D(),0.01);
 	}
+}
+/*----------------------------------------------------------------------------*/
+TEST(EllipticSmoothingTestSuite, grid2D_smoother_UC)
+{
+	Mesh m(MeshModel(DIM3 | F | E | N | F2N | F2E | E2F | E2N | N2E | N2F));
+
+	GridBuilder gb(&m,2);
+	gb.execute(3,1,3,1);
+
+	MeshDoctor doc(&m);
+	doc.buildEdgesAndX2E();
+	doc.updateUpwardConnectivity();
+	doc.orient2DFaces();
+	for(auto f_id:m.faces()) {
+		Face f=m.get<Face>(f_id);
+		if (f.normal().dot(math::Vector3d({.0, .0, 1.0})) <= 0) {
+			std::vector<TCellID> ns = f.getIDs<Node>();
+			std::vector<TCellID> ns2(ns.size());
+			for (auto i = 0; i < ns.size(); i++)
+				ns2[ns.size() - 1 - i] = ns[i];
+			f.set<Node>(ns2);
+		}
+	}
+
+	//==================================================================
+	// MARK ALL THE BOUNDARY CELL OF THE INIT MESH
+	//==================================================================
+	// we get all the nodes that are on the mesh boundary
+	BoundaryOperator2D op(&m);
+	auto mark_node_NAN = m.newMark<Node>();
+	auto mark_node_on_pnt = m.newMark<Node>();
+	auto mark_node_on_crv = m.newMark<Node>();
+	auto mark_edge_on_crv = m.newMark<Edge>();
+
+	op.markCellOnGeometry(mark_edge_on_crv, mark_node_on_crv, mark_node_on_pnt, mark_node_NAN);
+	auto nb_locked = 0;
+	auto mark_bnd_nodes = m.newMark<Node>();
+	for (auto n_id : m.nodes()) {
+		if (m.isMarked<Node>(n_id, mark_node_on_crv) || m.isMarked<Node>(n_id, mark_node_on_pnt)) {
+			nb_locked += 1;
+			m.mark<Node>(n_id, mark_bnd_nodes);
+		}
+	}
+	//==================================================================
+	// PERFORM THE PERTURBATION
+	//==================================================================
+	constexpr int FLOAT_MIN = -100;
+	constexpr int FLOAT_MAX = 108;
+	std::random_device rd;
+	std::default_random_engine eng(rd());
+	std::uniform_real_distribution<float> distr(FLOAT_MIN, FLOAT_MAX);
+
+	for(auto n_id:m.nodes()){
+		if(!m.isMarked<Node>(n_id,mark_bnd_nodes)) {
+			Node n = m.get<Node>(n_id);
+			n.setXYZ(distr(eng), distr(eng), 0);
+		}
+	}
+	//==================================================================
+	// PERFORM THE MESH SMOOTHING NOWEllipticSmoothingTestSuite.grid2D_smoother (1)
+	//==================================================================
+
+
+	EllipticSmoother2D smoother2D(&m);
+	smoother2D.lock(mark_bnd_nodes);
+	smoother2D.execute();
+
+	for(auto i:m.nodes()){
+		Node ni = m.get<Node>(i);
+		ASSERT_TRUE(ni.X()>=0. && ni.X()<=4.);
+		ASSERT_TRUE(ni.Y()>=0. && ni.Y()<=4.);
+		ASSERT_TRUE(ni.Z()>=0. && ni.Z()<=4.);
+	}
+	for(auto j:m.faces()){
+		Face fj = m.get<Face>(j);
+		ASSERT_NEAR(1.0,fj.computeScaledJacobian2D(),0.01);
+	}
+
+
+}
+
+/*----------------------------------------------------------------------------*/
+TEST(EllipticSmoothingTestSuite, aero_test1)
+{
+	Mesh m(MeshModel(DIM3 | F | E | N | F2N | F2E | E2F | E2N | N2E | N2F));
+
+	std::string dir(TEST_SAMPLES_DIR);
+	std::string vtk_file = dir+"/Aero/2D/ElipticSmoothTest1.vtk";
+	IGMeshIOService ioService(&m);
+
+	VTKReader vtkReader(&ioService);
+	vtkReader.setCellOptions(gmds::N|gmds::F);
+	vtkReader.read(vtk_file);
+
+	MeshDoctor doc(&m);
+	doc.buildEdgesAndX2E();
+	doc.updateUpwardConnectivity();
+	doc.orient2DFaces();
+	for(auto f_id:m.faces()) {
+		Face f=m.get<Face>(f_id);
+		if (f.normal().dot(math::Vector3d({.0, .0, 1.0})) <= 0) {
+			std::vector<TCellID> ns = f.getIDs<Node>();
+			std::vector<TCellID> ns2(ns.size());
+			for (auto i = 0; i < ns.size(); i++)
+				ns2[ns.size() - 1 - i] = ns[i];
+			f.set<Node>(ns2);
+		}
+	}
+
+	//==================================================================
+	// MARK ALL THE BOUNDARY CELL OF THE INIT MESH
+	//==================================================================
+	// we get all the nodes that are on the mesh boundary
+	BoundaryOperator2D op(&m);
+	auto mark_node_NAN = m.newMark<Node>();
+	auto mark_node_on_pnt = m.newMark<Node>();
+	auto mark_node_on_crv = m.newMark<Node>();
+	auto mark_edge_on_crv = m.newMark<Edge>();
+
+	op.markCellOnGeometry(mark_edge_on_crv, mark_node_on_crv, mark_node_on_pnt, mark_node_NAN);
+	auto nb_locked = 0;
+	auto mark_bnd_nodes = m.newMark<Node>();
+	for (auto n_id : m.nodes()) {
+		if (m.isMarked<Node>(n_id, mark_node_on_crv) || m.isMarked<Node>(n_id, mark_node_on_pnt)) {
+			nb_locked += 1;
+			m.mark<Node>(n_id, mark_bnd_nodes);
+		}
+	}
+	//==================================================================
+	// PERFORM THE MESH SMOOTHING NOW
+	//==================================================================
+	EllipticSmoother2D smoother2D(&m);
+	smoother2D.lock(mark_bnd_nodes);
+	smoother2D.execute();
+
+	/*
+	   VTKWriter w(&ioService);
+	   w.setCellOptions(gmds::N|gmds::F);
+	   w.setDataOptions(gmds::N|gmds::F);
+	   w.write("out_aero2.vtk");
+	*/
+}
+
+/*----------------------------------------------------------------------------*/
+TEST(EllipticSmoothingTestSuite, aero_test2)
+{
+	Mesh m(MeshModel(DIM3 | F | E | N | F2N | F2E | E2F | E2N | N2E | N2F));
+
+	std::string dir(TEST_SAMPLES_DIR);
+	std::string vtk_file = dir+"/Aero/2D/ElipticSmoothTest2.vtk";
+	IGMeshIOService ioService(&m);
+
+	VTKReader vtkReader(&ioService);
+	vtkReader.setCellOptions(gmds::N|gmds::F);
+	vtkReader.read(vtk_file);
+
+	MeshDoctor doc(&m);
+	doc.buildEdgesAndX2E();
+	doc.updateUpwardConnectivity();
+	doc.orient2DFaces();
+	for(auto f_id:m.faces()) {
+		Face f=m.get<Face>(f_id);
+		if (f.normal().dot(math::Vector3d({.0, .0, 1.0})) <= 0) {
+			std::vector<TCellID> ns = f.getIDs<Node>();
+			std::vector<TCellID> ns2(ns.size());
+			for (auto i = 0; i < ns.size(); i++)
+				ns2[ns.size() - 1 - i] = ns[i];
+			f.set<Node>(ns2);
+		}
+	}
+
+	//==================================================================
+	// MARK ALL THE BOUNDARY CELL OF THE INIT MESH
+	//==================================================================
+	// we get all the nodes that are on the mesh boundary
+	BoundaryOperator2D op(&m);
+	auto mark_node_NAN = m.newMark<Node>();
+	auto mark_node_on_pnt = m.newMark<Node>();
+	auto mark_node_on_crv = m.newMark<Node>();
+	auto mark_edge_on_crv = m.newMark<Edge>();
+
+	op.markCellOnGeometry(mark_edge_on_crv, mark_node_on_crv, mark_node_on_pnt, mark_node_NAN);
+	auto nb_locked = 0;
+	auto mark_bnd_nodes = m.newMark<Node>();
+	for (auto n_id : m.nodes()) {
+		if (m.isMarked<Node>(n_id, mark_node_on_crv) || m.isMarked<Node>(n_id, mark_node_on_pnt)) {
+			nb_locked += 1;
+			m.mark<Node>(n_id, mark_bnd_nodes);
+		}
+	}
+	//==================================================================
+	// PERFORM THE MESH SMOOTHING NOW
+	//==================================================================
+	EllipticSmoother2D smoother2D(&m);
+	smoother2D.lock(mark_bnd_nodes);
+	smoother2D.execute();
+
+	/*
+	   VTKWriter w(&ioService);
+	   w.setCellOptions(gmds::N|gmds::F);
+	   w.setDataOptions(gmds::N|gmds::F);
+	   w.write("out_aero2.vtk");
+	*/
 }
 
