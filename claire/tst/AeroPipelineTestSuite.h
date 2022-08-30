5bcaa871de3cc58dc565d691f07ec7b1c93ee537
Claire Ordi Pro <claireroche47@gmail.com>
2022-08-30 13:54:07 +0200

    - Correction critère qualité calcul angle

--- claire/tst/AeroPipelineTestSuite.h
+++ claire/tst/AeroPipelineTestSuite.h
@@ -42,9 +42,9 @@ TEST(AeroPipelineTestClass, AeroPipeline2D_Test1)
 	// Wall Discretization Parameter
 	params_aero.nbrMinBloc = 4;					// Minimal number of block on the wall
 	params_aero.nbrCellsInCL = 30;				// Number of cells in the boundary layer
-	params_aero.edge_size_wall = 1 ;										// Maximal size of the cells on the wall
-	params_aero.edge_size_default = 2 ;										// Default size of the cells in the domain
-	params_aero.edge_size_first_ortho_wall = 1*pow(10,-3);	// Size of the first edge orthogonal to the wall
+	params_aero.edge_size_wall = 3 ;										// Maximal size of the cells on the wall
+	params_aero.edge_size_default = 5 ;										// Default size of the cells in the domain
+	params_aero.edge_size_first_ortho_wall = 1*pow(10,-5);	// Size of the first edge orthogonal to the wall
 
 	// Vector Field Computation Parameters
 	params_aero.vectors_field = 1;				// Choose the way the vectors field is computed for the extrusion
@@ -52,8 +52,8 @@ TEST(AeroPipelineTestClass, AeroPipeline2D_Test1)
 	params_aero.x_VectorField_Z2 = 600;			// Choose the x value of the second zone [x_VectorField_Z2, +inf]
 
 	// Extrusion Parameters
-	params_aero.nbr_couches = 4;			// Number of layer in extrusion
-	params_aero.x_lim = 20000;				// Limites physiques à partir desquelles
+	params_aero.nbr_couches = 3;			// Number of layer in extrusion
+	params_aero.x_lim = 100;				// Limites physiques à partir desquelles
 	params_aero.y_lim = -10000;			// l'insertion et la fusion de blocs
 	params_aero.z_lim = -10000;			// sont autorisées
 
@@ -106,7 +106,7 @@ TEST(AeroPipelineTestClass, AeroPipeline2D_User)
 	params_aero.x_VectorField_Z2 = 600;			// Choose the x value of the second zone [x_VectorField_Z2, +inf]
 
 	// Extrusion Parameters
-	params_aero.nbr_couches = 3;			// Number of layer in extrusion
+	params_aero.nbr_couches = 10;			// Number of layer in extrusion
 	params_aero.x_lim = 0;				// Limites physiques à partir desquelles
 	params_aero.y_lim = -10000;			// l'insertion et la fusion de blocs
 	params_aero.z_lim = -10000;			// sont autorisées
