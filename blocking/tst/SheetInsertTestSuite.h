64219de15075abcdc8cc8fad20b6513ec6940791
nicolas le goff <nicolas.le-goff@cea.fr>
2022-09-04 22:56:10 +0200

    insertsheet first implementation

--- blocking/tst/SheetInsertTestSuite.h
+++ blocking/tst/SheetInsertTestSuite.h
@@ -38,7 +38,23 @@ TEST(SheetInsertTestSuite, pillow3d)
 	gmds::blocking::SheetInsert::STATUS status = is.pillow();
 	ASSERT_EQ(status, gmds::blocking::SheetInsert::NOT_YET_IMPLEMENTED);
 
+	bl.writeVTKFile("pillow3d.vtk");
+	bl.writeMokaFile("pillow3d.moka");
+}
+/*----------------------------------------------------------------------------*/
+TEST(SheetInsertTestSuite, insert3d)
+{
+	gmds::blocking::Blocking bl;
+	//	bl.createGrid3d();
+	bl.createGrid3d(gmds::math::Point(0,0,0), gmds::math::Point(2,2,2), 2,2,2);
+
+	gmds::blocking::SheetInsert is;
+	is.setBl(&bl);
+
+	gmds::blocking::SheetInsert::STATUS status = is.execute();
+	ASSERT_EQ(status, gmds::blocking::SheetInsert::NOT_YET_IMPLEMENTED);
+
 	bl.writeVTKFile("insertsheet3d.vtk");
-//	bl.writeMokaFile("insertsheet3d.moka");
+	bl.writeMokaFile("insertsheet3d.moka");
 }
 /*----------------------------------------------------------------------------*/
