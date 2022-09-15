ff14e52457d498626931da2ff519e85a0cff2d70
nicolas le goff <nicolas.le-goff@cea.fr>
2022-09-15 12:49:04 +0200

    added an lcc dart writer to the vtk file format.

---
+++ blocking/tst/WriterDartsVTKTestSuite.h
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------*/
+#include <gtest/gtest.h>
+/*----------------------------------------------------------------------------*/
+#include <filesystem>
+//#include <iostream>
+/*----------------------------------------------------------------------------*/
+#include <gmds/blocking/WriterDartsVTK.h>
+/*----------------------------------------------------------------------------*/
+TEST(WriterDartsVTKTestSuite, dummytest)
+{
+	ASSERT_EQ(0, 0);
+}
+/*----------------------------------------------------------------------------*/
+TEST(WriterDartsVTKTestSuite, grid2d)
+{
+	gmds::blocking::Blocking bl;
+	bl.createGrid2d(gmds::math::Point(0,0,0), gmds::math::Point(2,2, 0), 2,3);
+
+	gmds::blocking::WriterDartsVTK writer;
+	writer.setBl(&bl);
+	writer.execute("darts_grid2d.vtk");
+}
+/*----------------------------------------------------------------------------*/
+TEST(WriterDartsVTKTestSuite, grid3d)
+{
+	gmds::blocking::Blocking bl;
+	bl.createGrid3d(gmds::math::Point(0,0,0), gmds::math::Point(2,3,4), 2,3,4);
+
+	gmds::blocking::WriterDartsVTK writer;
+	writer.setBl(&bl);
+	writer.execute("darts_grid3d.vtk");
+}
+/*----------------------------------------------------------------------------*/
