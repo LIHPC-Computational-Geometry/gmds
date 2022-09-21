cdbe379a3f2011766524d45f9c1f7ea21661d6ed
franck.ledoux <franck.ledoux@live.fr>
2022-09-22 00:25:36 +0200

    - fast localization of a point in a triangular mesh for aero 2D publication - only closest node is returned right now

--- claire/tst/main_test.cpp
+++ claire/tst/main_test.cpp
@@ -15,6 +15,7 @@
 #include "AeroMeshQualityTestSuite.h"
 #include "RefinementBetaTestSuite.h"
 #include "SmoothLineSweepingTestSuite.h"
+#include "FastLocalizeTestSuite.h"
 /*----------------------------------------------------------------------------*/
 int main(int argc, char ** argv) {
   ::testing::InitGoogleTest(&argc, argv);
