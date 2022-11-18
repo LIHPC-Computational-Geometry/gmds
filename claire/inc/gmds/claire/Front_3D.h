5964ea048331ba842fa26530bcfc75f1e6da1dad
Claire Ordi Pro <claireroche47@gmail.com>
2022-11-18 11:42:10 +0100

    - Add the front 3D class.

--- claire/inc/gmds/claire/Front_3D.h
+++ claire/inc/gmds/claire/Front_3D.h
@@ -28,71 +28,19 @@ class LIB_GMDS_CLAIRE_API Front_3D {
 	/*-------------------------------------------------------------------*/
 	/** \brief Default constructor
 	 */
-	Front_3D(int front_id, std::vector<TCellID> nodes_Id, std::vector<TCellID> faces_Id);
+	Front_3D();
 	/*-------------------------------------------------------------------*/
 
  public:
 	/*-------------------------------------------------------------------*/
-	/** @brief Set the front id
+	/** @brief Change l'id du front
 	 * 	@param[in] layer_id nouvel id du front
 	 */
 	void setFrontID(int layer_id);
 	/*-------------------------------------------------------------------*/
-	/** @brief Give the front id
+	/** @brief Retourne l'id du front
 	 */
 	int getFrontID();
-	/*-------------------------------------------------------------------*/
-	/** @brief Returns the front nodes id
-	 */
-	std::vector<TCellID> getNodes();
-	/*-------------------------------------------------------------------*/
-	/** @brief Returns the front faces id
-	 */
-	std::vector<TCellID> getFaces();
-	/*-------------------------------------------------------------------*/
-	/** @brief Add the node id to the front
-	 * 	@param n_id id of the node to add
-	 */
-	void addNodeId(TCellID n_id);
-	/*-------------------------------------------------------------------*/
-	/** @brief Add the face id to the front
-	 * 	@param f_id id of the face to add
-	 */
-	void addFaceId(TCellID f_id);
-	/*-------------------------------------------------------------------*/
-	/** @brief Returns the ordered front edges around a node of the front.
-	 * 	@param m	the mesh
-	 *		@param n_id id of the node
-	 *
-	 *		\return  a vector of the edges id ordered
-	 */
-	std::vector<TCellID> orderedFrontEdgesAroundNode(Mesh *m, TCellID n_id);
-	/*-------------------------------------------------------------------*/
-	/** @brief Returns the two front faces around an edge.
-	 * 	@param m	the mesh
-	 *		@param e_id id of the edge
-	 *
-	 *		\return  a vector of the two faces id
-	 */
-	std::vector<TCellID> edgeFacesOnFront(Mesh *m, TCellID e_id);
-	/*-------------------------------------------------------------------*/
-	/** @brief Returns the outgoing normal to a face on the front.
-	 * 	@param m	the mesh
-	 *		@param f_id id of the face
-	 *
-	 *		\return  a 3d vector
-	 */
-	math::Vector3d outgoingNormal(Mesh *m, TCellID f_id);
-	/*-------------------------------------------------------------------*/
-	/** @brief Returns the face of the front adjacent to the face f_id
-	 * 	and the edge e_id.
-	 * 	@param m	the mesh
-	 *		@param f_id id of the face
-	 *		@param e_id id of the edge
-	 *
-	 *		\return  a 3d vector
-	 */
-	TCellID adjacentFaceOnFront(Mesh *m, TCellID f_id, TCellID e_id);
 	/*-------------------------------------------------------------------*/
 
  private:
@@ -102,6 +50,7 @@ class LIB_GMDS_CLAIRE_API Front_3D {
 	std::vector<TCellID> m_nodesId;
 	/** Liste d'id des arêtes du front */
 	std::vector<TCellID> m_facesId;
+
 
 };
 /*----------------------------------------------------------------------------*/
