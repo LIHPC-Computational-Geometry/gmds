cc7aa39c3bae6610d6fd5ca29eccd37e1be3d193
Claire Ordi Pro <claireroche47@gmail.com>
2022-11-24 11:12:01 +0100

    - Add a 3D test for the algorithm FastLocalize

--- claire/tst/FastLocalizeTestSuite.h
+++ claire/tst/FastLocalizeTestSuite.h
@@ -4,7 +4,6 @@
 
 #include <gmds/claire/FastLocalize.h>
 #include <gmds/ig/Mesh.h>
-#include <gmds/claire/Utils.h>
 #include <gmds/ig/MeshDoctor.h>
 #include <gmds/io/VTKWriter.h>
 #include <gmds/io/VTKReader.h>
@@ -79,29 +78,3 @@ TEST(FastLocalizeTestSuite, test_FastLocalize_3D)
 
 }
 
-TEST(FastLocalizeTestSuite, test_FastLocalize_3D_2)
-{
-	// WE READ
-	gmds::Mesh m(gmds::MeshModel(DIM3 | R | F | E | N | R2N | F2N | E2N | R2F | F2R |
-	                              F2E | E2F | R2E | E2R | N2R | N2F | N2E));
-
-	std::string dir(TEST_SAMPLES_DIR);
-	std::string vtk_file = dir + "/Aero/3D/C3_3D_0.3.vtk";
-
-	gmds::IGMeshIOService ioService(&m);
-	gmds::VTKReader vtkReader(&ioService);
-	vtkReader.setCellOptions(gmds::N | gmds::R);
-	vtkReader.read(vtk_file);
-
-	gmds::MeshDoctor doc(&m);
-	doc.buildEdgesAndX2E();
-	doc.updateUpwardConnectivity();
-
-	FastLocalize fl(&m);
-	Cell::Data data = fl.find({-2.74355, -1.62243, -1.49076});
-
-	gmds::TCellID r_id = fl.findTetra({-2.74355, -1.62243, -1.49076});
-
-	ASSERT_EQ(r_id, 3364);
-
-}
