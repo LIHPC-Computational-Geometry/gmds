6b7f843a3fd417780dac60490969bee670268112
Claire Ordi Pro <claireroche47@gmail.com>
2023-01-27 15:28:10 +0100

    - Fix a bug in the Template 3Corner 3End

--- claire/tst/AeroPipelineTestSuite.h
+++ claire/tst/AeroPipelineTestSuite.h
@@ -220,7 +220,7 @@ TEST(AeroPipelineTestClass, AeroPipeline3D_Test1)
 	*/
 
 	std::string dir(TEST_SAMPLES_DIR);
-	std::string input_file=dir+"/Aero/3D/param_C2_3D.ini";
+	std::string input_file=dir+"/Aero/3D/param_C5_3D.ini";
 
 	//---------------------//
 	//    AERO PIPELINE    //
