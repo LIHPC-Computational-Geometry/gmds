cb8c4f1a730544219fe0b4dc816879efe254fd2a
Claire Ordi Pro <claireroche47@gmail.com>
2023-02-01 13:39:03 +0100

    - Add a template to Node: 2 END, 1 REVERSAL

--- claire/tst/AeroPipelineTestSuite.h
+++ claire/tst/AeroPipelineTestSuite.h
@@ -220,7 +220,7 @@ TEST(AeroPipelineTestClass, AeroPipeline3D_Test1)
 	*/
 
 	std::string dir(TEST_SAMPLES_DIR);
-	std::string input_file=dir+"/Aero/3D/param_C7_3D.ini";
+	std::string input_file=dir+"/Aero/3D/param_C8_3D.ini";
 
 	//---------------------//
 	//    AERO PIPELINE    //
