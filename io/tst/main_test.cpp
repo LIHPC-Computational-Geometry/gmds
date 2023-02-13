42d2f9bee3dedc7b895c275c7bb0e1a72e7d8929
franck.ledoux <franck.ledoux@cea.fr>
2023-02-13 16:39:18 +0100

    Use the right C++ directives to avoid Lima Reader/Writer calls in IO testing suite

--- io/tst/main_test.cpp
+++ io/tst/main_test.cpp
@@ -4,9 +4,9 @@
 // Files containing the different test suites to launch
 #include "ReaderTestSuite.h"
 #include "WriterTestSuite.h"
-//#ifdef WITH_LIMA
+#ifdef WITH_LIMA
 #include "LimaWriteAndReadTestSuite.h"
-//#endif//WITH_LIMA
+#endif//WITH_LIMA
 //#include "MeshBWriterTestSuite.h"
 /*----------------------------------------------------------------------------*/
 int main(int argc, char ** argv) {
