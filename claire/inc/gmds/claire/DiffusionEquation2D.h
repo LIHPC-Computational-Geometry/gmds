347ad9d16ac307512bb94360652c993ebfdeecc7
Claire Ordi Pro <claireroche47@gmail.com>
2023-03-13 14:19:12 +0100

    - Fix some warnings in the DiffusionEquation2D files

--- claire/inc/gmds/claire/DiffusionEquation2D.h
+++ claire/inc/gmds/claire/DiffusionEquation2D.h
@@ -29,7 +29,7 @@ class LIB_GMDS_CLAIRE_API DiffusionEquation2D
 	/** @brief Constructor.
          *  @param AMesh the mesh where we work on
 	 */
-	DiffusionEquation2D(Mesh *AMesh, int AmarkFrontNodes_int, int AmarkFrontNodes_out, Variable<double>* Adistance);
+	DiffusionEquation2D(Mesh *AMesh, TInt AmarkFrontNodes_int, TInt AmarkFrontNodes_out, Variable<double>* Adistance);
 
 	/*-------------------------------------------------------------------*/
 	/** @brief Execute the algorithm
@@ -42,18 +42,18 @@ class LIB_GMDS_CLAIRE_API DiffusionEquation2D
 	/** @brief Basis function of the reference triangle.
     	*  @param[in] hat_i summit of the reference triangle (1, 2 or 3)
 	 */
-	double phi_hat(int hat_i, math::Point X_hat);
+	static double phi_hat(int hat_i, math::Point X_hat);
 	/*-------------------------------------------------------------------*/
 	/** @brief Grad of basis function of the reference triangle.
     	*  @param[in] hat_i summit of the reference triangle (1, 2 or 3)
 	 */
-	Eigen::Vector2d grad_phi_hat(int hat_i);
+	static Eigen::Vector2d grad_phi_hat(int hat_i);
 	/*-------------------------------------------------------------------*/
 	/** @brief Transformation of the point X_hat in the triangle K.
 	 	*  @param[in] triK_id id of the triangle K
     	*  @param[in] X_hat point in the ref triangle K_hat
 	 */
-	Eigen::Vector2d FK(TCellID triK_id, math::Point X_hat);
+	Eigen::Vector2d FK(TCellID triK_id, const math::Point& X_hat);
 	/*-------------------------------------------------------------------*/
 	/** @brief Jacobian of the function FK.
 	 	*  @param[in] triK_id id of the triangle K
@@ -76,7 +76,7 @@ class LIB_GMDS_CLAIRE_API DiffusionEquation2D
 	 	*  @param[in]
 	 	*  @param[in]
 	 */
-	void quadraturePointsAndWeightsSummitpointFormula(std::vector<double> &weights, std::vector<math::Point> &points);
+	static void quadraturePointsAndWeightsSummitpointFormula(std::vector<double> &weights, std::vector<math::Point> &points);
 	/*-------------------------------------------------------------------*/
 
 	/*-------------------------------------------------------------------*/
@@ -106,9 +106,9 @@ class LIB_GMDS_CLAIRE_API DiffusionEquation2D
 	/** mesh we work on */
 	Mesh *m_mesh;
 	/** mark on the interior nodes, first boundary */
-	int m_markNodes_int;
+	TInt m_markNodes_int;
 	/** mark on the exterior nodes, second boundary */
-	int m_markNodes_out;
+	TInt m_markNodes_out;
 	/** carte des distances par rapport au front concerné */
 	Variable<double>* m_distance;
 
