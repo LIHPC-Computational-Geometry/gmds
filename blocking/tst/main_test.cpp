5bb662e94b14fbcd5e384f9380d063fecde66a97
calderans <calderan.simon@gmail.com>
2023-03-15 14:33:45 +0100

    - Removed duplicated CGNSWriterTestSuite call in tests

--- blocking/tst/main_test.cpp
+++ blocking/tst/main_test.cpp
@@ -8,7 +8,6 @@
 #include "InputMarkedDartsTestSuite.h"
 #include "SheetInsertTestSuite.h"
 #include "WriterDartsVTKTestSuite.h"
-#include "CGNSWriterTestSuite.h"
 /*----------------------------------------------------------------------------*/
 int main(int argc, char ** argv) {
   ::testing::InitGoogleTest(&argc, argv);
