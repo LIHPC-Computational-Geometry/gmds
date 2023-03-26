60036c13ea56f2259ea4fa75fa46e327c9506387
Claire Ordi Pro <claireroche47@gmail.com>
2023-03-26 16:16:16 +0200

    - Add the Interval Assignment 3D

---
+++ claire/inc/gmds/claire/IntervalAssignment_3D.h
@@ -0,0 +1,89 @@
+//
+// Created by rochec on 23/03/2023.
+//
+
+#ifndef GMDS_INTERVALASSIGNMENT_3D_H
+#define GMDS_INTERVALASSIGNMENT_3D_H
+
+/*----------------------------------------------------------------------------*/
+#include "LIB_GMDS_CLAIRE_export.h"
+#include "gmds/ig/Mesh.h"
+#include <gmds/claire/Params.h>
+#include <string>
+#include <map>
+#include <fstream>
+namespace gmds {
+/*----------------------------------------------------------------------------*/
+class LIB_GMDS_CLAIRE_API IntervalAssignment_3D
+{
+ public:
+	/*--------------------------------------------------------------------*/
+	/** @enum  Status code for executing algorithms
+	 */
+	typedef enum {
+		FAIL,
+		SUCCESS
+	} STATUS;
+
+	/*-------------------------------------------------------------------*/
+	/** @brief Constructor.
+         *  @param[in] ABlocking2D the blocking 2D
+         *  @param[in] Aparams_aero parameters for aero algorithm
+         *
+	 */
+	IntervalAssignment_3D(Mesh* AMesh, ParamsAero& Aparams_aero, Variable<int>* AedgesDiscretization);
+
+	/*-------------------------------------------------------------------*/
+	/** @brief Execute the algorithm
+	 */
+	STATUS execute();
+	/*-------------------------------------------------------------------*/
+
+ private:
+	/*-------------------------------------------------------------------*/
+	/** @brief Compute the map of chords of the blocking.
+	 	* \param[in] e_id starting edge id
+		*
+		* \return a map with (int, std::vector<TCellID>) where the int is the id
+	 	* of the chord and the vector is filled with the ids of the chord edges
+	 */
+	std::vector<TCellID> ComputeSingleSheet(TCellID e_id);
+	/*-------------------------------------------------------------------*/
+	/** @brief Compute the map of sheets of the mesh.
+	 	* \param[in]
+		*
+		* \return  a map with (int, std::vector<TCellID>) where the int is the id
+	 	* of the sheet and the vector is filled with the ids of the sheet edges
+	 */
+	std::map<int, std::vector<TCellID>> ComputeSheets();
+	/*-------------------------------------------------------------------*/
+	/** @brief Get the constraint of the edge e_id
+	 	* \param[in] e_id the edge we want to know the constraint
+	 	* \param[out] N_ideal the ideal number of cells on the edge
+	 	* \param[out] hardConstraint is the constraint on the edge hard or not
+		*
+		* \return
+	 */
+	void EdgeConstraint(TCellID e_id, int &N_ideal, bool &hardConstraint);
+	/*-------------------------------------------------------------------*/
+	/** @brief Get the number of cells for the chord
+	 	* \param[in] chord the chord
+		*
+		* \return the number of cells for all the edges of the chord
+	 */
+	int ComputeSheetDiscretization(const std::vector<TCellID>& sheet);
+	/*-------------------------------------------------------------------*/
+
+ private:
+	/** 3D Mesh */
+	Mesh *m_mesh;
+	/** Params pour l'aéro */
+	ParamsAero m_params_aero;
+	/** Variable on the edges, filled with the edge discretization */
+	Variable<int>* m_edgesDiscretization;
+
+};
+/*----------------------------------------------------------------------------*/
+}     // namespace gmds
+
+#endif     // GMDS_INTERVALASSIGNMENT_3D_H
