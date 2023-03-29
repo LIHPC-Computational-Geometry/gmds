32ebb77e3c8d5b5ba493a5d0c9854d926f546ce0
nicolas le goff <nicolas.le-goff@cea.fr>
2023-03-30 00:05:37 +0200

    elgmorphing can now handle compound groups based on a prefix

--- elgmorphing/inc/gmds/elgmorphing/ElgMorphing.h
+++ elgmorphing/inc/gmds/elgmorphing/ElgMorphing.h
@@ -5,9 +5,8 @@
 #include "LIB_GMDS_ELGMORPHING_export.h"
 #include <gmds/ig/Mesh.h>
 #include <gmds/math/Point.h>
+#include <set>
 #include <string>
-//#include <map>
-//#include <fstream>
 /*----------------------------------------------------------------------------*/
 namespace gmds {
 /*----------------------------------------------------------------------------*/
@@ -40,6 +39,37 @@ class LIB_GMDS_ELGMORPHING_API ElgMorphing{
 	/** \brief
 	 */
 	ElgMorphing::STATUS execute();
+
+	/*-------------------------------------------------------------------------*/
+	/** \brief
+	 */
+	void setMeshOrig(gmds::Mesh* AMesh) {mesh_orig_ = AMesh;}
+	/*-------------------------------------------------------------------------*/
+	/** \brief
+	 */
+	void setMeshDest(gmds::Mesh* AMesh) {mesh_orig_ = AMesh;}
+
+	/**@brief Identify the nodes that are part of the cells groups
+	 * which names begin with @Astring
+	    	* @param AMesh
+         * @param AString the prefix we look for in the groups names
+         *
+         * @return a container of those nodes
+	 */
+	std::set<TCellID> compoundGrpNodes(gmds::Mesh* AMesh,
+												  std::string AString);
+
+	/*-------------------------------------------------------------------------*/
+	/**@brief Identify the nodes that are part of the cells groups
+	 * which names begin with @Astring, but only those on the boundary
+	    	* @param AMesh
+         * @param AString the prefix we look for in the groups names
+         *
+         * @return a container of those nodes
+	 */
+	std::set<TCellID> compoundGrpBnd(gmds::Mesh* AMesh,
+												std::string AString);
+
 	/*-------------------------------------------------------------------------*/
 	/** \brief Boundingbox computation
 	 */
@@ -47,17 +77,25 @@ class LIB_GMDS_ELGMORPHING_API ElgMorphing{
 	                        std::string AGroupName,
 									gmds::TCoord minXYZ[3],
 	                        gmds::TCoord maxXYZ[3]) const;
+	void computeBoundingBox(const gmds::Mesh* AMesh,
+	                        const std::set<TCellID>& AIDs,
+	                        gmds::TCoord minXYZ[3],
+	                        gmds::TCoord maxXYZ[3]) const;
 	/*-------------------------------------------------------------------------*/
 	/** \brief Boundingbox transform.
 	 */
 	gmds::math::Point bbtransform2d(gmds::math::Point APt,
-	                              gmds::TCoord minXYZ_orig[3], gmds::TCoord maxXYZ_orig[3],
-	                              gmds::TCoord minXYZ_dest[3], gmds::TCoord maxXYZ_dest[3]) const;
+											  gmds::TCoord minXYZ_orig[3], gmds::TCoord maxXYZ_orig[3],
+											  gmds::TCoord minXYZ_dest[3], gmds::TCoord maxXYZ_dest[3]) const;
 	/*-------------------------------------------------------------------------*/
 
  protected:
+
 	/** pointer to mesh we work on */
-	Mesh* m_mesh;
+	Mesh* mesh_orig_;
+	Mesh* mesh_dest_;
+
+	TInt mark_fixed_nodes_;
 };
 /*----------------------------------------------------------------------------*/
 }  // namespace elgmorphing
