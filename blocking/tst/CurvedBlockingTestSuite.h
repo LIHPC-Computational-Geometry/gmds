9af469a7a0ae5cc8dcf6b1678b1f68807a5e0cc7
franck.ledoux <franck.ledoux@live.fr>
2023-05-11 09:58:15 +0200

    - First implementation of block removal in the curved blocking data structure

--- blocking/tst/CurvedBlockingTestSuite.h
+++ blocking/tst/CurvedBlockingTestSuite.h
@@ -59,6 +59,34 @@ TEST(CurvedBlockingTestSuite, init)
 	ASSERT_EQ(11, bl.get_nb_cells<2>());
 	ASSERT_EQ(2, bl.get_nb_cells<3>());
 }
+
+/*----------------------------------------------------------------------------*/
+TEST(CurvedBlockingTestSuite, remove_block)
+{
+	gmds::cad::FACManager geom_model;
+	setUp(geom_model);
+	gmds::blocking::CurvedBlocking bl(&geom_model);
+	gmds::math::Point p000(0, 0, 0);
+	gmds::math::Point p010(0, 1, 0);
+	gmds::math::Point p110(1, 1, 0);
+	gmds::math::Point p100(1, 0, 0);
+
+	gmds::math::Point p001(0, 0, 1);
+	gmds::math::Point p011(0, 1, 1);
+	gmds::math::Point p111(1, 1, 1);
+	gmds::math::Point p101(1, 0, 1);
+
+	gmds::math::Point p002(0, 0, 2);
+	gmds::math::Point p012(0, 1, 2);
+	gmds::math::Point p112(1, 1, 2);
+	gmds::math::Point p102(1, 0, 2);
+	auto b1 = bl.create_block(p000, p010, p110, p100, p001, p011, p111, p101);
+	auto b2 = bl.create_block(p001, p011, p111, p101, p002, p012, p112, p102);
+	bl.sew<3>(b1->dart(), b2->dart());
+	ASSERT_EQ(bl.get_nb_cells<3>(),2);
+	bl.remove_block(b1);
+	ASSERT_EQ(bl.get_nb_cells<3>(),1);
+}
 /*----------------------------------------------------------------------------*/
 TEST(CurvedBlockingTestSuite, single_block)
 {
