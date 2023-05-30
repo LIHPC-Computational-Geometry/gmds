2ffae0d18305cc83770b05b478b464545cea16bc
nicolas le goff <nicolas.le-goff@cea.fr>
2023-05-30 23:36:03 +0200

    switch_to_spack-0.19.2

--- .github/workflows/env_macos.sh
+++ .github/workflows/env_macos.sh
@@ -2,7 +2,7 @@
 
 brew install autoconf automake libtool gcc
 
-git clone --depth=1 -b releases/latest https://github.com/spack/spack.git
+git clone --depth=1 -b v0.19.2 https://github.com/spack/spack.git
 
 #replace the default configuration file of spack by a simpler one without hash, compiler versions, tags and so on 
 cp /Users/runner/work/gmds/gmds/.github/workflows/misc/config.yaml /Users/runner/work/gmds/gmds/spack/etc/spack/defaults/
