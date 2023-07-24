86a499e6df9ce17e7d6327e62153fa8ec46c24a9
Alphadius <paul.bourmaud@hotmail.fr>
2023-07-24 18:39:15 +0200

    modifications of the tests

--- blocking/tst/ExecutionActionsTestSuite.h
+++ blocking/tst/ExecutionActionsTestSuite.h
@@ -8,6 +8,8 @@
 #include <gmds/io/IGMeshIOService.h>
 #include <gmds/io/VTKReader.h>
 #include <unit_test_config.h>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 /*----------------------------------------------------------------------------*/
 /**@brief setup function that initialize a geometric model using the faceted
  * representation and an input vtk file name. The vtk file must contain a
@@ -191,14 +193,10 @@ TEST(ExecutionActionsTestSuite,cb1){
 	ASSERT_EQ(33,bl.get_all_edges().size());
 	ASSERT_EQ(20,bl.get_all_faces().size());
 
-	/*
-	// For delete the useless block
-	auto eS = bl.get_all_edges()[5];
-	auto ed = bl.get_edges_of_node(bl.get_all_nodes()[7]);
-	auto fe = bl.get_faces_of_edge(ed[1]);
 
-	bl.remove_block(bl.get_blocks_of_face(fe[0])[0]);
-	 */
+
+
+
 
 	errors = classifier.classify();
 
@@ -223,6 +221,7 @@ TEST(ExecutionActionsTestSuite,cb1){
 	bl.remove_block(bl.get_blocks_of_face(fe[0])[0]);
 
 
+
 	gmds::Mesh m(gmds::MeshModel(gmds::DIM3|gmds::N|gmds::E|gmds::F|gmds::R|gmds::E2N|gmds::F2N|gmds::R2N));
 	bl.convert_to_mesh(m);
 
@@ -231,11 +230,11 @@ TEST(ExecutionActionsTestSuite,cb1){
 	gmds::VTKWriter vtk_writer(&ios);
 	vtk_writer.setCellOptions(gmds::N|gmds::R);
 	vtk_writer.setDataOptions(gmds::N|gmds::R);
-	vtk_writer.write("debug_blocking.vtk");
+	vtk_writer.write("debug_blocking_cb1.vtk");
 	gmds::VTKWriter vtk_writer_edges(&ios);
 	vtk_writer_edges.setCellOptions(gmds::N|gmds::E);
 	vtk_writer_edges.setDataOptions(gmds::N|gmds::E);
-	vtk_writer_edges.write("debug_blocking_edges.vtk");
+	vtk_writer_edges.write("debug_blocking_edges_cb1.vtk");
 }
 
 
@@ -283,13 +282,14 @@ TEST(ExecutionActionsTestSuite,cb2){
 	//============================================
 	listEdgesPara = bl.get_all_sheet_edge_sets();
 	paramCut = classifier.get_cut_info(errors.non_captured_points[1],listEdgesPara);
+
 	//Do 2 cut
 	bl.cut_sheet(paramCut.first,paramCut.second);
-
 
 	//============================================
 	listEdgesPara = bl.get_all_sheet_edge_sets();
 	paramCut = classifier.get_cut_info(errors.non_captured_points[2],listEdgesPara);
+
 	//Do 3 cut
 	bl.cut_sheet(paramCut.first,paramCut.second);
 
@@ -298,9 +298,9 @@ TEST(ExecutionActionsTestSuite,cb2){
 	//============================================
 	listEdgesPara = bl.get_all_sheet_edge_sets();
 	paramCut = classifier.get_cut_info(errors.non_captured_points[6],listEdgesPara);
+
 	//Do 4 cut
 	bl.cut_sheet(paramCut.first,paramCut.second);
-
 
 
 
@@ -327,10 +327,13 @@ TEST(ExecutionActionsTestSuite,cb2){
 
 
 	auto listB = bl.get_all_blocks();
+
 	/*
 	for(int i =0; i<listB.size();i++){
 		std::cout<<"Block : "<<listB[i]->info().topo_id<<" a la position : "<<i<<std::endl;
 	}*/
+
+
 	//Delete the useless block
 	bl.remove_block(listB[0]);
 
@@ -363,15 +366,15 @@ TEST(ExecutionActionsTestSuite,cb2){
 	gmds::VTKWriter vtk_writer(&ios);
 	vtk_writer.setCellOptions(gmds::N|gmds::R);
 	vtk_writer.setDataOptions(gmds::N|gmds::R);
-	vtk_writer.write("debug_blocking.vtk");
+	vtk_writer.write("debug_blocking_cb2.vtk");
 	gmds::VTKWriter vtk_writer_edges(&ios);
 	vtk_writer_edges.setCellOptions(gmds::N|gmds::E);
 	vtk_writer_edges.setDataOptions(gmds::N|gmds::E);
-	vtk_writer_edges.write("debug_blocking_edges.vtk");
+	vtk_writer_edges.write("debug_blocking_edges_cb2.vtk");
 	gmds::VTKWriter vtk_writer_faces(&ios);
 	vtk_writer_faces.setCellOptions(gmds::N|gmds::F);
 	vtk_writer_faces.setDataOptions(gmds::N|gmds::F);
-	vtk_writer_faces.write("debug_blocking_faces.vtk");
+	vtk_writer_faces.write("debug_blocking_faces_cb2.vtk");
 }
 
 
@@ -468,6 +471,7 @@ TEST(ExecutionActionsTestSuite,cb3){
 	ASSERT_EQ(0,errors.non_captured_curves.size());
 	ASSERT_EQ(3,errors.non_captured_surfaces.size());
 
+
 	//======================================================
 	//==================== DELETE ==============================
 
@@ -481,6 +485,8 @@ TEST(ExecutionActionsTestSuite,cb3){
 	ASSERT_EQ(0,errors.non_captured_curves.size());
 	ASSERT_EQ(0,errors.non_captured_surfaces.size());
 
+
+
 	gmds::Mesh m(gmds::MeshModel(gmds::DIM3|gmds::N|gmds::E|gmds::F|gmds::R|gmds::E2N|gmds::F2N|gmds::R2N));
 	bl.convert_to_mesh(m);
 
@@ -489,11 +495,11 @@ TEST(ExecutionActionsTestSuite,cb3){
 	gmds::VTKWriter vtk_writer(&ios);
 	vtk_writer.setCellOptions(gmds::N|gmds::R);
 	vtk_writer.setDataOptions(gmds::N|gmds::R);
-	vtk_writer.write("debug_blocking.vtk");
+	vtk_writer.write("debug_blocking_cb3.vtk");
 	gmds::VTKWriter vtk_writer_edges(&ios);
 	vtk_writer_edges.setCellOptions(gmds::N|gmds::E);
 	vtk_writer_edges.setDataOptions(gmds::N|gmds::E);
-	vtk_writer_edges.write("debug_blocking_edges.vtk");
+	vtk_writer_edges.write("debug_blocking_edges_cb3.vtk");
 }
 
 
@@ -639,9 +645,9 @@ TEST(ExecutionActionsTestSuite,cb5){
 
 
 	//Check nb points of the geometry and nb nodes of the blocking
-	ASSERT_EQ(42,geom_model.getNbPoints());
-	ASSERT_EQ(63,geom_model.getNbCurves());
-	ASSERT_EQ(23,geom_model.getNbSurfaces());
+	ASSERT_EQ(28,geom_model.getNbPoints());
+	ASSERT_EQ(42,geom_model.getNbCurves());
+	ASSERT_EQ(16,geom_model.getNbSurfaces());
 
 	ASSERT_EQ(8,bl.get_all_nodes().size());
 	ASSERT_EQ(12,bl.get_all_edges().size());
@@ -650,14 +656,14 @@ TEST(ExecutionActionsTestSuite,cb5){
 
 	//Check elements class and captured
 	//Check nb nodes/edges/faces no classified
-	ASSERT_EQ(3,errors.non_classified_nodes.size());
-	ASSERT_EQ(9,errors.non_classified_edges.size());
+	ASSERT_EQ(2,errors.non_classified_nodes.size());
+	ASSERT_EQ(8,errors.non_classified_edges.size());
 	ASSERT_EQ(6,errors.non_classified_faces.size());
 
 	//Check nb points/curves/surfaces no captured
-	ASSERT_EQ(37,errors.non_captured_points.size());
-	ASSERT_EQ(60,errors.non_captured_curves.size());
-	ASSERT_EQ(23,errors.non_captured_surfaces.size());
+	ASSERT_EQ(22,errors.non_captured_points.size());
+	ASSERT_EQ(38,errors.non_captured_curves.size());
+	ASSERT_EQ(16,errors.non_captured_surfaces.size());
 
 
 	//======================= START CUTTING =======================
@@ -672,24 +678,86 @@ TEST(ExecutionActionsTestSuite,cb5){
 	errors = classifier.classify();
 	//Check elements class and captured
 	//Check nb nodes/edges/faces no classified
-	ASSERT_EQ(3,errors.non_classified_nodes.size());
-	ASSERT_EQ(16,errors.non_classified_edges.size());
+	ASSERT_EQ(2,errors.non_classified_nodes.size());
+	ASSERT_EQ(13,errors.non_classified_edges.size());
 	ASSERT_EQ(11,errors.non_classified_faces.size());
 
 	//Check nb points/curves/surfaces no captured
-	ASSERT_EQ(34,errors.non_captured_points.size());
-	ASSERT_EQ(59,errors.non_captured_curves.size());
-	ASSERT_EQ(23,errors.non_captured_surfaces.size());
+	ASSERT_EQ(20,errors.non_captured_points.size());
+	ASSERT_EQ(35,errors.non_captured_curves.size());
+	ASSERT_EQ(16,errors.non_captured_surfaces.size());
+
+
+	//=======================================================================
+	listEdgesPara = bl.get_all_sheet_edge_sets();
+	paramCut = classifier.get_cut_info(errors.non_captured_points[3],listEdgesPara);
+	//Do 1 cut
+	bl.cut_sheet(paramCut.first,paramCut.second);
+
+
+	errors = classifier.classify();
+	//=======================================================================
+	listEdgesPara = bl.get_all_sheet_edge_sets();
+	paramCut = classifier.get_cut_info(errors.non_captured_points[3],listEdgesPara);
+	//Do 1 cut
+	bl.cut_sheet(paramCut.first,paramCut.second);
+
+
+	errors = classifier.classify();
+
+	//=======================================================================
+	listEdgesPara = bl.get_all_sheet_edge_sets();
+	paramCut = classifier.get_cut_info(errors.non_captured_points[5],listEdgesPara);
+	//Do 1 cut
+	bl.cut_sheet(paramCut.first,paramCut.second);
+
+
+	errors = classifier.classify();
+
+	//=======================================================================
+	listEdgesPara = bl.get_all_sheet_edge_sets();
+	paramCut = classifier.get_cut_info(errors.non_captured_points[1],listEdgesPara);
+	//Do 1 cut
+	bl.cut_sheet(paramCut.first,paramCut.second);
+
+
+	errors = classifier.classify();
+
+	//=======================================================================
+	listEdgesPara = bl.get_all_sheet_edge_sets();
+	paramCut = classifier.get_cut_info(errors.non_captured_points[0],listEdgesPara);
+	//Do 1 cut
+	bl.cut_sheet(paramCut.first,paramCut.second);
+
+
+	errors = classifier.classify();
+
+	//=======================================================================
+	listEdgesPara = bl.get_all_sheet_edge_sets();
+	paramCut = classifier.get_cut_info(errors.non_captured_points[1],listEdgesPara);
+	//Do 1 cut
+	bl.cut_sheet(paramCut.first,paramCut.second);
+
+
+	errors = classifier.classify();
+
+	srand (time(NULL));
 
 	//===================================================================
 	while(errors.non_captured_points.size()!=0) {
+
+		int sizeVect = errors.non_captured_points.size()-1;
+		int random = rand() % sizeVect;
 		listEdgesPara = bl.get_all_sheet_edge_sets();
-		paramCut = classifier.get_cut_info(errors.non_captured_points[0], listEdgesPara);
+		paramCut = classifier.get_cut_info(errors.non_captured_points[random], listEdgesPara);
 		// Do 1 cut
 		bl.cut_sheet(paramCut.first, paramCut.second);
 
 		errors = classifier.classify();
+
+		std::cout<<"CUT"<<std::endl;
 	}
+	/*
 	//Check elements class and captured
 	//Check nb nodes/edges/faces no classified
 	ASSERT_EQ(27,errors.non_classified_nodes.size());
@@ -700,6 +768,7 @@ TEST(ExecutionActionsTestSuite,cb5){
 	ASSERT_EQ(0,errors.non_captured_points.size());
 	ASSERT_EQ(21,errors.non_captured_curves.size());
 	ASSERT_EQ(23,errors.non_captured_surfaces.size());
+
 
 	auto listB = bl.get_all_blocks();
 
@@ -733,11 +802,12 @@ TEST(ExecutionActionsTestSuite,cb5){
 	bl.remove_block(listB[6]);
 
 	bl.remove_block(listB[44]);
+	 */
 	/*
 	for(int i =0; i<listB.size();i++){
 		std::cout<<"Block : "<<listB[i]->info().topo_id<<" a la position : "<<i<<std::endl;
 	}*/
-
+/*
 	errors = classifier.classify();
 
 	//Check elements class and captured
@@ -750,7 +820,7 @@ TEST(ExecutionActionsTestSuite,cb5){
 	ASSERT_EQ(0,errors.non_captured_points.size());
 	ASSERT_EQ(21,errors.non_captured_curves.size());
 	ASSERT_EQ(23,errors.non_captured_surfaces.size());
-
+*/
 
 	gmds::Mesh m(gmds::MeshModel(gmds::DIM3|gmds::N|gmds::E|gmds::F|gmds::R|gmds::E2N|gmds::F2N|gmds::R2N));
 	bl.convert_to_mesh(m);
