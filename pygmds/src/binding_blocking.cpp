649b1d25b53f97686c9372c2808f16b475665154
Alphadius <paul.bourmaud@hotmail.fr>
2023-08-22 13:36:06 +0200

    add the function on binding_blocking.cpp

--- pygmds/src/binding_blocking.cpp
+++ pygmds/src/binding_blocking.cpp
@@ -74,5 +74,6 @@ void bind_blocking(py::module &m){
             .def(py::init<gmds::blocking::CurvedBlocking *>())
             .def("clear_classification", &gmds::blocking::CurvedBlockingClassifier::clear_classification)
             .def("classify", &gmds::blocking::CurvedBlockingClassifier::classify)
-            .def("detect_classification_errors", &gmds::blocking::CurvedBlockingClassifier::detect_classification_errors);
+            .def("detect_classification_errors", &gmds::blocking::CurvedBlockingClassifier::detect_classification_errors)
+            .def("actions_current_state", &gmds::blocking::CurvedBlockingClassifier::list_Possible_Cuts);
 }
