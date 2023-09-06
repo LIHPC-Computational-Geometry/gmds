78b1eb8c4e36f47a37dd4d690a0779e38e70edca
nicolas le goff <nicolas.le-goff@cea.fr>
2023-09-06 23:03:05 +0200

    added MeshDoctor::buildEfromF and buildEfromR methods

--- ig/tst/MeshDoctorTestSuite.h
+++ ig/tst/MeshDoctorTestSuite.h
@@ -39,22 +39,35 @@ TEST(MeshDocClass, buildE)
 	gmds::Mesh m(gmds::MeshModel(DIM3 | R | F | E | N | R2N | F2N | E2N | R2F | F2R |
 	                             F2E | E2F | R2E | E2R | N2R | N2F | N2E));
 
-	gmds::Node n0 = m.newNode(0,0,0);
-	gmds::Node n1 = m.newNode(1,1,0);
-	gmds::Node n2 = m.newNode(0,1,0);
-	gmds::Node n3 = m.newNode(1,0,0);
+	gmds::Node n0 = m.newNode(0,0,1);
+	gmds::Node n1 = m.newNode(1,0,1);
+	gmds::Node n2 = m.newNode(1,1,1);
+	gmds::Node n3 = m.newNode(0,1,1);
+	gmds::Node n4 = m.newNode(0,0,1);
+	gmds::Node n5 = m.newNode(1,0,1);
+	gmds::Node n6 = m.newNode(1,1,1);
+	gmds::Node n7 = m.newNode(0,1,1);
 
-	m.newTriangle(n0,n1,n3);
-	m.newTriangle(n0,n3,n2);
+	m.newTriangle(n0,n1,n2);
+	m.newTriangle(n0,n2,n3);
 	m.newQuad(n0,n1,n2,n3);
 
 	ASSERT_EQ(m.getNbEdges(), 0);
-
-	std::cout << "1. Nbr edges: " << m.getNbEdges() << std::endl;
 	gmds::MeshDoctor doc(&m);
 	doc.buildE();
-	std::cout << "2. Nbr edges: " << m.getNbEdges() << std::endl;
+	ASSERT_EQ(m.getNbEdges(), 0);
+	m.newEdge(n0,n7);
+	ASSERT_EQ(m.getNbEdges(), 1);
+	doc.buildEfromR();
+	ASSERT_EQ(m.getNbEdges(), 1);
+	doc.buildEfromF();
+	ASSERT_EQ(m.getNbEdges(), 6);
+
+	m.newHex(n0,n1,n2,n3,n4,n5,n6,n7);
+	m.newPyramid(n0,n3,n7,n4,n1);
+	m.newPrism3(n0,n1,n2,n4,n5,n6);
+	doc.buildEfromR();
+	ASSERT_EQ(m.getNbEdges(), 18);
 
 }
-
 /*----------------------------------------------------------------------------*/
