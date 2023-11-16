6dfa10fa7a52c638dcbd8f3db462128e59b8e597
calderans <calderan.simon@gmail.com>
2023-11-16 14:05:18 +0100

    - modified blocking main in order to use it for dev

---
+++ blocking/src/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by calderans on 30/06/23.
+//
+#include <gmds/blocking/CGNSWriter3D.h>
+
+using namespace gmds;
+using namespace blocking;
+/*----------------------------------------------------------------------------*/
+int main(int argc, char** argv)
+{
+	std::cout << "Empty main for dev" << std::endl;
+	if (argc != 1 )
+	{
+		std::cout << "" << std::endl;
+		exit(0);
+	}
+
+}
