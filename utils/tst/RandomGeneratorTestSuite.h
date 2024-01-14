2e1329ad6cf973bd97568eca8cad2f7f23ba281f
Svvmy <benidir91@hotmail.fr>
2024-01-14 01:33:24 +0100

    add test suite RandomGenerator

---
+++ utils/tst/RandomGeneratorTestSuite.h
@@ -0,0 +1,27 @@
+#include "gmds/utils/RandomGenerator.h"
+#include "gtest/gtest.h"
+#include <unit_test_config.h>
+
+TEST(RandomGeneratorTest, ValueInRange)
+{
+	gmds::RandomGenerator randomGen;
+	randomGen.init();
+
+	for (int i = 0; i < 1000; ++i) {
+		double value = randomGen.value();
+		EXPECT_GE(value, 0.0);
+		EXPECT_LE(value, 1.0);
+	}
+}
+
+TEST(RandomGeneratorTest, Initialization)
+{
+	gmds::RandomGenerator randomGen1;
+	gmds::RandomGenerator randomGen2;
+
+	randomGen1.init();
+	randomGen2.init();
+
+	// Test if two instances have different seed values
+	EXPECT_NE(randomGen1.value(), randomGen2.value());
+}
--- utils/tst/main_test.cpp
+++ utils/tst/main_test.cpp
@@ -2,13 +2,15 @@
 #include <gtest/gtest.h>
 /*----------------------------------------------------------------------------*/
 // Files containing the different test suites to launch
-#include "UtilsTestSuite.h"
-#include "ParamTestSuite.h"
 #include "ArrayTestSuite.h"
+#include "ParamTestSuite.h"
+#include "RandomGeneratorTest.h"
+#include "UtilsTestSuite.h"
 /*----------------------------------------------------------------------------*/
-int main(int argc, char ** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+int
+main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
 /*----------------------------------------------------------------------------*/
-
