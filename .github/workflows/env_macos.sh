43afa7101edc152dd9b555793e801b8b335c087f
nicolas le goff <nicolas.le-goff@cea.fr>
2024-01-19 06:45:20 +0100

    desabling spack bootstrapong to avoid collision for clingo in macosCI

--- .github/workflows/env_macos.sh
+++ .github/workflows/env_macos.sh
@@ -4,7 +4,7 @@
 git clone --depth=1 -b v0.21.1 https://github.com/spack/spack.git
 
 #replace the default configuration file of spack by a simpler one without hash, compiler versions, tags and so on 
-cp /Users/runner/work/gmds/gmds/.github/workflows/misc/config-0.21.1.yaml /Users/runner/work/gmds/gmds/spack/etc/spack/defaults/config.yaml
+#cp /Users/runner/work/gmds/gmds/.github/workflows/misc/config-0.21.1.yaml /Users/runner/work/gmds/gmds/spack/etc/spack/defaults/config.yaml
 . ./spack/share/spack/setup-env.sh
 
 git clone --branch gmds_temp --depth=1 https://github.com/LIHPC-Computational-Geometry/spack_recipes.git
