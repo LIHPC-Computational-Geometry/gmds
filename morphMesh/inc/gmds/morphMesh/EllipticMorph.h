3bc36fac4e6bfe1be10d7c621045fc679f9494cd
calderans <calderan.simon@gmail.com>
2024-01-29 15:52:51 +0100

    - Improving EllipticMorph documentation

--- morphMesh/inc/gmds/morphMesh/EllipticMorph.h
+++ morphMesh/inc/gmds/morphMesh/EllipticMorph.h
@@ -12,10 +12,43 @@ namespace morphmesh {
 /*----------------------------------------------------------------------------*/
 class LIB_GMDS_MORPHMESH_API EllipticMorph
 {
+	/*
+	 * Need a parameter file in argument
+	 *
+	 * param file format:
+	 *
+	 * Input file name 				//string
+	 * <a_input_filename>.vtk|.mli2
+	 *
+	 * Output file name 				//string
+	 * <a_output_filename>.vtk|.mli2
+	 *
+	 * To morph							//string
+	 * <groupname0>
+	 * <groupname1>
+	 * ...
+	 *
+	 * External lock					//string
+	 * <groupname2>
+	 * <groupname3>
+	 * ...
+	 *
+	 * Internal lock					//string
+    * <groupname4>
+    * <groupname5>
+    * ...
+    *
+    * Ellipses 						//double double double double
+    * X0 coefa0 coefb0 coefc0
+	 * X1 coefa1 coefb1 coefc1
+	 * ...
+	 */
 
  public:
 	/*------------------------------------------------------------------------*/
 	/** @brief Constructor.
+	 * @param AFilename parameters file name
+	 * @param AMesh Empty mesh used to instantiate intern mesh attribute
 	 */
 	EllipticMorph(const std::string& AFilename, Mesh* AMesh);
 
@@ -25,7 +58,7 @@ class LIB_GMDS_MORPHMESH_API EllipticMorph
 	~EllipticMorph();
 
 	/*------------------------------------------------------------------------*/
-	/** @brief Constructor.
+	/** @brief Execute the elliptic algorithm using parameters given in argument.
 	 */
 	void execute();
 
@@ -40,22 +73,38 @@ class LIB_GMDS_MORPHMESH_API EllipticMorph
 
  private:
 
-	/* Locked nodes */
+	/* Mesh mark for locked nodes */
 	int m_locked;
-	/* Nodes on the surface of the mesh */
-	std::vector<TCellID> m_surfNodes;
-	std::vector<TCellID> m_locked_faces;
+	//List of gmds nodes that will be moved in the algorithm
 	std::vector<Node> m_morphed;
+	//List of gmds nodes that are locked as internal origin
 	std::vector<Node> m_lockedIn;
+	//List of gmds nodes that are locked as external surface
 	std::vector<Node> m_lockedOut;
 
 	std::string m_inputName;
 	std::string m_outputName;
 
+	//Lists used to store groups name (extracted from param file) which are used in the algorithm
+	//Groups name that will be moved in the algorithm
 	std::vector<std::string> m_to_morph;
+	//Lists used to store groups name (extracted from param file) which are used in the algorithm
+	//Groups name that are locked as external surface
 	std::vector<std::string> m_ext_lock;
+	//Lists used to store groups name (extracted from param file) which are used in the algorithm
+	//Groups name that are locked as internal origin
 	std::vector<std::string> m_int_lock;
 
+	/* *Ellipses (extracted from param file) store as lists of doubles as follows {X, coefa, coefb, coefc}, X is the
+	 * position of the ellispe on X axis and coefs are
+    *                a
+    *                |
+    *                |
+    *     b ------------------- b
+    *                |
+    *                |
+    *                c
+	 */
 	std::vector<std::vector<double>> m_ellipses;
 
 
--- morphMesh/src/main.cpp
+++ morphMesh/src/main.cpp
@@ -12,37 +12,12 @@ int main(int argc, char* argv[]){
 
 	std::string param_file(argv[1]);
 
+	/*
+	 * Mesh model needs to be changed to reduce mesh cells and memory usage
+	 */
 	gmds::Mesh m_mesh(gmds::MeshModel(gmds::DIM3 | gmds::R | gmds::F | gmds::N | gmds::N2R | gmds::R2N | gmds::R2F | gmds::F2N | gmds::F2R));
-	gmds::IGMeshIOService ioService(&m_mesh);
-	gmds::VTKReader vtkReader(&ioService);
-	vtkReader.setCellOptions(gmds::N|gmds::R);
-	vtkReader.setDataOptions(gmds::N|gmds::R);
-	//vtkReader.read("test_morph.vtk");
-
-
-
-	gmds::MeshDoctor doc(&m_mesh);
-	doc.buildN2R(m_mesh.getModel());
-	doc.buildFacesAndR2F();
-	doc.updateUpwardConnectivity();
-
-	//gmds::morphmesh::MorphMesh morph(&m_mesh,points,2.5);
-	//morph.execute();
 
 	morphmesh::EllipticMorph emorph(param_file, &m_mesh);
-
-	std::vector<std::vector<double>> ellipses;
-	ellipses.push_back({4.5,1,1,1});
-	ellipses.push_back({5,1,1.2,1});
-	ellipses.push_back({5.5,1,1,1});
-	//ellipses.push_back({0.2,1,1,1});
-	//ellipses.push_back({3,1.2,1.5,1});
-	//ellipses.push_back({4,1.2,1.5,1});
-	//ellipses.push_back({5,1.4,1.75,1.5});
-	//ellipses.push_back({7,1.4,1.75,1.5});
-	//ellipses.push_back({8,1,1,1});
-	//ellipses.push_back({5.6,1.2,1.5,1});
-	//ellipses.push_back({8.5,1.2,1.5,1});
 
 	emorph.execute();
 }
