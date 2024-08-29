7f3e28e57e68536ba4769f52c487b3cf22f23a23
nicolas le goff <nicolas.le-goff@cea.fr>
2024-08-29 21:52:17 +0200

    removed use of the gmds_temp branch

--- .github/workflows/env_macos.sh
+++ .github/workflows/env_macos.sh
@@ -5,12 +5,12 @@ git clone --depth=1 -b v0.21.1 https://github.com/spack/spack.git
 
 #replace the default configuration file of spack by a simpler one without hash, compiler versions, tags and so on 
 #cp /Users/runner/work/gmds/gmds/.github/workflows/misc/config-0.21.1.yaml /Users/runner/work/gmds/gmds/spack/etc/spack/defaults/config.yaml
-. ./spack/share/spack/setup-env.sh
+source ./spack/share/spack/setup-env.sh
 
-git clone --branch gmds_temp --depth=1 https://github.com/LIHPC-Computational-Geometry/spack_recipes.git
+git clone --depth=1 https://github.com/LIHPC-Computational-Geometry/spack_recipes.git
 
-spack repo add ./spack_recipes/meshing_repo
-spack repo add ./spack_recipes/supersede_repo
+spack repo add ./spack_recipes/meshing
+spack repo add ./spack_recipes/meshing_supersede
 
 spack external find cmake
 spack install --only dependencies gmds+blocking~cgns
