e704a49b45dcf0742fd492c14f8b406fae4a2827
nicolas le goff <nicolas.le-goff@cea.fr>
2024-10-25 15:28:52 +0200

    CI: switched to spack-0.22.2 in the env_macos.sh and env_linux.sh scripts

--- .github/workflows/env_linux.sh
+++ .github/workflows/env_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --depth=1 -b v0.20.3 https://github.com/spack/spack.git
+git clone --depth=1 -b v0.22.2 https://github.com/spack/spack.git
 sed -i 's#"{architecture}/{compiler.name}-{compiler.version}/{name}-{version}-{hash}"#"{name}"#g' spack/etc/spack/defaults/config.yaml
 source ./spack/share/spack/setup-env.sh
 
