06212a977f47b4174d727461b156ec4342760e8d
nicolas le goff <nicolas.le-goff@cea.fr>
2025-09-16 13:59:02 +0200

    macOS CIswitched to spack-0.23.1 and added installation of py-pytest

--- .github/workflows/env_macos.sh
+++ .github/workflows/env_macos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --depth=1 -b v0.22.2 https://github.com/spack/spack.git
+git clone --depth=1 -b v0.23.1 https://github.com/spack/spack.git
 
 #replace the default configuration file of spack by a simpler one without hash, compiler versions, tags and so on 
 #cp /Users/runner/work/gmds/gmds/.github/workflows/misc/config-0.21.1.yaml /Users/runner/work/gmds/gmds/spack/etc/spack/defaults/config.yaml
@@ -12,4 +12,5 @@ spack repo add ./spack_recipes/meshing
 spack repo add ./spack_recipes/meshing_supersede
 
 spack external find cmake
-spack install --only dependencies gmds+blocking~cgns+python
+spack compiler find
+spack install --only dependencies gmds+python~blocking+cgns ^cgns~mpi ^hdf5~mpi
